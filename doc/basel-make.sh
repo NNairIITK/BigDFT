4230fe3021576c5a9bad771731008dbfa0ac6aa8
Caliste Damien <caliste@pcpm.ucl.ac.be>
2006-09-29 18:48:52 +0200

    Summary: improve documentation and update input files
    Changes:
    	* utils/inguess.dat: add the atomic orbital description file.
    	* utils/Makefile.am: add inguess.dat in the distribution.
    	* doc/examples/input.dat: add the three lines required by the
    	  new preconditioner.
    	* doc/basel-make.sh: upgrade the way to use the autotools
    	  in Basel node.

--- doc/basel-make.sh
+++ doc/basel-make.sh
@@ -9,8 +9,10 @@ echo "Configuring the package for paralell version with ifort"
 export FC=/opt/intel/fc/9.0/bin/ifort   # Choose your Fortran Compiler
 export MPI_INCLUDE=/opt/scali/include   # Set the include directory for MPI
 export MPI_LDFLAGS=/opt/scali/lib       # Set the directory where to find MPI library
-export LDFLAGS="-L/opt/intel/mkl72/lib/32/ -lguide -lpthread -D_REENTRANT"
-./configure --enable-mpi --with-lapack=/opt/intel/mkl72/lib/32/libmkl_lapack.a --with-blas=/opt/intel/mkl72/lib/32/libmkl_ia32.a --prefix=/home/caliste/usr
+export LDFLAGS="/opt/intel/mkl72/lib/32/libmkl_ia32.a -lguide -lpthread -D_REENTRANT"
+./configure --enable-mpi --without-lapack --without-blas --with-ext-linalg-path=/opt/intel/mkl72/lib/32 --with-ext-linalg=mkl_lapack --prefix=$HOME/usr
+
+
 
 echo "Compile the sources."
 make
