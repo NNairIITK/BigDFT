360c6eef1af03c1bd1fc5fb5aaebc6ea017a3e12
Luigi Genovese <gigi@zahtar>
2007-04-26 18:29:44 +0200

    Fixed some modification for the semicore electron, routines for memeory saving in transall

--- src/cluster_all.f90
+++ src/cluster_all.f90
@@ -1173,12 +1173,12 @@ subroutine cluster(parallel,nproc,iproc,nat,ntypes,iatype,atomnames, rxyz, energ
         write(*,'(1x,a,3(1x,1pe12.5))')'BIG: simulation cell',alatb1,alatb2,alatb3
      endif
      !    ---reformat potential
+     allocate(pot((2*n1+31),(2*n2+31),(2*n3+31)),stat=i_all)
+     if (i_all /= 0) then
+        write(*,*)' cluster: problem of memory allocation'
+        stop
+     end if
      if (datacode=='D') then
-        allocate(pot((2*n1+31),(2*n2+31),(2*n3+31)),stat=i_all)
-        if (i_all /= 0) then
-           write(*,*)' cluster: problem of memory allocation'
-           stop
-        end if
        write(*,*) 'n3d,n3p,n3pi',n3d,n3p,n3pi
         call MPI_ALLGATHERV(rhopot(1,1,1+i3xcsh),(2*n1+31)*(2*n2+31)*n3d,MPI_DOUBLE_PRECISION, &
              pot,(2*n1+31)*(2*n2+31)*nscatterarr(:,2),(2*n1+31)*(2*n2+31)*nscatterarr(:,3), & 
@@ -1671,52 +1671,95 @@ subroutine transallwaves(iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp,psi,psit)
 
 END SUBROUTINE transallwaves
 
+subroutine switch_waves(iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp,psi,psiw)
+  implicit none
+  integer, intent(in) :: iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp
+  real(kind=8), dimension(nvctr_c+7*nvctr_f,norbp), intent(in) :: psi
+  real(kind=8), dimension(nvctrp,norbp,nproc), intent(out) :: psiw
+  !local variables
+  integer :: iorb,i,j,ij
+
+  do iorb=iproc*norbp+1,min((iproc+1)*norbp,norb)
+     ij=1
+     do j=1,nproc
+        do i=1,nvctrp
+           if (ij .le. nvctr_c+7*nvctr_f) then
+              psiw(i,iorb-iproc*norbp,j)=psi(ij,iorb-iproc*norbp)
+           else
+              psiw(i,iorb-iproc*norbp,j)=0.d0
+           endif
+           ij=ij+1
+        enddo
+     enddo
+  enddo
+
+end subroutine switch_waves
+
+subroutine unswitch_waves(iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp,psiw,psi)
+  implicit none
+  integer, intent(in) :: iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp
+  real(kind=8), dimension(nvctrp,norbp,nproc), intent(in) :: psiw
+  real(kind=8), dimension(nvctr_c+7*nvctr_f,norbp), intent(out) :: psi
+  !local variables
+  integer :: iorb,i,j,ij
+
+  do iorb=iproc*norbp+1,min((iproc+1)*norbp,norb)
+     ij=1
+     loop: do j=1,nproc
+        do i=1,nvctrp
+           psi(ij,iorb-iproc*norbp)=psiw(i,iorb-iproc*norbp,j)
+           ij=ij+1
+           if (ij.gt. nvctr_c+7*nvctr_f) exit loop
+        enddo
+     enddo loop
+  enddo
+
+end subroutine unswitch_waves
+
+subroutine untransallwaves(iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp,psit,psi)
+  implicit real*8 (a-h,o-z)
+  logical, parameter :: parallel=.true.
+  integer recvcount,sendcount
+  dimension psi(nvctr_c+7*nvctr_f,norbp),psit(nvctrp,norbp*nproc)
+  real*8, allocatable :: psiw(:,:,:)
+  include 'mpif.h'
+
+  call timing(iproc,'Un-Transall   ','ON')
+
+  allocate(psiw(nvctrp,norbp,nproc),stat=i_all)
+  if (i_all /= 0) then
+     write(*,*)' untransallwaves: problem of memory allocation'
+     stop
+  end if
+
+  sendcount=nvctrp*norbp
+  recvcount=nvctrp*norbp
+
+  ! transposition: psiw(i,iorb,j,jorb) <- psit(i,iorb,jorb,j) 
+  call MPI_ALLTOALL(psit,sendcount,MPI_DOUBLE_PRECISION,  &
+       psiw,recvcount,MPI_DOUBLE_PRECISION,MPI_COMM_WORLD,ierr)
+
+  ! reformatting: psi(ij,iorb,jorb) <- psiw(i,iorb,j,jorb)
+  do iorb=iproc*norbp+1,min((iproc+1)*norbp,norb)
+     ij=1
+     do j=1,nproc
+        do i=1,nvctrp
+           psi(ij,iorb-iproc*norbp)=psiw(i,iorb-iproc*norbp,j)
+           ij=ij+1
+           if (ij.gt. nvctr_c+7*nvctr_f) goto 333
+        enddo
+     enddo
+333  continue
+  enddo
+
+  deallocate(psiw,stat=i_all)
+  if (i_all /= 0) then
+     write(*,*)' untransallwaves: problem of memory deallocation'
+     stop
+  end if
+  call timing(iproc,'Un-Transall   ','OF')
 
 
-        subroutine untransallwaves(iproc,nproc,norb,norbp,nvctr_c,nvctr_f,nvctrp,psit,psi)
-        implicit real*8 (a-h,o-z)
-        logical, parameter :: parallel=.true.
-        integer recvcount,sendcount
-        dimension psi(nvctr_c+7*nvctr_f,norbp),psit(nvctrp,norbp*nproc)
-        real*8, allocatable :: psiw(:,:,:)
-        include 'mpif.h'
- 
-         call timing(iproc,'Un-Transall   ','ON')
-
-         allocate(psiw(nvctrp,norbp,nproc),stat=i_all)
-         if (i_all /= 0) then
-            write(*,*)' untransallwaves: problem of memory allocation'
-            stop
-         end if
- 
-          sendcount=nvctrp*norbp
-          recvcount=nvctrp*norbp
- 
-! transposition: psiw(i,iorb,j,jorb) <- psit(i,iorb,jorb,j) 
-       call MPI_ALLTOALL(psit,sendcount,MPI_DOUBLE_PRECISION,  &
-                         psiw,recvcount,MPI_DOUBLE_PRECISION,MPI_COMM_WORLD,ierr)
- 
-! reformatting: psi(ij,iorb,jorb) <- psiw(i,iorb,j,jorb)
-      do iorb=iproc*norbp+1,min((iproc+1)*norbp,norb)
-         ij=1
-         do j=1,nproc
-         do i=1,nvctrp
-         psi(ij,iorb-iproc*norbp)=psiw(i,iorb-iproc*norbp,j)
-         ij=ij+1
-         if (ij.gt. nvctr_c+7*nvctr_f) goto 333
-         enddo
-         enddo
-333     continue
-      enddo
- 
-        deallocate(psiw,stat=i_all)
-        if (i_all /= 0) then
-           write(*,*)' untransallwaves: problem of memory deallocation'
-           stop
-        end if
-        call timing(iproc,'Un-Transall   ','OF')
- 
- 
 END SUBROUTINE untransallwaves
 
 !-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!-!
@@ -5336,7 +5379,9 @@ subroutine readAtomicOrbitals(iproc,ngx,xp,psiat,occupat,ng,nl,psppar,npspcode,&
            lsc=floor(sccode/10.d0**ipow)
            iorbsc_count=iorbsc_count+(2*lsc-1)
            scorb(lsc,iatsc)=.true.
+           sccode=sccode-real(lsc,kind=8)*10.d0**ipow
            ipow=ipow-1
+           !print *,iasctype(ity),inorbsc,lsc
         end do
         norbsc_arr(iatsc)=iorbsc_count
         norbsc=norbsc+iorbsc_count
@@ -5525,7 +5570,7 @@ subroutine input_wf_diag(parallel,iproc,nproc,nfl1,nfu1,nfl2,nfu2,nfl3,nfu3, &
   dimension ppsi(nvctr_c+7*nvctr_f,norbp)
   dimension nscatterarr(0:nproc-1,4)!n3d,n3p,i3s+i3xcsh-1,i3xcsh
 
-  allocatable :: psi(:,:),hpsi(:,:),psit(:,:),hpsit(:,:),ppsit(:,:),occupe(:)
+  allocatable :: psi(:,:),hpsi(:,:),psit(:,:),hpsit(:,:),ppsit(:,:),occupe(:),psiw(:,:,:)
   allocatable :: hamovr(:,:),evale(:),work_lp(:),pot(:),norbsc_arr(:)
   logical, allocatable :: scorb(:,:)
   include 'mpif.h'
@@ -5693,6 +5738,22 @@ subroutine input_wf_diag(parallel,iproc,nproc,nfl1,nfu1,nfl2,nfu2,nfl3,nfu3, &
 
      !transpose all the wavefunctions for having a piece of all the orbitals 
      !for each processor
+     call timing(iproc,'Un-Transall   ','ON')
+     allocate(psiw(nvctrp,norbep,nproc),stat=i_all)
+     if (i_all /= 0) then
+        write(*,*)' diag_wf: problem of memory allocation'
+        stop
+     end if
+
+     !call  transallwaves(iproc,nproc,norbe,norbep,nvctr_c,nvctr_f,nvctrp,psi,psit)
+
+     call switch_waves(iproc,nproc,norbe,norbep,nvctr_c,nvctr_f,nvctrp,psi,psiw)
+
+     deallocate(psi,stat=i_all)
+     if (i_all /= 0) then
+        write(*,*)' input_wf_diag: problem of memory deallocation'
+        stop
+     end if
      if (iproc.eq.0) write(*,'(1x,a,i0)') 'Allocate words for psit inguess ',nvctrp*norbep*nproc
      allocate(psit(nvctrp,norbep*nproc),stat=i_all)
      if (i_all /= 0) then
@@ -5701,13 +5762,15 @@ subroutine input_wf_diag(parallel,iproc,nproc,nfl1,nfu1,nfl2,nfu2,nfl3,nfu3, &
      end if
      if (iproc.eq.0) write(*,*) 'Allocation done'
 
-     call  transallwaves(iproc,nproc,norbe,norbep,nvctr_c,nvctr_f,nvctrp,psi,psit)
+     call MPI_ALLTOALL(psiw,nvctrp*norbep,MPI_DOUBLE_PRECISION,  &
+          psit,nvctrp*norbep,MPI_DOUBLE_PRECISION,MPI_COMM_WORLD,ierr)
 
-     deallocate(psi,stat=i_all)
+     deallocate(psiw,stat=i_all)
      if (i_all /= 0) then
         write(*,*)' input_wf_diag: problem of memory deallocation'
         stop
      end if
+     call timing(iproc,'Un-Transall   ','OF')
 
 
      if (iproc.eq.0) write(*,'(1x,a,i0)')&
