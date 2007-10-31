dbac66317454d7de5c5c9e646404a1d97e5c8e66
Luigi Genovese <genovese@zahtar>
2007-10-31 11:48:29 +0100

    Interfaces modified, sumrho to be verified

--- src/interfaces.f90
+++ src/interfaces.f90
@@ -49,6 +49,20 @@ interface
      real(kind=8), intent(out) :: alat1,alat2,alat3
    end subroutine system_size
 
+   subroutine MemoryEstimator(nproc,idsx,n1,n2,n3,alat1,alat2,alat3,hgrid,nat,ntypes,iatype,&
+        rxyz,radii_cf,crmult,frmult,norb,atomnames,output_grid,nspin,peakmem)
+     implicit none
+     !Arguments
+     logical, intent(in) :: output_grid
+     integer, intent(in) :: nproc,idsx,n1,n2,n3,nat,ntypes,norb,nspin
+     integer, dimension(nat), intent(in) :: iatype
+     character(len=20), dimension(100), intent(in) :: atomnames
+     real(kind=8), intent(in) :: hgrid,crmult,frmult,alat1,alat2,alat3
+     real(kind=8), dimension(3,nat), intent(in) :: rxyz
+     real(kind=8), dimension(ntypes,2), intent(in) ::  radii_cf
+     real(kind=8), intent(out) :: peakmem
+   end subroutine MemoryEstimator
+
    subroutine createWavefunctionsDescriptors(iproc,nproc,idsx,n1,n2,n3,output_grid,&
         hgrid,nat,ntypes,iatype,atomnames,alat1,alat2,alat3,rxyz,radii_cf,crmult,frmult,&
         wfd,nvctrp,norb,norbp,nfl1,nfu1,nfl2,nfu2,nfl3,nfu3,bounds)
