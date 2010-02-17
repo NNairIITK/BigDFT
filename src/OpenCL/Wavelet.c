e58fce1f141cf3657b086b1ddad8b5032fd3de5b
Brice Videau <brice.videau@cea.fr>
2010-02-17 18:33:34 +0100

    Further optimized syn1d kernel.

--- src/OpenCL/Wavelet.c
+++ src/OpenCL/Wavelet.c
@@ -87,14 +87,13 @@ void FC_FUNC_(syn1d_d,SYN1D_D)(cl_command_queue *command_queue, cl_uint *n, cl_u
 #endif
     int FILTER_WIDTH = 16;
     if(*n<FILTER_WIDTH) { fprintf(stderr,"%s %s : matrix is too small!\n", __func__, __FILE__); exit(1);}
-    size_t block_size_i=FILTER_WIDTH, block_size_j=FILTER_WIDTH;
+    size_t block_size_i=16, block_size_j=16;
     cl_uint i = 0;
     clSetKernelArg(syn1d_kernel_d, i++,sizeof(*n), (void*)n);
     clSetKernelArg(syn1d_kernel_d, i++,sizeof(*ndat), (void*)ndat);
     clSetKernelArg(syn1d_kernel_d, i++,sizeof(*psi), (void*)psi);
     clSetKernelArg(syn1d_kernel_d, i++,sizeof(*out), (void*)out);
-    clSetKernelArg(syn1d_kernel_d, i++,sizeof(double)*block_size_j*(block_size_i+FILTER_WIDTH + 1), NULL);
-    clSetKernelArg(syn1d_kernel_d, i++,sizeof(double)*block_size_j*(block_size_i+FILTER_WIDTH + 1), NULL);
+    clSetKernelArg(syn1d_kernel_d, i++,sizeof(double)*block_size_j*(block_size_i*2+FILTER_WIDTH*2+1), NULL);
     size_t localWorkSize[] = { block_size_i,block_size_j };
     size_t globalWorkSize[] ={ shrRoundUp(block_size_i,*n), shrRoundUp(block_size_j,*ndat)};
     ciErrNum = clEnqueueNDRangeKernel  (*command_queue, syn1d_kernel_d, 2, NULL, globalWorkSize, localWorkSize, 0, NULL, NULL);
--- src/OpenCL/Wavelet.h
+++ src/OpenCL/Wavelet.h
@@ -37,39 +37,39 @@ barrier(CLK_LOCAL_MEM_FENCE);\n\
 \
 double ci = 0.0;\n\
 double di = 0.0;\n\
-__local double * tmp_o = tmp + j2*(3*FILTER_WIDTH+1) + FILTER_WIDTH/2+2*i2;\n\
-ci += tmp_o[ 7] * -0.00030292051472413308126;\n\
-di += tmp_o[ 7] *  0.00054213233180001068935;\n\
-di += tmp_o[-6] * -0.00030292051472413308126;\n\
-ci += tmp_o[-6] * -0.00054213233180001068935;\n\
-ci += tmp_o[ 8] *  0.0018899503327676891843;\n\
-di += tmp_o[ 8] * -0.0033824159510050025955;\n\
-di += tmp_o[-7] * -0.0018899503327676891843;\n\
-ci += tmp_o[-7] * -0.0033824159510050025955;\n\
-ci += tmp_o[ 5] *  0.0038087520138944894631;\n\
-di += tmp_o[ 5] * -0.0076074873249766081919;\n\
-di += tmp_o[-4] *  0.0038087520138944894631;\n\
-ci += tmp_o[-4] *  0.0076074873249766081919;\n\
-ci += tmp_o[ 6] * -0.014952258337062199118;\n\
-di += tmp_o[ 6] *  0.031695087811525991431;\n\
-di += tmp_o[-5] *  0.014952258337062199118;\n\
-ci += tmp_o[-5] *  0.031695087811525991431;\n\
-ci += tmp_o[ 3] * -0.027219029917103486322;\n\
-di += tmp_o[ 3] *  0.061273359067811077843;\n\
-di += tmp_o[-2] * -0.027219029917103486322;\n\
-ci += tmp_o[-2] * -0.061273359067811077843;\n\
-di += tmp_o[-3] * -0.049137179673730286787;\n\
-ci += tmp_o[-3] * -0.14329423835127266284;\n\
-ci += tmp_o[ 2] * -0.051945838107881800736;\n\
-di += tmp_o[ 2] *  0.48135965125905339159;\n\
-di += tmp_o[-1] *  0.051945838107881800736;\n\
-ci += tmp_o[-1] *  0.48135965125905339159;\n\
-ci += tmp_o[ 4] *  0.049137179673730286787;\n\
-di += tmp_o[ 4] * -0.14329423835127266284;\n\
-ci += tmp_o[ 1] *  0.36444189483617893676;\n\
-di += tmp_o[ 1] * -0.77718575169962802862;\n\
-di += tmp_o[ 0] *  0.36444189483617893676;\n\
-ci += tmp_o[ 0] *  0.77718575169962802862;\n\
+tmp = tmp + j2*(3*FILTER_WIDTH+1) + FILTER_WIDTH/2+2*i2;\n\
+ci += tmp[ 7] * -0.00030292051472413308126;\n\
+di += tmp[ 7] *  0.00054213233180001068935;\n\
+di += tmp[-6] * -0.00030292051472413308126;\n\
+ci += tmp[-6] * -0.00054213233180001068935;\n\
+ci += tmp[ 8] *  0.0018899503327676891843;\n\
+di += tmp[ 8] * -0.0033824159510050025955;\n\
+di += tmp[-7] * -0.0018899503327676891843;\n\
+ci += tmp[-7] * -0.0033824159510050025955;\n\
+ci += tmp[ 5] *  0.0038087520138944894631;\n\
+di += tmp[ 5] * -0.0076074873249766081919;\n\
+di += tmp[-4] *  0.0038087520138944894631;\n\
+ci += tmp[-4] *  0.0076074873249766081919;\n\
+ci += tmp[ 6] * -0.014952258337062199118;\n\
+di += tmp[ 6] *  0.031695087811525991431;\n\
+di += tmp[-5] *  0.014952258337062199118;\n\
+ci += tmp[-5] *  0.031695087811525991431;\n\
+ci += tmp[ 3] * -0.027219029917103486322;\n\
+di += tmp[ 3] *  0.061273359067811077843;\n\
+di += tmp[-2] * -0.027219029917103486322;\n\
+ci += tmp[-2] * -0.061273359067811077843;\n\
+di += tmp[-3] * -0.049137179673730286787;\n\
+ci += tmp[-3] * -0.14329423835127266284;\n\
+ci += tmp[ 2] * -0.051945838107881800736;\n\
+di += tmp[ 2] *  0.48135965125905339159;\n\
+di += tmp[-1] *  0.051945838107881800736;\n\
+ci += tmp[-1] *  0.48135965125905339159;\n\
+ci += tmp[ 4] *  0.049137179673730286787;\n\
+di += tmp[ 4] * -0.14329423835127266284;\n\
+ci += tmp[ 1] *  0.36444189483617893676;\n\
+di += tmp[ 1] * -0.77718575169962802862;\n\
+di += tmp[ 0] *  0.36444189483617893676;\n\
+ci += tmp[ 0] *  0.77718575169962802862;\n\
 \
 \
 out[(jg*(2*n)+ig)]=ci;\n\
@@ -151,9 +151,10 @@ out[(jg*(2*n)+ig+n)]=di;\n\
 char * syn1d_program="\
 #define FILTER_WIDTH 16\n\
 #pragma OPENCL EXTENSION cl_khr_fp64: enable \n\
-__kernel void syn1dKernel_d(size_t n, size_t ndat, __global const double *psi, __global double *out, __local double tmp_1[], __local double tmp_2[]){\n\
+__kernel void syn1dKernel_d(size_t n, size_t ndat, __global const double *psi, __global double *out, __local double tmp_1[]){\n\
 size_t ig = get_global_id(0);\n\
 size_t jg = get_global_id(1);\n\
+{\n\
 const size_t i2 = get_local_id(0);\n\
 const size_t j2 = get_local_id(1);\n\
 size_t igt = get_group_id(0);\n\
@@ -166,66 +167,67 @@ ig  = igt == get_num_groups(0) - 1 ? ig - ( get_global_size(0) - n ) : ig;\n\
 igt = ig - i2 + j2;\n\
 jgt = jg - j2 + i2;\n\
 //If I'm on the outside, select a border element to load\n\
-ioff = i2*(2*FILTER_WIDTH+1) + j2;\n\
+ioff = i2*(4*FILTER_WIDTH+1) + j2;\n\
 if(j2 < FILTER_WIDTH/2)\n\
   { if (igt < FILTER_WIDTH/2)\n\
       { jb = n - ( FILTER_WIDTH/2 - j2 ); }\n\
     else { jb = igt - FILTER_WIDTH/2; }\n\
     tmp_1[ioff]=psi[jgt+jb*ndat];\n\
-    tmp_2[ioff]=psi[jgt+(jb+n)*ndat];\n\
+    tmp_1[ioff+2*FILTER_WIDTH]=psi[jgt+(jb+n)*ndat];\n\
   }\n\
 if (j2 >= FILTER_WIDTH/2)\n\
   { if (igt >= n - FILTER_WIDTH/2)\n\
       { jb = igt - n + FILTER_WIDTH/2; }\n\
     else { jb = igt + FILTER_WIDTH/2; }\n\
     tmp_1[ioff+FILTER_WIDTH]=psi[jgt+jb*ndat];\n\
-    tmp_2[ioff+FILTER_WIDTH]=psi[jgt+(jb+n)*ndat];\n\
+    tmp_1[ioff+3*FILTER_WIDTH]=psi[jgt+(jb+n)*ndat];\n\
   }\n\
 //Load the elements I am to calculate\n\
 tmp_1[ioff+FILTER_WIDTH/2]=psi[jgt+igt*ndat];\n\
-tmp_2[ioff+FILTER_WIDTH/2]=psi[jgt+(igt+n)*ndat];\n\
+tmp_1[ioff+2*FILTER_WIDTH+FILTER_WIDTH/2]=psi[jgt+(igt+n)*ndat];\n\
 barrier(CLK_LOCAL_MEM_FENCE);\n\
-ioff = j2*(2*FILTER_WIDTH+1) + FILTER_WIDTH/2+i2;\n\
-__local double * tmp_o_1 = tmp_1 + ioff;\n\
-__local double * tmp_o_2 = tmp_2 + ioff;\n\
+ioff = j2*(4*FILTER_WIDTH+1) + FILTER_WIDTH/2+i2;\n\
+tmp_1 = tmp_1 + ioff;\n\
+}\n\
 double se = 0.0;\n\
-double so = 0.0;\n\
-se += tmp_o_2[ 3] * -0.00030292051472413308126;\n\
-so += tmp_o_2[ 3] *  0.014952258337062199118;\n\
-se += tmp_o_1[ 3] * -0.00054213233180001068935;\n\
-so += tmp_o_1[ 3] *  0.031695087811525991431;\n\
-se += tmp_o_1[-4] *  0.0018899503327676891843;\n\
-se += tmp_o_2[-4] * -0.0033824159510050025955;\n\
-so += tmp_o_2[ 4] * -0.0018899503327676891843;\n\
-so += tmp_o_1[ 4] * -0.0033824159510050025955;\n\
-se += tmp_o_2[ 2] *  0.0038087520138944894631;\n\
-so += tmp_o_2[ 2] * -0.049137179673730286787;\n\
-se += tmp_o_1[ 2] *  0.0076074873249766081919;\n\
-so += tmp_o_1[ 2] * -0.14329423835127266284;\n\
-se += tmp_o_1[-3] * -0.014952258337062199118;\n\
-so += tmp_o_1[-3] * -0.00030292051472413308126;\n\
-se += tmp_o_2[ 1] * -0.027219029917103486322;\n\
-so += tmp_o_2[ 1] *  0.051945838107881800736;\n\
-se += tmp_o_2[-3] *  0.031695087811525991431;\n\
-so += tmp_o_2[-3] *  0.00054213233180001068935;\n\
-se += tmp_o_1[-2] *  0.049137179673730286787;\n\
-so += tmp_o_1[-2] *  0.0038087520138944894631;\n\
-se += tmp_o_1[-1] * -0.051945838107881800736;\n\
-so += tmp_o_1[-1] * -0.027219029917103486322;\n\
-se += tmp_o_1[ 1] * -0.061273359067811077843;\n\
-so += tmp_o_1[ 1] *  0.48135965125905339159;\n\
-se += tmp_o_2[-2] * -0.14329423835127266284;\n\
-so += tmp_o_2[-2] * -0.0076074873249766081919;\n\
-se += tmp_o_2[-1] *  0.48135965125905339159;\n\
-so += tmp_o_2[-1] *  0.061273359067811077843;\n\
-se += tmp_o_2[ 0] *  0.36444189483617893676;\n\
-so += tmp_o_2[ 0] * -0.77718575169962802862;\n\
-se += tmp_o_1[ 0] *  0.77718575169962802862;\n\
-so += tmp_o_1[ 0] *  0.36444189483617893676;\n\
-\
-\
+//double so = 0.0;\n\
+se += tmp_1[2*FILTER_WIDTH +  3] * -0.00030292051472413308126;\n\
+se += tmp_1[ 3] * -0.00054213233180001068935;\n\
+se += tmp_1[-4] *  0.0018899503327676891843;\n\
+se += tmp_1[2*FILTER_WIDTH + -4] * -0.0033824159510050025955;\n\
+se += tmp_1[2*FILTER_WIDTH +  2] *  0.0038087520138944894631;\n\
+se += tmp_1[ 2] *  0.0076074873249766081919;\n\
+se += tmp_1[-3] * -0.014952258337062199118;\n\
+se += tmp_1[2*FILTER_WIDTH + -3] *  0.031695087811525991431;\n\
+se += tmp_1[2*FILTER_WIDTH +  1] * -0.027219029917103486322;\n\
+se += tmp_1[-2] *  0.049137179673730286787;\n\
+se += tmp_1[-1] * -0.051945838107881800736;\n\
+se += tmp_1[2*FILTER_WIDTH + -2] * -0.14329423835127266284;\n\
+se += tmp_1[ 1] * -0.061273359067811077843;\n\
+se += tmp_1[2*FILTER_WIDTH + -1] *  0.48135965125905339159;\n\
+se += tmp_1[2*FILTER_WIDTH +  0] *  0.36444189483617893676;\n\
+se += tmp_1[ 0] *  0.77718575169962802862;\n\
 out[(jg*(2*n)+ig*2)]=se;\n\
-out[(jg*(2*n)+ig*2+1)]=so;\n\
+\
+se = 0.0;\n\
+se += tmp_1[2*FILTER_WIDTH +  3] *  0.014952258337062199118;\n\
+se += tmp_1[ 3] *  0.031695087811525991431;\n\
+se += tmp_1[2*FILTER_WIDTH +  4] * -0.0018899503327676891843;\n\
+se += tmp_1[ 4] * -0.0033824159510050025955;\n\
+se += tmp_1[2*FILTER_WIDTH +  2] * -0.049137179673730286787;\n\
+se += tmp_1[ 2] * -0.14329423835127266284;\n\
+se += tmp_1[-3] * -0.00030292051472413308126;\n\
+se += tmp_1[2*FILTER_WIDTH +  1] *  0.051945838107881800736;\n\
+se += tmp_1[2*FILTER_WIDTH + -3] *  0.00054213233180001068935;\n\
+se += tmp_1[-2] *  0.0038087520138944894631;\n\
+se += tmp_1[-1] * -0.027219029917103486322;\n\
+se += tmp_1[ 1] *  0.48135965125905339159;\n\
+se += tmp_1[2*FILTER_WIDTH + -2] * -0.0076074873249766081919;\n\
+se += tmp_1[2*FILTER_WIDTH + -1] *  0.061273359067811077843;\n\
+se += tmp_1[2*FILTER_WIDTH +  0] * -0.77718575169962802862;\n\
+se += tmp_1[ 0] *  0.36444189483617893676;\n\
+\
+out[(jg*(2*n)+ig*2+1)]=se;\n\
 };\n\
 __kernel void syn1dKernel_l(size_t n, size_t ndat, __global const float *psi, __global float *out, __local float tmp_1[], __local float tmp_2[]){\n\
 size_t ig = get_global_id(0);\n\
