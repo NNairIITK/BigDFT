d1dea3cfd76dfdeaace8eb26a935b52c303d2da3
Brice Videau <brice.videau@cea.fr>
2010-03-11 14:56:01 +0100

    Added OpenCL Stream library.

---
+++ src/OpenCL/Stream.c
@@ -0,0 +1,113 @@
+#include "Stream.h"
+
+cl_kernel void_kernel;
+
+cl_int oclInitStreams(cl_context * context){
+  cl_int ciErrNum = CL_SUCCESS;
+  const char * stream_program ="__kernel void voidKernel() {return;};";
+  cl_program streamProgram = clCreateProgramWithSource(*context,1,(const char**) &stream_program, NULL, &ciErrNum);
+  if( ciErrNum != CL_SUCCESS)
+    return ciErrNum;
+  ciErrNum = clBuildProgram(streamProgram, 0, NULL, "-cl-mad-enable", NULL, NULL);
+  if (ciErrNum != CL_SUCCESS)
+    return ciErrNum;
+  ciErrNum = CL_SUCCESS;
+  void_kernel=clCreateKernel(streamProgram,"voidKernel",&ciErrNum);
+  if (ciErrNum != CL_SUCCESS)
+    return ciErrNum;
+  ciErrNum = clReleaseProgram(streamProgram);
+  if (ciErrNum != CL_SUCCESS)
+    return ciErrNum;
+  return CL_SUCCESS;
+}
+
+cl_int oclEndStreams(){
+  return clReleaseKernel(void_kernel);
+}
+
+ocl_stream oclCreateStream(cl_command_queue command_queue, cl_int *errcode_ret) {
+  ocl_stream s = (struct _ocl_stream *)malloc(sizeof(struct _ocl_stream));
+  if(s == NULL) {
+    *errcode_ret = CL_OUT_OF_HOST_MEMORY;
+    return NULL;
+  }
+  size_t localWorksize[] = {32};
+  size_t globalWorkSize[] = {32};
+  *errcode_ret = clEnqueueNDRangeKernel(command_queue, void_kernel, 1, NULL, globalWorkSize, localWorksize, 0, NULL, &(s->event));
+  if(*errcode_ret != CL_SUCCESS) {
+    free(s);
+    return NULL;
+  }
+  *errcode_ret = clRetainCommandQueue(command_queue);
+  if(*errcode_ret != CL_SUCCESS) {
+    free(s);
+    return NULL;
+  }
+  s->command_queue = command_queue;
+  s->reference_counter = 1;
+  *errcode_ret = CL_SUCCESS;
+  return s;
+}
+
+cl_int oclReleaseStream( ocl_stream stream) {
+  cl_int errcode_ret;
+  errcode_ret = clReleaseCommandQueue(stream->command_queue);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  errcode_ret = clReleaseEvent(stream->event);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  stream->reference_counter--;
+  if( stream->reference_counter==0 )
+    free(stream);
+  return CL_SUCCESS;
+}
+
+cl_int oclEnstreamNDRangeKernel(ocl_stream stream, 
+                                     cl_kernel kernel, 
+                                     cl_uint work_dim, 
+                                     const size_t *global_work_offset,
+                                     const size_t *global_work_size,
+                                     const size_t *local_work_size)
+{
+  cl_int errcode_ret;
+  cl_event event;
+  errcode_ret = clEnqueueNDRangeKernel(stream->command_queue, kernel, work_dim, global_work_offset, global_work_size, local_work_size, 1, &(stream->event), &event);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  errcode_ret = clReleaseEvent(stream->event);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  stream->event = event;
+  return CL_SUCCESS;
+}
+
+cl_int oclStreamFinish(ocl_stream stream) {
+  return clEnqueueWaitForEvents(stream->command_queue, 1, &(stream->event));
+}
+
+cl_int oclEnstreamWriteBuffer(ocl_stream stream, cl_mem buffer, cl_bool blocking_write, size_t offset, size_t cb, const void *ptr) {
+  cl_int errcode_ret;
+  cl_event event;
+  errcode_ret = clEnqueueWriteBuffer(stream->command_queue, buffer, blocking_write, offset, cb, ptr, 1, &(stream->event), &(event));
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  errcode_ret = clReleaseEvent(stream->event);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  stream->event = event;
+  return CL_SUCCESS;
+}
+
+cl_int oclEnstreamReadBuffer(ocl_stream stream, cl_mem buffer, cl_bool blocking_read,  size_t offset, size_t cb, void *ptr) {
+  cl_int errcode_ret;
+  cl_event event;
+  errcode_ret = clEnqueueReadBuffer(stream->command_queue, buffer, blocking_read, offset, cb, ptr, 1, &(stream->event), &(event));
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  errcode_ret = clReleaseEvent(stream->event);
+  if( errcode_ret != CL_SUCCESS)
+    return errcode_ret;
+  stream->event = event;
+  return CL_SUCCESS;
+}
