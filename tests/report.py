b39d79a1e5c40ebab667ce8abee4c46f1c8565d8
Thierry Deutsch <deutsch@zedoaire>
2010-03-12 15:41:49 +0100

    Change report.py to generate a report into the toplevel directory.

--- tests/report.py
+++ tests/report.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: us-ascii -*-
 #----------------------------------------------------------------------------
-# Final report (read fldiff.report from fldiff.py)
-# Date: 06/03/2010
+# Final report (read *.report from fldiff.py)
+# Date: 12/03/2010
 #----------------------------------------------------------------------------
 
-import glob
+import fnmatch
+import os
 import re
 import sys
 
 #Regular expression
 re_discrepancy = re.compile("Max Discrepancy[^:]*:[ ]+([^ ]+)")
 
+def callback(pattern,dirname,names):
+    "Return the files given by the pattern"
+    for name in names:
+        if fnmatch.fnmatch(name,pattern):
+            files.append(os.path.join(dirname,name))
+
+#Liste of files
+files = []
+os.path.walk(".",callback,"*.report")
+
 Exit = 0
 print "Final report:"
-for file in glob.glob("*/*.report"):
-    ll = file.split("/")
-    dir = ll[0]
-    fic = "("+ll[1]+")"
+for file in files:
+    dir = os.path.dirname(file)
+    fic = "(%s)" % os.path.basename(file)
     #Max value
     max_discrepancy = float(open(file).readline())
     discrepancy = re_discrepancy.findall(open(file).read())
@@ -32,6 +42,6 @@ for file in glob.glob("*/*.report"):
             state = "%7.1e > (%7.1e)    failed" % (discrepancy,max_discrepancy)
             end = "\033[m"
             Exit = 1
-        print "%s%-13s %-17s %s%s" % (start,dir,fic,state,end)
+        print "%s%-23s %-17s %s%s" % (start,dir,fic,state,end)
 #Error code
 sys.exit(Exit)
