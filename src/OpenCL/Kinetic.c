a2175a79b0598bb19ed766daa215e1c37d9ddafc
Brice Videau <brice.videau@cea.fr>
2010-03-16 11:44:04 +0100

    Free condition working.

--- src/OpenCL/Kinetic.c
+++ src/OpenCL/Kinetic.c
@@ -54,6 +54,55 @@ y_out[jg*n + ig] = tmp2[j2*(32+1) + i2] + scale * conv;\n\
 x_out[jg*n + ig] = tmp_o[0];\n\
 };\n\
 \n\
+__kernel void kinetic1d_fKernel_d(size_t n, size_t ndat, double scale, __global const double * x_in, __global double * x_out, __global const double * y_in, __global double * y_out, __local double * tmp, __local double * tmp2) {\n\
+size_t ig = get_global_id(0);\n\
+size_t jg = get_global_id(1);\n\
+const size_t i2 = get_local_id(0);\n\
+const size_t j2 = get_local_id(1);\n\
+ptrdiff_t igt = get_group_id(0);\n\
+ptrdiff_t jgt = get_group_id(1);\n\
+size_t jb;\n\
+const size_t j2t = 2*j2 + i2/16;\n\
+const size_t i2t = i2 - 16 * (i2 / 16);\n\
+//if data are ill dimentioned last block recomputes part of the data\n\
+jg  = jgt == get_num_groups(1) - 1 ? jg - ( get_global_size(1) - ndat ) : jg;\n\
+ig  = igt == get_num_groups(0) - 1 ? ig - ( get_global_size(0) - n ) : ig;\n\
+igt = ig - i2 + j2t;\n\
+jgt = jg - j2 + i2t;\n\
+tmp2[i2t * (32 + 1) + j2t] = y_in[jgt+igt*ndat];\n\
+igt -= FILTER_WIDTH/2;\n\
+if ( igt < 0 ) \n\
+  tmp[i2t * (2 * FILTER_WIDTH + 1) + j2t] = 0.0;\n\
+else \n\
+  tmp[i2t * (2 * FILTER_WIDTH + 1) + j2t] = x_in[jgt + igt * ndat];\n\
+igt += FILTER_WIDTH;\n\
+if ( igt >= n ) \n\
+  tmp[i2t * (2 * FILTER_WIDTH + 1) + j2t + FILTER_WIDTH] = 0.0;\n\
+else\n\
+  tmp[i2t * (2 * FILTER_WIDTH + 1) + j2t + FILTER_WIDTH] = x_in[jgt +  igt * ndat];\n\
+barrier(CLK_LOCAL_MEM_FENCE);\n\
+__local double * tmp_o = tmp + j2*(2*32 + 1) + FILTER_WIDTH/2+i2;\n\
+double conv = 0.0;\n\
+conv += (tmp_o[14] + tmp_o[-14]) * -6.924474940639200152025730585882e-18;\n\
+conv += (tmp_o[13] + tmp_o[-13]) *  2.70800493626319438269856689037647576e-13;\n\
+conv += (tmp_o[12] + tmp_o[-12]) * -5.813879830282540547959250667e-11;\n\
+conv += (tmp_o[11] + tmp_o[-11]) * -1.05857055496741470373494132287e-8;\n\
+conv += (tmp_o[10] + tmp_o[-10]) * -3.7230763047369275848791496973044e-7;\n\
+conv += (tmp_o[ 9] + tmp_o[ -9]) *  2.0904234952920365957922889447361e-6;\n\
+conv += (tmp_o[ 8] + tmp_o[ -8]) * -0.2398228524507599670405555359023135e-4;\n\
+conv += (tmp_o[ 7] + tmp_o[ -7]) *  0.45167920287502235349480037639758496e-3;\n\
+conv += (tmp_o[ 6] + tmp_o[ -6]) * -0.409765689342633823899327051188315485e-2;\n\
+conv += (tmp_o[ 5] + tmp_o[ -5]) *  0.02207029188482255523789911295638968409e0;\n\
+conv += (tmp_o[ 4] + tmp_o[ -4]) * -0.0822663999742123340987663521e0;\n\
+conv += (tmp_o[ 3] + tmp_o[ -3]) *  0.2371780582153805636239247476e0;\n\
+conv += (tmp_o[ 2] + tmp_o[ -2]) * -0.6156141465570069496314853949e0;\n\
+conv += (tmp_o[ 1] + tmp_o[ -1]) *  2.2191465938911163898794546405e0;\n\
+conv +=  tmp_o[ 0]             * -3.5536922899131901941296809374e0;\n\
+//y_out[jg*n + ig] = y_in[jg + ig*ndat] - scale * conv;\n\
+y_out[jg*n + ig] = tmp2[j2*(32+1) + i2] + scale * conv;\n\
+x_out[jg*n + ig] = tmp_o[0];\n\
+};\n\
+\n\
 ";
 
 inline void kinetic_generic(cl_kernel kernel, cl_command_queue *command_queue, cl_uint *n, cl_uint *ndat, double *scale, cl_mem *x_in, cl_mem *x_out, cl_mem *y_in, cl_mem *y_out) {
@@ -209,6 +258,7 @@ inline void kinetic_k_generic(cl_kernel kernel, cl_command_queue *command_queue,
 
 
 cl_kernel kinetic1d_kernel_d;
+cl_kernel kinetic1d_f_kernel_d;
 cl_kernel kinetic_k1d_kernel_d;
 
 void build_kinetic_kernels(cl_context * context){
@@ -227,6 +277,9 @@ void build_kinetic_kernels(cl_context * context){
     }
     ciErrNum = CL_SUCCESS;
     kinetic1d_kernel_d=clCreateKernel(kinetic1dProgram,"kinetic1dKernel_d",&ciErrNum);
+    oclErrorCheck(ciErrNum,"Failed to create kernel!");
+    ciErrNum = CL_SUCCESS;
+    kinetic1d_f_kernel_d=clCreateKernel(kinetic1dProgram,"kinetic1d_fKernel_d",&ciErrNum);
     oclErrorCheck(ciErrNum,"Failed to create kernel!");
     ciErrNum = clReleaseProgram(kinetic1dProgram);
     oclErrorCheck(ciErrNum,"Failed to release program!");
@@ -291,18 +344,30 @@ void FC_FUNC_(kinetic_d_generic,KINETIC_D_GENERIC)(cl_command_queue *command_que
   cl_uint n1 = dimensions[0] * 2;             
   cl_uint n2 = dimensions[1] * 2;
   cl_uint n3 = dimensions[2] * 2;
-  if( !periodic[0] ) n1 += 2*7 + 15;
-  if( !periodic[1] ) n2 += 2*7 + 15;
-  if( !periodic[2] ) n3 += 2*7 + 15;
+  if( !periodic[0] ) n1 += 2*7;
+  if( !periodic[1] ) n2 += 2*7;
+  if( !periodic[2] ) n3 += 2*7;
   scale = -0.5 / ( h[2] * h[2] );
   ng = n1 * n2;
-  kinetic_generic(kinetic1d_kernel_d, command_queue, &n3, &ng, &scale, x, work_x, y, work_y);
+  if( periodic[2] ) {
+    kinetic_generic(kinetic1d_kernel_d, command_queue, &n3, &ng, &scale, x, work_x, y, work_y);
+  } else {
+    kinetic_generic(kinetic1d_f_kernel_d, command_queue, &n3, &ng, &scale, x, work_x, y, work_y);
+  }
   scale = -0.5 / ( h[1] * h[1] );
   ng = n1 * n3;
-  kinetic_generic(kinetic1d_kernel_d, command_queue, &n2, &ng, &scale, work_x, x, work_y, y);
+  if( periodic[1] ) {
+    kinetic_generic(kinetic1d_kernel_d, command_queue, &n2, &ng, &scale, work_x, x, work_y, y);
+  } else {
+    kinetic_generic(kinetic1d_f_kernel_d, command_queue, &n2, &ng, &scale, work_x, x, work_y, y);
+  }
   scale = -0.5 / ( h[0] * h[0] );
   ng = n2 * n3;
-  kinetic_generic(kinetic1d_kernel_d, command_queue, &n1, &ng, &scale, x, work_x, y, work_y);
+  if( periodic[0] ) {
+    kinetic_generic(kinetic1d_kernel_d, command_queue, &n1, &ng, &scale, x, work_x, y, work_y);
+  } else {
+    kinetic_generic(kinetic1d_f_kernel_d, command_queue, &n1, &ng, &scale, x, work_x, y, work_y);
+  }
 }
 
 void FC_FUNC_(kinetic_d,KINETIC_D)(cl_command_queue *command_queue, cl_uint *n1, cl_uint *n2, cl_uint *n3, double *h, cl_mem *x, cl_mem *y, cl_mem *work_x, cl_mem *work_y) {
@@ -329,5 +394,6 @@ void FC_FUNC_(kinetic1d_d,KINETIC1D_D)(cl_command_queue *command_queue, cl_uint 
 
 void clean_kinetic_kernels(){
   clReleaseKernel(kinetic1d_kernel_d);
+  clReleaseKernel(kinetic1d_f_kernel_d);
   clReleaseKernel(kinetic_k1d_kernel_d);
 }
