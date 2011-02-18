61963ff2a71ffbbf8d331fe657db72e092cdff08
Stefan Goedecker <stefan@phys-sesame>
2011-02-18 14:37:30 +0100

    OpenMP bugs in combined_grow_optim.f90 and razero corrected

--- src/wavelib/razero.f90
+++ src/wavelib/razero.f90
@@ -11,13 +11,9 @@ subroutine razero(n,x)
   real(kind=8), intent(out) :: x(n)
   !Local variables
   integer :: i
-!$omp parallel default(private) shared(n,x)
-!$omp do 
   do i=1,n
      x(i)=0.d0
   end do
-!$omp enddo
-!$omp end parallel
 END SUBROUTINE razero
 !!***
 
