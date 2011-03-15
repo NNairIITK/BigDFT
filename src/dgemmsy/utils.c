56f36e5f868c311e7aba54e691d264aa8b9488d3
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2011-03-15 16:16:22 +0100

    Comments for c and cu files

--- src/dgemmsy/utils.c
+++ src/dgemmsy/utils.c
@@ -1,7 +1,14 @@
-// Various functions used during experiences
-//
-// Copyright (c) 2010, Commissariat a l'Energie Atomique
-// Eric Bainville, Mar 2010
+//! @file
+//! Various functions used during experiences (dgemmsy)
+//!
+//! @author
+//!    Copyright (C) 2009-2011 BigDFT group 
+//!    This file is distributed under the terms of the
+//!    GNU General Public License, see ~/COPYING file
+//!    or http://www.gnu.org/copyleft/gpl.txt .
+//!    For the list of contributors, see ~/AUTHORS 
+//! Eric Bainville, Mar 2010
+
 
 #include <stdlib.h>
 #include <string.h>
