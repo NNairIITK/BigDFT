a3963eb2d31240a40aba318ac3bbf79c3ce693a3
Brice Videau <brice.videau@cea.fr>
2011-06-10 15:53:35 +0200

    More optims.

--- src/OpenCL/Kinetic_Generator.cpp
+++ src/OpenCL/Kinetic_Generator.cpp
@@ -17,7 +17,41 @@ static void generate_header(std::stringstream &program){
 #elif defined (cl_amd_fp64)\n\
 #pragma OPENCL EXTENSION cl_amd_fp64: enable \n\
 #endif\n\
-#define FILTER_WIDTH "<<FILTER_WIDTH<<"\n";
+#define FILTER_WIDTH "<<FILTER_WIDTH<<"\n\
+#define FILT1_14 -6.924474940639200152025730585882e-18\n\
+#define FILT1_13  2.70800493626319438269856689037647576e-13\n\
+#define FILT1_12 -5.813879830282540547959250667e-11\n\
+#define FILT1_11 -1.05857055496741470373494132287e-8\n\
+#define FILT1_10 -3.7230763047369275848791496973044e-7\n\
+#define FILT1_9   2.0904234952920365957922889447361e-6\n\
+#define FILT1_8  -0.2398228524507599670405555359023135e-4\n\
+#define FILT1_7   0.45167920287502235349480037639758496e-3\n\
+#define FILT1_6  -0.409765689342633823899327051188315485e-2\n\
+#define FILT1_5   0.02207029188482255523789911295638968409e0\n\
+#define FILT1_4  -0.0822663999742123340987663521e0\n\
+#define FILT1_3   0.2371780582153805636239247476e0\n\
+#define FILT1_2  -0.6156141465570069496314853949e0\n\
+#define FILT1_1   2.2191465938911163898794546405e0\n\
+#define FILT1_0  -3.5536922899131901941296809374e0\n";
+}
+
+static void generate_filters(std::stringstream &program){
+  program<<"#define filter1(tt,tmp) \
+tt = fma(tmp[14] + tmp[-14], FILT1_14, tt);\
+tt = fma(tmp[13] + tmp[-13], FILT1_13, tt);\
+tt = fma(tmp[12] + tmp[-12], FILT1_12, tt);\
+tt = fma(tmp[11] + tmp[-11], FILT1_11, tt);\
+tt = fma(tmp[10] + tmp[-10], FILT1_10, tt);\
+tt = fma(tmp[ 9] + tmp[ -9], FILT1_9 , tt);\
+tt = fma(tmp[ 8] + tmp[ -8], FILT1_8 , tt);\
+tt = fma(tmp[ 7] + tmp[ -7], FILT1_7 , tt);\
+tt = fma(tmp[ 6] + tmp[ -6], FILT1_6 , tt);\
+tt = fma(tmp[ 5] + tmp[ -5], FILT1_5 , tt);\
+tt = fma(tmp[ 4] + tmp[ -4], FILT1_4 , tt);\
+tt = fma(tmp[ 3] + tmp[ -3], FILT1_3 , tt);\
+tt = fma(tmp[ 2] + tmp[ -2], FILT1_2 , tt);\
+tt = fma(tmp[ 1] + tmp[ -1], FILT1_1 , tt);\
+tt = fma(tmp[ 0]           , FILT1_0 , tt);\n";
 }
 
 static void generate_kinetic1dKernel(std::stringstream &program, struct bigdft_device_infos * infos){
@@ -54,21 +88,7 @@ static void generate_kinetic1dKernel(std::stringstream &program, struct bigdft_d
   barrier(CLK_LOCAL_MEM_FENCE);\n\
   __local double * tmp_o = tmp + j2*(2*32 + 1) + FILTER_WIDTH/2+i2;\n\
   double conv = 0.0;\n\
-  conv += (tmp_o[14] + tmp_o[-14]) * -6.924474940639200152025730585882e-18;\n\
-  conv += (tmp_o[13] + tmp_o[-13]) *  2.70800493626319438269856689037647576e-13;\n\
-  conv += (tmp_o[12] + tmp_o[-12]) * -5.813879830282540547959250667e-11;\n\
-  conv += (tmp_o[11] + tmp_o[-11]) * -1.05857055496741470373494132287e-8;\n\
-  conv += (tmp_o[10] + tmp_o[-10]) * -3.7230763047369275848791496973044e-7;\n\
-  conv += (tmp_o[ 9] + tmp_o[ -9]) *  2.0904234952920365957922889447361e-6;\n\
-  conv += (tmp_o[ 8] + tmp_o[ -8]) * -0.2398228524507599670405555359023135e-4;\n\
-  conv += (tmp_o[ 7] + tmp_o[ -7]) *  0.45167920287502235349480037639758496e-3;\n\
-  conv += (tmp_o[ 6] + tmp_o[ -6]) * -0.409765689342633823899327051188315485e-2;\n\
-  conv += (tmp_o[ 5] + tmp_o[ -5]) *  0.02207029188482255523789911295638968409e0;\n\
-  conv += (tmp_o[ 4] + tmp_o[ -4]) * -0.0822663999742123340987663521e0;\n\
-  conv += (tmp_o[ 3] + tmp_o[ -3]) *  0.2371780582153805636239247476e0;\n\
-  conv += (tmp_o[ 2] + tmp_o[ -2]) * -0.6156141465570069496314853949e0;\n\
-  conv += (tmp_o[ 1] + tmp_o[ -1]) *  2.2191465938911163898794546405e0;\n\
-  conv +=  tmp_o[ 0]             * -3.5536922899131901941296809374e0;\n\
+  filter1(conv, tmp_o);\n\
   //y_out[jg*n + ig] = y_in[jg + ig*ndat] - scale * conv;\n\
   y_out[jg*n + ig] = tmp2[j2*(32+1) + i2] + scale * conv;\n\
   x_out[jg*n + ig] = tmp_o[0];\n\
@@ -109,21 +129,7 @@ static void generate_kinetic1d_fKernel(std::stringstream &program, struct bigdft
   barrier(CLK_LOCAL_MEM_FENCE);\n\
   __local double * tmp_o = tmp + j2*(2*32 + 1) + FILTER_WIDTH/2+i2;\n\
   double conv = 0.0;\n\
-  conv += (tmp_o[14] + tmp_o[-14]) * -6.924474940639200152025730585882e-18;\n\
-  conv += (tmp_o[13] + tmp_o[-13]) *  2.70800493626319438269856689037647576e-13;\n\
-  conv += (tmp_o[12] + tmp_o[-12]) * -5.813879830282540547959250667e-11;\n\
-  conv += (tmp_o[11] + tmp_o[-11]) * -1.05857055496741470373494132287e-8;\n\
-  conv += (tmp_o[10] + tmp_o[-10]) * -3.7230763047369275848791496973044e-7;\n\
-  conv += (tmp_o[ 9] + tmp_o[ -9]) *  2.0904234952920365957922889447361e-6;\n\
-  conv += (tmp_o[ 8] + tmp_o[ -8]) * -0.2398228524507599670405555359023135e-4;\n\
-  conv += (tmp_o[ 7] + tmp_o[ -7]) *  0.45167920287502235349480037639758496e-3;\n\
-  conv += (tmp_o[ 6] + tmp_o[ -6]) * -0.409765689342633823899327051188315485e-2;\n\
-  conv += (tmp_o[ 5] + tmp_o[ -5]) *  0.02207029188482255523789911295638968409e0;\n\
-  conv += (tmp_o[ 4] + tmp_o[ -4]) * -0.0822663999742123340987663521e0;\n\
-  conv += (tmp_o[ 3] + tmp_o[ -3]) *  0.2371780582153805636239247476e0;\n\
-  conv += (tmp_o[ 2] + tmp_o[ -2]) * -0.6156141465570069496314853949e0;\n\
-  conv += (tmp_o[ 1] + tmp_o[ -1]) *  2.2191465938911163898794546405e0;\n\
-  conv +=  tmp_o[ 0]             * -3.5536922899131901941296809374e0;\n\
+  filter1(conv, tmp_o);\n\
   //y_out[jg*n + ig] = y_in[jg + ig*ndat] - scale * conv;\n\
   y_out[jg*n + ig] = tmp2[j2*(32+1) + i2] + scale * conv;\n\
   x_out[jg*n + ig] = tmp_o[0];\n\
@@ -135,6 +141,7 @@ extern "C" char* generate_kinetic_program(struct bigdft_device_infos * infos){
   std::stringstream program;
 
   generate_header(program);
+  generate_filters(program);
   generate_kinetic1dKernel(program, infos);
   generate_kinetic1d_fKernel(program, infos);
 
