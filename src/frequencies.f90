7ac020b68ed07116e4c86f29dddde2207063a238
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2011-09-01 14:57:58 +0200

    Add Hartree (0) and Hartree-Fock (100) in the manual

--- src/frequencies.f90
+++ src/frequencies.f90
@@ -140,7 +140,7 @@ program frequencies
    !Message
    if (iproc == 0) then
       write(*,'(1x,a,i0,a,i0,a)') '=F=> There are ', imoves, ' moves already calculated over ', &
-      n_order*3*atoms%nat,' frequencies.'
+         &   n_order*3*atoms%nat,' frequencies.'
       write(*,*)
    end if
 
@@ -151,7 +151,7 @@ program frequencies
    else
       call call_bigdft(nproc,iproc,atoms,rxyz,inputs,etot,fxyz,fnoise,rst,infocode)
       call frequencies_write_restart(iproc,0,0,0,rxyz,etot,fxyz,&
-      n_order=n_order,freq_step=freq_step,amu=atoms%amu)
+         &   n_order=n_order,freq_step=freq_step,amu=atoms%amu)
       moves(:,0) = .true.
       call restart_inputs(inputs)
    end if
@@ -162,7 +162,7 @@ program frequencies
       write(*,'(1x,a,19x,a)') 'Final values of the Forces for each atom'
       do iat=1,atoms%nat
          write(*,'(1x,i5,1x,a6,3(1x,1pe12.5))') &
-         iat,trim(atoms%atomnames(atoms%iatype(iat))),(fxyz(i+3*(iat-1)),i=1,3)
+            &   iat,trim(atoms%atomnames(atoms%iatype(iat))),(fxyz(i+3*(iat-1)),i=1,3)
       end do
    end if
 
@@ -214,7 +214,7 @@ program frequencies
             rpos=rxyz
             if (iproc == 0) then
                write(*,"(1x,a,i0,a,a,a,1pe20.10,a)") &
-               '=F Move the atom ',iat,' in the direction ',cc,' by ',dd,' bohr'
+                  &   '=F Move the atom ',iat,' in the direction ',cc,' by ',dd,' bohr'
             end if
             if (atoms%geocode == 'P') then
                rpos(i,iat)=modulo(rxyz(i,iat)+dd,alat)
@@ -314,7 +314,7 @@ program frequencies
             ity=atoms%iatype(iat)
             do j=1,3
                write(15,'(1x,a,1x,100(1pe20.10))') &
-               atoms%atomnames(ity),vector_l(3*(iat-1)+j,iperm(i))
+                  &   atoms%atomnames(ity),vector_l(3*(iat-1)+j,iperm(i))
             end do
          end do
          !Blank line
@@ -342,13 +342,13 @@ program frequencies
       total_energy=energies(1,0)+vibrational_energy
       write(*,'(1x,a,81("="))') '=F '
       write(*,'(1x,a,f13.2,1x,a,5x,1pe20.10,1x,a)') &
-      '=F: Zero-point energy   =', zpenergy*Ha_cmm1, 'cm-1',zpenergy,'Hartree'
+         &   '=F: Zero-point energy   =', zpenergy*Ha_cmm1, 'cm-1',zpenergy,'Hartree'
       write(*,'(1x,a,1pe22.10,a,0pf5.1,a)') &
-      '=F: Vibrational entropy =', vibrational_entropy,' at ',Temperature,'K'
+         &   '=F: Vibrational entropy =', vibrational_entropy,' at ',Temperature,'K'
       write(*,'(1x,a,f13.2,1x,a,5x,1pe20.10,1x,a,0pf5.1,a)') &
-      '=F: Vibrational  energy =', vibrational_energy*Ha_cmm1, 'cm-1',vibrational_energy,'Hartree at ',Temperature,'K'
+         &   '=F: Vibrational  energy =', vibrational_energy*Ha_cmm1, 'cm-1',vibrational_energy,'Hartree at ',Temperature,'K'
       write(*,'(1x,a,1pe22.10,1x,a,0pf5.1,a)') &
-      '=F: Total energy        =', total_energy,'Hartree at ',Temperature,'K'
+         &   '=F: Total energy        =', total_energy,'Hartree at ',Temperature,'K'
    end if
 
    !Deallocations
@@ -488,8 +488,8 @@ program frequencies
          return
       else
          if (steps(1) /= freq_step(1) .or. &
-         steps(2) /= freq_step(2) .or. &
-         steps(3) /= freq_step(3)) then
+            &   steps(2) /= freq_step(2) .or. &
+            &   steps(3) /= freq_step(3)) then
          if (iproc == 0) write(*,freq_form) 'The step to calculate frequencies is not the same: stop.'
          stop
       end if
@@ -551,7 +551,7 @@ subroutine frequencies_write_restart(iproc,km,i,iat,rxyz,etot,fxyz,n_order,freq_
    integer, parameter :: iunit = 15
 
    if (km == 0 .and. &
-   .not.(present(n_order).and.present(freq_step).and.present(amu))) then
+      &   .not.(present(n_order).and.present(freq_step).and.present(amu))) then
    if (iproc == 0) write(*,*) "Bug for use of frequencies_write_restart"
    stop
 end if
