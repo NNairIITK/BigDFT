a7195791289c1b1a26ec41da34d1812eb5097cd6
Luigi Genovese <luigi.genovese@cea.fr>
2011-10-06 19:20:07 +0200

    Minima Hopping to be tested again

--- src/global.f90
+++ src/global.f90
@@ -21,7 +21,7 @@ program MINHOP
   logical :: newmin,CPUcheck,occured
   character(len=20) :: unitsp,units,atmn
   character(len=80) :: line
-  type(atoms_data) :: atoms
+  type(atoms_data) :: atoms,md_atoms
   type(input_variables) :: inputs_opt, inputs_md
   type(restart_objects) :: rst
   integer, parameter :: npminx=200
@@ -107,21 +107,35 @@ program MINHOP
   endif
   close(12)
 
+  call standard_inputfile_names(inputs_opt,'input')
+  call standard_inputfile_names(inputs_md,'mdinput')
+
   call read_atomic_file('poscur',iproc,atoms,pos)
+
   !Read input parameters for geometry optimization 
-  call default_input_variables(inputs_opt)
-  call dft_input_variables_new(iproc,'input.dft',inputs_opt)
-  call geopt_input_variables('input.geopt',inputs_opt)
-  call kpt_input_variables(iproc,'input.kpt',inputs_opt,atoms)
+  call read_input_parameters(iproc,inputs_opt,atoms,pos)
+
+!!$  call default_input_variables(inputs_opt)
+!!$  call dft_input_variables_new(iproc,'input.dft',inputs_opt)
+!!$  call geopt_input_variables('input.geopt',inputs_opt)
+!!$  call kpt_input_variables(iproc,'input.kpt',inputs_opt,atoms)
 
   !read input parameters for molecular dynamics
-  call default_input_variables(inputs_md)
-  call dft_input_variables_new(iproc,'mdinput.dft',inputs_md)
-  call geopt_input_variables('mdinput.geopt',inputs_md)
-  call kpt_input_variables(iproc,'input.kpt',inputs_md,atoms)
+  call read_input_parameters(iproc,inputs_md,md_atoms,pos)
+!!$  call default_input_variables(inputs_md)
+!!$  call dft_input_variables_new(iproc,'mdinput.dft',inputs_md)
+!!$  call geopt_input_variables('mdinput.geopt',inputs_md)
+!!$  call kpt_input_variables(iproc,'input.kpt',inputs_md,atoms)
+
+
+  !use only the atoms structure for the run
+  call init_atomic_values(iproc,md_atoms,inputs_md%ixc)
+  call deallocate_atoms(md_atoms,subname) 
 
   ! Read associated pseudo files. Based on the inputs_opt set
   call init_atomic_values(iproc, atoms, inputs_opt%ixc)
+
+
 
 
   do iat=1,atoms%nat
@@ -1219,6 +1233,7 @@ END SUBROUTINE localdist
 
 
 subroutine torque(nat,rxyz,vxyz)
+  use module_base, only: gp
   implicit real*8 (a-h,o-z)
   dimension rxyz(3,nat),vxyz(3,nat)
 
@@ -1757,174 +1772,174 @@ if (option.ne.1 .and. option.ne.2) stop "Wrong option in fixfrag_refvels"
 
 !Calculate number of fragments and fragmentlist of the atoms
 do
-  nfragold=nfrag
-  do iat=1,nat                !Check the first atom that isn't part of a cluster yet
-    if(fragarr(iat)==0) then
-      nfrag=nfrag+1
-      fragarr(iat)=nfrag
-      exit
-    endif
-  enddo
-  if (nfragold==nfrag) exit
+   nfragold=nfrag
+   do iat=1,nat                !Check the first atom that isn't part of a cluster yet
+      if(fragarr(iat)==0) then
+         nfrag=nfrag+1
+         fragarr(iat)=nfrag
+         exit
+      endif
+   enddo
+   if (nfragold==nfrag) exit
 7000 niter=.false.
-  do iat=1,nat                !Check if all the other atoms are part of the current cluster
-    do jat=1,nat
-    bondlength=rcov(iat)+rcov(jat)
-    if(nfrag==fragarr(iat) .AND. jat.ne.iat .AND. fragarr(jat)==0) then
-         dist=(pos(1,iat)-pos(1,jat))**2+(pos(2,iat)-pos(2,jat))**2+(pos(3,iat)-pos(3,jat))**2
-         if(dist<(bfactor*bondlength)**2) then
-         fragarr(jat)=nfrag
-         niter=.true.
+   do iat=1,nat                !Check if all the other atoms are part of the current cluster
+      do jat=1,nat
+         bondlength=rcov(iat)+rcov(jat)
+         if(nfrag==fragarr(iat) .AND. jat.ne.iat .AND. fragarr(jat)==0) then
+            dist=(pos(1,iat)-pos(1,jat))**2+(pos(2,iat)-pos(2,jat))**2+(pos(3,iat)-pos(3,jat))**2
+            if(dist<(bfactor*bondlength)**2) then
+               fragarr(jat)=nfrag
+               niter=.true.
+            endif
          endif
-    endif
-    enddo
-  enddo
-  if(niter) then
-  goto 7000
-  endif
+      enddo
+   enddo
+   if(niter) then
+      goto 7000
+   endif
 enddo
 
 
 !   if(iproc==0) write(*,*) '#nfrag=',nfrag
-   occured=.false.
+occured=.false.
 if(nfrag.ne.1) then          !"if there is fragmentation..."
    occured=.true.
    if(iproc==0) write(*,*) '#FIX: Number of Fragments counted with option', nfrag,option
 
-if (option==1) then !OPTION=1, FIX FRAGMENTATION
-!   if(nfrag.ne.1) then          !"if there is fragmentation..."
-   
-   !Find out which fragment is the main cluster
-   allocate(fragcount(nfrag))
-   fragcount=0
-   do ifrag=1,nfrag
-      do iat=1,nat
-         if(fragarr(iat)==ifrag) then
-         fragcount(ifrag)=fragcount(ifrag)+1
-         endif
+   if (option==1) then !OPTION=1, FIX FRAGMENTATION
+      !   if(nfrag.ne.1) then          !"if there is fragmentation..."
+
+      !Find out which fragment is the main cluster
+      allocate(fragcount(nfrag))
+      fragcount=0
+      do ifrag=1,nfrag
+         do iat=1,nat
+            if(fragarr(iat)==ifrag) then
+               fragcount(ifrag)=fragcount(ifrag)+1
+            endif
+         enddo
       enddo
-   enddo
-   nmax=maxloc(fragcount(:))
-   if(iproc==0) write(*,*) '#FIX: The main Fragment index is', nmax(1)
+      nmax=maxloc(fragcount(:))
+      if(iproc==0) write(*,*) '#FIX: The main Fragment index is', nmax(1)
 
-   !Find the minimum distance between the clusters
-   do ifrag=1,nfrag
-      mindist=1.d100
-      if(ifrag.ne.nmax(1)) then
-           do iat=1,nat
-           if(fragarr(iat)==ifrag) then
-             do jat=1,nat
-               if(fragarr(jat)==nmax(1)) then
-               dist=(pos(1,iat)-pos(1,jat))**2+(pos(2,iat)-pos(2,jat))**2+(pos(3,iat)-pos(3,jat))**2
-                  if(dist<mindist**2) then
-                  mindist=sqrt(dist)
-                  imin(1)=jat  !Atom with minimal distance in main fragment
-                  imin(2)=iat   !Atom with minimal distance in fragment ifrag
-                  endif
-                endif
-              enddo
-             endif
-           enddo
+      !Find the minimum distance between the clusters
+      do ifrag=1,nfrag
+         mindist=1.d100
+         if(ifrag.ne.nmax(1)) then
+            do iat=1,nat
+               if(fragarr(iat)==ifrag) then
+                  do jat=1,nat
+                     if(fragarr(jat)==nmax(1)) then
+                        dist=(pos(1,iat)-pos(1,jat))**2+(pos(2,iat)-pos(2,jat))**2+(pos(3,iat)-pos(3,jat))**2
+                        if(dist<mindist**2) then
+                           mindist=sqrt(dist)
+                           imin(1)=jat  !Atom with minimal distance in main fragment
+                           imin(2)=iat   !Atom with minimal distance in fragment ifrag
+                        endif
+                     endif
+                  enddo
+               endif
+            enddo
 
-        if (iproc == 0) then
-           write(444,*) nat, 'atomic '
-           write(444,*) 'A fragmented configuration ',imin(1),imin(2)
-           do iat=1,nat
-              write(444,'(a5,3(e15.7),l1)') ' Mg  ',pos(1,iat),pos(2,iat),pos(3,iat)
-           enddo
-        endif
+            if (iproc == 0) then
+               write(444,*) nat, 'atomic '
+               write(444,*) 'A fragmented configuration ',imin(1),imin(2)
+               do iat=1,nat
+                  write(444,'(a5,3(e15.7),l1)') ' Mg  ',pos(1,iat),pos(2,iat),pos(3,iat)
+               enddo
+            endif
 
 
-         vec(:)=pos(:,imin(1))-pos(:,imin(2))
-         bondlength=rcov(imin(1))+rcov(imin(2))
+            vec(:)=pos(:,imin(1))-pos(:,imin(2))
+            bondlength=rcov(imin(1))+rcov(imin(2))
             do iat=1,nat        !Move fragments back towards the main fragment 
-                if(fragarr(iat)==ifrag) then
-                pos(:,iat)=pos(:,iat)+vec(:)*((mindist-1.5d0*bondlength)/mindist)
-                fragarr(iat)=nmax(1)
-                endif
+               if(fragarr(iat)==ifrag) then
+                  pos(:,iat)=pos(:,iat)+vec(:)*((mindist-1.5d0*bondlength)/mindist)
+                  fragarr(iat)=nmax(1)
+               endif
             enddo
 
 
+         endif
+      enddo
+      deallocate(fragcount)
+      if(iproc==0) write(*,*) '#FIX: Fragmentation fixed! Keep on hopping...'
+      if (iproc == 0) then
+         write(444,*) nat, 'atomic '
+         write(444,*) ' fixed configuration '
+         do iat=1,nat
+            write(444,'(a5,3(e15.7),l1)') ' Mg  ',pos(1,iat),pos(2,iat),pos(3,iat)
+         enddo
       endif
-   enddo
-   deallocate(fragcount)
-   if(iproc==0) write(*,*) '#FIX: Fragmentation fixed! Keep on hopping...'
-        if (iproc == 0) then
-           write(444,*) nat, 'atomic '
-           write(444,*) ' fixed configuration '
-           do iat=1,nat
-              write(444,'(a5,3(e15.7),l1)') ' Mg  ',pos(1,iat),pos(2,iat),pos(3,iat)
-           enddo
-        endif
 
-!   endif
+      !   endif
    elseif(option==2) then !OPTION=2, INVERT VELOCITIES
-!   if(nfrag.ne.1) then          !"if there is fragmentation..."
-   if(iproc==0) write(*,*) "#FIX: Preparing to invert velocities, option:",option
-   !Compute center of mass of all fragments and the collectiove velocity of each fragment
-   allocate(cm_frags(3,nfrag),vel_frags(3,nfrag),nat_frags(nfrag))
-   allocate(invert(nfrag))
-   cm_frags(:,:)=0.d0
-   vel_frags(:,:)=0.d0
-   nat_frags(:)=0         !number of atoms per fragment
-   cmass(:)=0.d0
-   velcm(:)=0.d0
-        do iat=1,nat
-           ifrag=fragarr(iat)
-           nat_frags(ifrag)=nat_frags(ifrag)+1
-           cm_frags(:,ifrag)=cm_frags(:,ifrag)+pos(:,iat)
-           vel_frags(:,ifrag)=vel_frags(:,ifrag)+vel(:,iat)
-        enddo
+      !   if(nfrag.ne.1) then          !"if there is fragmentation..."
+      if(iproc==0) write(*,*) "#FIX: Preparing to invert velocities, option:",option
+      !Compute center of mass of all fragments and the collectiove velocity of each fragment
+      allocate(cm_frags(3,nfrag),vel_frags(3,nfrag),nat_frags(nfrag))
+      allocate(invert(nfrag))
+      cm_frags(:,:)=0.d0
+      vel_frags(:,:)=0.d0
+      nat_frags(:)=0         !number of atoms per fragment
+      cmass(:)=0.d0
+      velcm(:)=0.d0
+      do iat=1,nat
+         ifrag=fragarr(iat)
+         nat_frags(ifrag)=nat_frags(ifrag)+1
+         cm_frags(:,ifrag)=cm_frags(:,ifrag)+pos(:,iat)
+         vel_frags(:,ifrag)=vel_frags(:,ifrag)+vel(:,iat)
+      enddo
 
       do ifrag=1,nfrag
-      cm_frags(:,ifrag)=cm_frags(:,ifrag)/real(nat_frags(ifrag),8)
-      vel_frags(:,ifrag)=vel_frags(:,ifrag)/real(nat_frags(ifrag),8)
-      cmass(:)=cmass(:)+cm_frags(:,ifrag)*nat_frags(ifrag)/real(nat,8)
-      velcm(:)=velcm(:)+vel_frags(:,ifrag)*nat_frags(ifrag)/real(nat,8)
+         cm_frags(:,ifrag)=cm_frags(:,ifrag)/real(nat_frags(ifrag),8)
+         vel_frags(:,ifrag)=vel_frags(:,ifrag)/real(nat_frags(ifrag),8)
+         cmass(:)=cmass(:)+cm_frags(:,ifrag)*nat_frags(ifrag)/real(nat,8)
+         velcm(:)=velcm(:)+vel_frags(:,ifrag)*nat_frags(ifrag)/real(nat,8)
       enddo
-        if (iproc==0) write(*,*) '# CM VELOCITY',sqrt(velcm(1)**2+velcm(2)**2+velcm(3)**2)
+      if (iproc==0) write(*,*) '# CM VELOCITY',sqrt(velcm(1)**2+velcm(2)**2+velcm(3)**2)
       if (velcm(1)**2+velcm(2)**2+velcm(3)**2.gt.1.d-24) then
-        if (iproc==0) write(*,*) '# NONZERO CM VELOCITY'
+         if (iproc==0) write(*,*) '# NONZERO CM VELOCITY'
       endif
 
 
-! now cm_frags contains the unit vector pointing from the center of mass of the entire system to the center of mass of the fragment
+      ! now cm_frags contains the unit vector pointing from the center of mass of the entire system to the center of mass of the fragment
       do ifrag=1,nfrag
-        cm_frags(:,ifrag)=cm_frags(:,ifrag)-cmass(:)
-        rnrmi=1.d0/sqrt(cm_frags(1,ifrag)**2+cm_frags(2,ifrag)**2+cm_frags(3,ifrag)**2)
-        cm_frags(1,ifrag)=cm_frags(1,ifrag)*rnrmi
-        cm_frags(2,ifrag)=cm_frags(2,ifrag)*rnrmi
-        cm_frags(3,ifrag)=cm_frags(3,ifrag)*rnrmi
-        angle=cm_frags(1,ifrag)*vel_frags(1,ifrag)+cm_frags(2,ifrag)*vel_frags(2,ifrag)+cm_frags(3,ifrag)*vel_frags(3,ifrag)
-        rnrmi=1.d0/sqrt(vel_frags(1,ifrag)**2+vel_frags(2,ifrag)**2+vel_frags(3,ifrag)**2)
-        angle=angle*rnrmi
-        if (angle.gt.0.d0) then
-          invert(ifrag)=.true.
-        else
-          invert(ifrag)=.false.
-        endif
-        if (iproc==0) write(*,*) '# ifrag, angle ',ifrag, angle,invert(ifrag)
+         cm_frags(:,ifrag)=cm_frags(:,ifrag)-cmass(:)
+         rnrmi=1.d0/sqrt(cm_frags(1,ifrag)**2+cm_frags(2,ifrag)**2+cm_frags(3,ifrag)**2)
+         cm_frags(1,ifrag)=cm_frags(1,ifrag)*rnrmi
+         cm_frags(2,ifrag)=cm_frags(2,ifrag)*rnrmi
+         cm_frags(3,ifrag)=cm_frags(3,ifrag)*rnrmi
+         angle=cm_frags(1,ifrag)*vel_frags(1,ifrag)+cm_frags(2,ifrag)*vel_frags(2,ifrag)+cm_frags(3,ifrag)*vel_frags(3,ifrag)
+         rnrmi=1.d0/sqrt(vel_frags(1,ifrag)**2+vel_frags(2,ifrag)**2+vel_frags(3,ifrag)**2)
+         angle=angle*rnrmi
+         if (angle.gt.0.d0) then
+            invert(ifrag)=.true.
+         else
+            invert(ifrag)=.false.
+         endif
+         if (iproc==0) write(*,*) '# ifrag, angle ',ifrag, angle,invert(ifrag)
       enddo
-!Decompose each atomic velocity into an component parallel and perpendicular to the cm_frags  vector and inter the 
-!paralle part if it point away from the CM
+      !Decompose each atomic velocity into an component parallel and perpendicular to the cm_frags  vector and inter the 
+      !paralle part if it point away from the CM
 
-!Check kinetic energy before inversion
-       ekin0=0.d0
-       vcm1=0.d0
-       vcm2=0.d0
-       vcm3=0.d0
-       do iat=1,nat
-       ekin0=ekin0+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
-       vcm1=vcm1+vel(1,iat)
-       vcm2=vcm2+vel(2,iat)
-       vcm3=vcm3+vel(3,iat)
-       enddo
+      !Check kinetic energy before inversion
+      ekin0=0.d0
+      vcm1=0.d0
+      vcm2=0.d0
+      vcm3=0.d0
+      do iat=1,nat
+         ekin0=ekin0+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
+         vcm1=vcm1+vel(1,iat)
+         vcm2=vcm2+vel(2,iat)
+         vcm3=vcm3+vel(3,iat)
+      enddo
       if (iproc==0) write(*,'(a,e14.7,3(e10.3))') '# EKIN CM before invert',ekin0,vcm1,vcm2,vcm3
       if (iproc==0) call torque(nat,pos,vel)
-!Checkend kinetic energy before inversion
+      !Checkend kinetic energy before inversion
 
-     do iat=1,nat
-! inversions  by fragment group
+      do iat=1,nat
+         ! inversions  by fragment group
          ifrag=fragarr(iat)
          if (invert(ifrag)) then
             scpr=cm_frags(1,ifrag)*vel(1,iat)+cm_frags(2,ifrag)*vel(2,iat)+cm_frags(3,ifrag)*vel(3,iat)
@@ -1932,58 +1947,58 @@ if (option==1) then !OPTION=1, FIX FRAGMENTATION
          endif
       enddo
 
-       call elim_moment(nat,vel)
-       call elim_torque_reza(nat,pos,vel)
+      call elim_moment(nat,vel)
+      call elim_torque_reza(nat,pos,vel)
 
-! scale velocities to regain initial ekin0
-       ekin=0.d0
-       do iat=1,nat
-       ekin=ekin+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
-       enddo
-       scale=sqrt(ekin0/ekin)
-       do iat=1,nat
-       vel(1,iat)=vel(1,iat)*scale
-       vel(2,iat)=vel(2,iat)*scale
-       vel(3,iat)=vel(3,iat)*scale
-       enddo
+      ! scale velocities to regain initial ekin0
+      ekin=0.d0
+      do iat=1,nat
+         ekin=ekin+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
+      enddo
+      scale=sqrt(ekin0/ekin)
+      do iat=1,nat
+         vel(1,iat)=vel(1,iat)*scale
+         vel(2,iat)=vel(2,iat)*scale
+         vel(3,iat)=vel(3,iat)*scale
+      enddo
 
-!Check kinetic energy after inversion
-       ekin=0.d0
-       vcm1=0.d0
-       vcm2=0.d0
-       vcm3=0.d0
-       do iat=1,nat
-       ekin=ekin+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
-       vcm1=vcm1+vel(1,iat)
-       vcm2=vcm2+vel(2,iat)
-       vcm3=vcm3+vel(3,iat)
-       enddo
+      !Check kinetic energy after inversion
+      ekin=0.d0
+      vcm1=0.d0
+      vcm2=0.d0
+      vcm3=0.d0
+      do iat=1,nat
+         ekin=ekin+vel(1,iat)**2+vel(2,iat)**2+vel(3,iat)**2
+         vcm1=vcm1+vel(1,iat)
+         vcm2=vcm2+vel(2,iat)
+         vcm3=vcm3+vel(3,iat)
+      enddo
       if (iproc==0) write(*,'(a,e14.7,3(e10.3))') '# EKIN CM after  invert',ekin,vcm1,vcm2,vcm3
       if (iproc==0) call torque(nat,pos,vel)
-!Checkend kinetic energy after inversion
+      !Checkend kinetic energy after inversion
 
-!Check angle  after inversion
+      !Check angle  after inversion
       vel_frags(:,:)=0.d0
       do iat=1,nat
-           ifrag=fragarr(iat)
-           vel_frags(:,ifrag)=vel_frags(:,ifrag)+vel(:,iat)
+         ifrag=fragarr(iat)
+         vel_frags(:,ifrag)=vel_frags(:,ifrag)+vel(:,iat)
       enddo
       do ifrag=1,nfrag
-      angle=cm_frags(1,ifrag)*vel_frags(1,ifrag)+cm_frags(2,ifrag)*vel_frags(2,ifrag)+cm_frags(3,ifrag)*vel_frags(3,ifrag)
-      rnrmi=1.d0/sqrt(vel_frags(1,ifrag)**2+vel_frags(2,ifrag)**2+vel_frags(3,ifrag)**2)
-      angle=angle*rnrmi
-      if (iproc==0) write(*,*) '# ifrag, angle a invert',ifrag, angle
+         angle=cm_frags(1,ifrag)*vel_frags(1,ifrag)+cm_frags(2,ifrag)*vel_frags(2,ifrag)+cm_frags(3,ifrag)*vel_frags(3,ifrag)
+         rnrmi=1.d0/sqrt(vel_frags(1,ifrag)**2+vel_frags(2,ifrag)**2+vel_frags(3,ifrag)**2)
+         angle=angle*rnrmi
+         if (iproc==0) write(*,*) '# ifrag, angle a invert',ifrag, angle
       enddo
-!Checkend kinetic energy after inversion
+      !Checkend kinetic energy after inversion
 
 
-   deallocate(cm_frags,vel_frags,nat_frags)
-   deallocate(invert)
-!   endif
-!else
-!   stop "Wrong option within ff-rv"
-   if(iproc==0) write(*,*) "#FIX: Velocity component towards the center of mass inverted! Keep on hopping..."
-endif
+      deallocate(cm_frags,vel_frags,nat_frags)
+      deallocate(invert)
+      !   endif
+      !else
+      !   stop "Wrong option within ff-rv"
+      if(iproc==0) write(*,*) "#FIX: Velocity component towards the center of mass inverted! Keep on hopping..."
+   endif
 endif
 end subroutine fixfrag_posvel
 
