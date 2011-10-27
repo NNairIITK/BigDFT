42f9995677b1033c4f3765f41fdb8e7f1546ed05
Damien Caliste <damien.caliste@cea.fr>
2011-10-27 11:54:09 +0200

    Correct the flush inclusion when not available.

--- src/modules/defs.F90
+++ src/modules/defs.F90
@@ -44,6 +44,10 @@ module module_defs
   logical, parameter :: have_mpi2 = .false. !< Flag to use in the code to switch between MPI1 and MPI2
 #endif
 
+#ifndef HAVE_FC_FLUSH
+  integer :: flush
+#endif
+
   !> Flag for GPU computing, if CUDA libraries are present
   !! in that case if a GPU is present a given MPI processor may or not perform a GPU calculation
   !! this value can be changed in the read_input_variables routine
