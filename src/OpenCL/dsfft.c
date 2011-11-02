c0890906235cf194fc29b051685d0213ef09d249
Brice Videau <brice.videau@cea.fr>
2011-11-02 12:01:45 +0100

    Removed debug printing.

--- src/OpenCL/dsfft.c
+++ src/OpenCL/dsfft.c
@@ -199,7 +199,7 @@ void build_fft_programs(cl_context * context){
     fft_code * c;
     if(fft_size[0]!=0){
       c = generate_fft_program(fft_size[0],&infos);
-      printf("%s\n",c->code);
+//      printf("%s\n",c->code);
       fftProgramd0 = clCreateProgramWithSource(*context,1,(const char**) &(c->code), NULL, &ciErrNum);
       oclErrorCheck(ciErrNum,"Failed to create programd0!");
       ciErrNum = clBuildProgram(fftProgramd0, 0, NULL, "-cl-mad-enable", NULL, NULL);
@@ -222,7 +222,7 @@ void build_fft_programs(cl_context * context){
     }
     if(fft_size[1]!=0){
       c = generate_fft_program(fft_size[1], &infos);
-      printf("%s\n",c->code);
+//      printf("%s\n",c->code);
       fftProgramd1 = clCreateProgramWithSource(*context,1,(const char**) &(c->code), NULL, &ciErrNum);
       oclErrorCheck(ciErrNum,"Failed to create programd1!");
       ciErrNum = clBuildProgram(fftProgramd1, 0, NULL, "-cl-mad-enable", NULL, NULL);
@@ -245,7 +245,7 @@ void build_fft_programs(cl_context * context){
     }
     if(fft_size[2]!=0){
       c = generate_fft_program(fft_size[2], &infos);
-      printf("%s\n",c->code);
+//      printf("%s\n",c->code);
       fftProgramd2 = clCreateProgramWithSource(*context,1,(const char**) &(c->code), NULL, &ciErrNum);
       oclErrorCheck(ciErrNum,"Failed to create programd1!");
       ciErrNum = clBuildProgram(fftProgramd2, 0, NULL, "-cl-mad-enable", NULL, NULL);
