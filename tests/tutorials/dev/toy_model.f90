a1a3644b474280584901df3afe1ee26c6753283b
Damien Caliste <damien.caliste@cea.fr>
2012-03-06 09:42:20 +0100

    Update the interface of createIonicPotential in toy_model.

--- tests/tutorials/dev/toy_model.f90
+++ tests/tutorials/dev/toy_model.f90
@@ -192,7 +192,7 @@ program wvl
   call createKernel(iproc,nproc,atoms%geocode,Lzd%Glr%d%n1i,Lzd%Glr%d%n2i,Lzd%Glr%d%n3i, &
        & inputs%hx / 2._gp,inputs%hy / 2._gp,inputs%hz / 2._gp,16,pkernel,.false.)
   allocate(pot_ion(Lzd%Glr%d%n1i * Lzd%Glr%d%n2i * n3p))
-  call createIonicPotential(atoms%geocode,iproc,nproc,atoms,rxyz,&
+  call createIonicPotential(atoms%geocode,iproc,nproc,(iproc==0),atoms,rxyz,&
        & inputs%hx / 2._gp,inputs%hy / 2._gp,inputs%hz / 2._gp, &
        & inputs%elecfield,Lzd%Glr%d%n1,Lzd%Glr%d%n2,Lzd%Glr%d%n3, &
        & n3pi,i3s+i3xcsh,Lzd%Glr%d%n1i,Lzd%Glr%d%n2i,Lzd%Glr%d%n3i, &
