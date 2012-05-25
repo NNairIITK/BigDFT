af95b8ed2e8c533db3f9fd0c589190632bed6fbf
Damien Caliste <damien.caliste@cea.fr>
2012-05-25 10:59:54 +0200

    Try to correct minor issues on buildbot.

--- autogen.sh
+++ autogen.sh
@@ -16,7 +16,7 @@ rm -f src/init/psp.inc
 echo "Autotoolize the libXC source tree."
 cd libxc-1.1.0; libtoolize; autoreconf; cd -
 echo "Autotoolize the libyaml source tree."
-cd yaml-0.1.4; libtoolize; autoreconf; cd -
+cd yaml-0.1.4; libtoolize -f; autoreconf -i; cd -
 echo "Autotoolize the S_GPU source tree."
 cd S_GPU; ./autogen.sh; cd -
 
