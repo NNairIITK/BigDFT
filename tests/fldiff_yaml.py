258beb31fe2821aff241e089f9a08d79fc68bc29
Luigi Genovese <luigi.genovese@cea.fr>
2012-05-29 15:07:19 +0200

    Commit of fldiff.yaml

--- tests/fldiff_yaml.py
+++ tests/fldiff_yaml.py
@@ -20,61 +20,69 @@ tols       = yaml.load(open(sys.argv[3], "r"), Loader = yaml.CLoader)
 #descend recursively in the dictionary until a scalar is found
 #a tolerance value might be passed
 def compare(data, ref, tols = None):
-  ret = None
   if type(ref) == type({}):
-    compare_map(data, ref, tols)
+    ret = compare_map(data, ref, tols)
   elif type(ref) == type([]):
-    compare_seq(data, ref, tols)
+    ret = compare_seq(data, ref, tols)
   else:
     ret = compare_scl(data, ref, tols)
   return ret
 
 #sequence comparison routine
 def compare_seq(seq, ref, tols):
-  if tols is not None and len(tols) == len(ref):
+  if tols is not None:
     for i in range(len(ref)):
-      ret = compare(seq[i], ref[i], tols[i])
-      if ret is not None:
-        ref[i] = ret
+      (failed, newtols) = compare(seq[i], ref[i], tols[0])
+      if failed:
+        tols[0] = newtols
   else:
+    tols = []
     for i in range(len(ref)):
-      ret = compare(seq[i], ref[i])
-      if ret is not None:
-        ref[i] = ret
+      (failed, newtols) = compare(seq[i], ref[i])
+      if failed:
+        if len(tols) == 0:
+          tols.append(newtols)
+        else:
+          tols[0] = newtols          
+  return (len(tols) > 0, tols)
 
 def compare_map(map, ref, tols):
+  if tols is None:
+    tols = {}
   for (key, value) in ref.items():
 #    print "#####", key
     if type(tols) == type({}) and key in tols:
-      ret = compare(map[key], value, tols[key])
+      (failed, newtols) = compare(map[key], value, tols[key])
     else:
-      ret = compare(map[key], value)
-    if ret is not None:
-      ref[key] = ret
+      (failed, newtols) = compare(map[key], value)
+    if failed:
+      tols[key] = newtols
+  return (len(tols) > 0, tols)  
+  
 
 #compare the scalars.
 #return the tolerance if the results are ok
 def compare_scl(scl, ref, tols):
   global failed_checks
+  failed = False
+  ret = (failed, None)
   if type(ref) == type(""):
-    if scl == ref:
-      ret = ref
-    else:
-      failed_checks +=1
-      ret = start_fail + scl + end
+    if not(scl == ref):
+      ret = (True, scl)
   else:
     if tols is None:
-      ret = math.fabs(scl - ref) == 0
+      failed = not(math.fabs(scl - ref) == 0)
     else:
-      ret = math.fabs(scl - ref) < tols
-    if ret:
+      failed = not(math.fabs(scl - ref) < float(str(tols).split()[0]))
+    if not(failed):
       if tols is None:
-        ret = ".inf" #"%s.inf%s" % (start_success, end) #float("inf") #
+        ret = (False, None)
       else:
-        ret = tols
+        ret = (True, float(str(tols).split()[0]))
     else:
-      failed_checks +=1
-      ret = start_fail + str(scl - ref)  + " (" + str(tols) + ")" + end
+      ret = (True, str(math.fabs(scl - ref)) + " FAILED")
+  if failed:
+    failed_checks +=1
 #  print ret
   return ret
 
@@ -91,7 +99,7 @@ for i in range(len(references)):
   print "failed checks",failed_checks
   if failed_checks > 0:
     failed_documents+=1
-    sys.stdout.write(yaml.dump(reference))
+    sys.stdout.write(yaml.dump(tols))
 #  print yaml.scan(reference)
 #  print substitute(reference,tols)
 #  hl.input=reference
