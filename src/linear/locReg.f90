97cddfae90e402d26450bbba0f80bf3591e6c3c7
Stephan Mohr <stephan@phys-comp-03>
2012-07-26 14:43:09 +0200

    - some cleaning

--- src/linear/locReg.f90
+++ src/linear/locReg.f90
@@ -152,21 +152,8 @@ subroutine determine_locregSphere_parallel(iproc,nproc,nlr,cxyz,locrad,hx,hy,hz,
   integer :: ln1,ln2,ln3, iorb, jproc, jlr
   integer :: ierr, ii, istat, root, iall
   integer,dimension(3) :: outofzone
-  !integer,dimension(6,3):: ncounts
-  integer(kind=mpi_address_kind),dimension(6,4):: mpi_ncounts
-  integer,dimension(6,4):: ncounts
-  integer(kind=mpi_address_kind):: mpi_0, mpi_36, mpi_30, size_of_integer, mpi_6
   real(gp) :: rx,ry,rz,cutoff
-  real(8):: tt1, tt2, tt3, tt4, t1, t2, tl1, tl2
-  integer,dimension(:,:,:,:),allocatable:: ab
-  integer,dimension(:,:),allocatable:: wins_bounds
-  integer,dimension(:,:,:),allocatable::  wins_arrays, ifake
-  integer:: i1, i2, i3
   integer,dimension(:),allocatable:: rootarr
-tt1=0.d0
-tt2=0.d0
-tt3=0.d0
-tt4=0.d0
 
 
   allocate(rootarr(nlr), stat=istat)
@@ -174,7 +161,6 @@ tt4=0.d0
 
   ! Determine how many locregs one process handles at most
   ii=ceiling(dble(nlr)/dble(nproc))
-tl1=mpi_wtime()
   !determine the limits of the different localisation regions
 
   rootarr=1000000000
@@ -206,12 +192,9 @@ tl1=mpi_wtime()
          llr(ilr)%locrad=cutoff
     
          ! Determine the extrema of this localization regions (using only the coarse part, since this is always larger or equal than the fine part).
-t1=mpi_wtime()
          call determine_boxbounds_sphere(glr%d%n1, glr%d%n2, glr%d%n3, glr%ns1, glr%ns2, glr%ns3, hx, hy, hz, &
               cutoff, llr(ilr)%locregCenter, &
                glr%wfd%nseg_c, glr%wfd%keygloc, glr%wfd%keyvloc, isx, isy, isz, iex, iey, iez)
-t2=mpi_wtime()
-tt1=tt1+t2-t1
     
          ln1 = iex-isx
          ln2 = iey-isy
@@ -385,158 +368,33 @@ tt1=tt1+t2-t1
     !DEBUG
     
         ! construct the wavefunction descriptors (wfd)
-t1=mpi_wtime()
          call determine_wfdSphere(ilr,nlr,Glr,hx,hy,hz,Llr)
-t2=mpi_wtime()
-tt2=tt2+t2-t1
     
          ! Sould check if nfu works properly... also relative to locreg!!
          !if the localisation region is isolated build also the bounds
          if (Llr(ilr)%geocode=='F') then
-            ! Check whether the bounds shall be calculated. Do this only if the currect process handles
-            ! orbitals in the current localization region.
-            !if(calculateBounds(ilr)) then
-t1=mpi_wtime()
-                call locreg_bounds(Llr(ilr)%d%n1,Llr(ilr)%d%n2,Llr(ilr)%d%n3,&
-                     Llr(ilr)%d%nfl1,Llr(ilr)%d%nfu1,Llr(ilr)%d%nfl2,Llr(ilr)%d%nfu2,&
-                     Llr(ilr)%d%nfl3,Llr(ilr)%d%nfu3,Llr(ilr)%wfd,Llr(ilr)%bounds)
-t2=mpi_wtime()
-tt3=tt3+t2-t1
-            !end if
+            call locreg_bounds(Llr(ilr)%d%n1,Llr(ilr)%d%n2,Llr(ilr)%d%n3,&
+                 Llr(ilr)%d%nfl1,Llr(ilr)%d%nfu1,Llr(ilr)%d%nfl2,Llr(ilr)%d%nfu2,&
+                 Llr(ilr)%d%nfl3,Llr(ilr)%d%nfu3,Llr(ilr)%wfd,Llr(ilr)%bounds)
          end if
      end if
   end do !on ilr
-call mpi_barrier(mpi_comm_world, ierr)
 
-!call mpiallred(rootarr(1), nlr, mpi_sum, mpi_comm_world, ierr)
 call mpiallred(rootarr(1), nlr, mpi_min, mpi_comm_world, ierr)
 
-tl2=mpi_wtime()
-!if(iproc==0) write(*,*) 'in determine_locregSphere_parallel, loop 1: time',tl2-tl1
 
   ! Communicate the locregs
-tl1=mpi_wtime()
   do ilr=1,nlr
-     !root=mod(ilr-1,nproc)
      root=rootarr(ilr)
      if (nproc > 1) then
         call communicate_locreg_descriptors(iproc, root, llr(ilr))
      end if
  end do
-call mpi_barrier(mpi_comm_world, ierr)
-tl2=mpi_wtime()
-!if(iproc==0) write(*,*) 'in determine_locregSphere_parallel, loop 2: time',tl2-tl1
 
-!allocate(wins_bounds(4,nlr), stat=istat)
-!call memocc(istat, wins_bounds, 'wins_bounds', subname)
-!allocate(wins_arrays(6,4,nlr), stat=istat)
-!call memocc(istat, wins_arrays, 'wins_arrays', subname)
-!allocate(ab(6,6,4,nlr), stat=istat)
-!call memocc(istat, ab, 'ab', subname)
-!allocate(ifake(6,4,nlr), stat=istat)
-!call memocc(istat, ifake, 'ifake', subname)
-
-call mpi_type_size(mpi_integer, size_of_integer, ierr)
-
-tl1=mpi_wtime()
-  !!do ilr=1,nlr
-  !!   root=mod(ilr-1,nproc)
-  !!   if (Llr(ilr)%geocode=='F') then
-  !!      ! Check whether the bounds shall be calculated. Do this only if the currect process handles
-  !!      ! orbitals in the current localization region.
-  !!      if (nproc > 1) then
-  !!         call communicate_convolutions_bounds(iproc, root, llr(ilr)%bounds)
-  !!      end if
-  !!      if(.not.calculateBounds(ilr)) then
-  !!          call deallocate_convolutions_bounds(llr(ilr)%bounds, subname)
-  !!      end if
-
-  !!      !!if(iproc==root) then
-  !!      !!    !determine array boundaries
-  !!      !!    call get_convarrays_bounds(llr(ilr)%bounds, ab(1,1,1,ilr), mpi_ncounts)
-  !!      !!    call create_convolutions_windows_root(ab(1,1,1,ilr), mpi_ncounts, llr(ilr)%bounds, wins_bounds(1,ilr), wins_arrays(1,1,ilr))
-
-  !!      !!else
-  !!      !!    ! Initialize the windows with a fake array, since the convolutions bounds are nullified and I don't
-  !!      !!    ! know what happens when a window is opened with a nullified pointer...
-  !!      !!    call create_convolutions_windows_else(ab(1,1,1,ilr), ifake(1,1,ilr), wins_bounds(1,ilr), wins_arrays(1,1,ilr))
-  !!      !!end if
-
-  !!      !!call convolutions_bounds_fences(wins_bounds(1,ilr))
-  !!      !!call convolutions_arrays_fences(wins_arrays(1,1,ilr))
-
-  !!      !!if(calculateBounds(ilr) .and. iproc/=root) then
-  !!      !!    !this process needs the bounds, so get them from root
-  !!      !!    call get_convolutions_bounds(root, ab(1,1,1,ilr), wins_bounds(1,ilr))
-  !!      !!end if
-  !!  end if
-  !!end do
-
-  !!! Make sure the bounds arrived...
-  !!do ilr=1,nlr
-  !!    call convolutions_bounds_fences(wins_bounds(1,ilr))
-  !!end do
-
-  !!do ilr=1,nlr
-  !!    root=mod(ilr-1,nproc)
-  !!    if(calculateBounds(ilr) .and. iproc/=root) then
-  !!        call allocate_convolutions_bounds(ab(1,1,1,ilr), subname, llr(ilr)%bounds)
-  !!        call get_convolutions_arrays(root, ab(1,1,1,ilr), llr(ilr)%bounds, wins_arrays(1,1,ilr))
-  !!    end if
-  !!end do
-
-  !!! Make sure everything arrived
-  !!do ilr=1,nlr
-  !!    call convolutions_arrays_fences(wins_arrays(1,1,ilr))
-  !!    call free_convolutions_bounds_windows(wins_bounds(1,ilr))
-  !!    call free_convolutions_arrays_windows(wins_arrays(1,1,ilr))
-  !!    !!if(calculateBounds(ilr) .or. iproc==root) then
-  !!    !!    if(calculateBounds(ilr)) write(*,'(3(a,i0))') 'locreg ',ilr,': process ',iproc, ' should receive bounds from process ',root
-  !!    !!    if(iproc==root) write(*,'(2(a,i0))') 'locreg ',ilr,' was calculated by process ',iproc
-  !!    !!    do i3=ab(5,4,2,ilr),ab(6,4,2,ilr)
-  !!    !!      do i2=ab(3,4,2,ilr),ab(4,4,2,ilr)
-  !!    !!        do i1=ab(1,4,2,ilr),ab(2,4,2,ilr)
-  !!    !!          if(iproc==root) then
-  !!    !!            write(1000*(iproc+1)+ilr,'(a,i5,6i8,3i8,i12)') 'r: values',ilr, ab(5,1,3,ilr),ab(6,1,3,ilr),ab(3,1,3,ilr),ab(4,1,3,ilr),ab(1,1,3,ilr),ab(2,1,3,ilr),i1, i2, i3, llr(ilr)%bounds%sb%ibzzx_f(i1,i2,i3)
-  !!    !!          else
-  !!    !!            write(1000*(iproc+1)+100+ilr,'(a,i5,6i8,3i8,i12)') 'e: values',ilr, ab(5,1,3,ilr),ab(6,1,3,ilr),ab(3,1,3,ilr),ab(4,1,3,ilr),ab(1,1,3,ilr),ab(2,1,3,ilr), i1, i2, i3, llr(ilr)%bounds%sb%ibzzx_f(i1,i2,i3)
-  !!    !!          end if
-  !!    !!        end do
-  !!    !!      end do
-  !!    !!    end do
-  !!    !!end if
-  !!    if(.not.calculateBounds(ilr)) then
-  !!        call deallocate_convolutions_bounds(llr(ilr)%bounds, subname)
-  !!    end if
-  !!end do
-call mpi_barrier(mpi_comm_world, ierr)
-!call mpi_finalize(ierr)
-tl2=mpi_wtime()
-!if(iproc==0) write(*,*) 'in determine_locregSphere_parallel, loop 3: time',tl2-tl1
-
-!iall = -product(shape(wins_bounds))*kind(wins_bounds)
-!deallocate(wins_bounds,stat=istat)
-!call memocc(istat,iall,'wins_bounds',subname)
-!iall = -product(shape(wins_arrays))*kind(wins_arrays)
-!deallocate(wins_arrays,stat=istat)
-!call memocc(istat,iall,'wins_arrays',subname)
-!iall = -product(shape(ab))*kind(ab)
-!deallocate(ab,stat=istat)
-!call memocc(istat,iall,'ab',subname)
-!iall = -product(shape(ifake))*kind(ifake)
-!deallocate(ifake,stat=istat)
-!call memocc(istat,iall,'ifake',subname)
 
 iall = -product(shape(rootarr))*kind(rootarr)
 deallocate(rootarr,stat=istat)
 call memocc(istat,iall,'rootarr',subname)
-
-!if(iproc==0) then
-!    write(*,*) 'determine_locregSphere_parallel: time 1',tt1
-!    write(*,*) 'determine_locregSphere_parallel: time 2',tt2
-!    write(*,*) 'determine_locregSphere_parallel: time 3',tt3
-!    write(*,*) 'determine_locregSphere_parallel: time 4',tt4
-!end if
 
 END SUBROUTINE determine_locregSphere_parallel
 
