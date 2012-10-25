141aa80c15f87b032c44b07e05ff41e12ffff318
Stephan Mohr <stephan@phys-comp-03>
2012-10-25 07:54:34 +0200

    improved dsygv test

--- tests/libs/dsygv/init.f90
+++ tests/libs/dsygv/init.f90
@@ -6,11 +6,14 @@ subroutine init_matrices(n, A, S)
   real(kind=8),dimension(n,n),intent(out) :: A, S
 
   ! Local variables
-  integer :: i, j
+  integer :: i, j, ii
+  real(4) :: builtin_rand
 
+  ii=0
   do i=1,n
       do j=i,n
-          A(j,i)=dble(j)
+          !A(j,i)=dble(j)
+          A(j,i)=dble(builtin_rand(ii))
           A(i,j)=A(j,i)
           if(i==j) then
               S(i,j)=1.d0
