73587ec3f594c8a7384cb418e36499af9c985f38
Jan Spoerri <jan@phys-comp-06.physik.unibas.ch>
2012-11-01 14:29:44 +0100

    sparse matrix matrix multiplication

--- src/linear/chebyshev.f90
+++ src/linear/chebyshev.f90
@@ -13,7 +13,7 @@ subroutine chebyshev(iproc, nproc, npl, cc, tmb, ham, ovrlp, fermi)
   ! Local variables
   integer :: istat, iorb,iiorb, jorb, iall,ipl,norb,norbp,isorb
   character(len=*),parameter :: subname='chebyshev'
-  real(8), dimension(:,:), allocatable :: column,column_tmp, t,t1,t2,t1_tmp
+  real(8), dimension(:,:), allocatable :: column,column_tmp, t,t1,t2,t1_tmp,ts
   real(8), dimension(tmb%orbs%norb,tmb%orbs%norb) :: ovrlp_tmp,ham_eff
 
   norb = tmb%orbs%norb
@@ -32,6 +32,8 @@ subroutine chebyshev(iproc, nproc, npl, cc, tmb, ham, ovrlp, fermi)
   call memocc(istat, t1_tmp, 't1_tmp', subname)
   allocate(t2(norb,norbp), stat=istat)
   call memocc(istat, t2, 't2', subname)
+  allocate(ts(norb,norbp), stat=istat)
+  call memocc(istat, ts, 'ts', subname)
   
   call to_zero(norb*norbp, column(1,1))
   call to_zero(norb*norbp, column_tmp(1,1))
@@ -70,7 +72,19 @@ subroutine chebyshev(iproc, nproc, npl, cc, tmb, ham, ovrlp, fermi)
      fermi(:,iorb) = cc(1)*0.5d0*t(:,iorb) + cc(2)*t1(:,iorb)
   end do
 
+   
+  call dsymm('L', 'U', norb, norbp,1.d0,ovrlp_tmp,norb,t1_tmp,norb,0.d0,t1,norb)
   
+  call sparsemm(ovrlp_tmp,t1_tmp,ts,norb,tmb%mad)
+
+  do i=1,norb
+    do j=1,norb
+    if(iproc==0) write(100,*) i,j,t1(i,j)
+    if(iproc==0) write(200,*) i,j,ts(i,j)
+    end do
+  end do
+
+ 
 
   do ipl=3,npl
      !calculate (3/2 - 1/2 S) H (3/2 - 1/2 S) t
@@ -102,5 +116,47 @@ subroutine chebyshev(iproc, nproc, npl, cc, tmb, ham, ovrlp, fermi)
   iall=-product(shape(t2))*kind(t2)
   deallocate(t2, stat=istat)
   call memocc(istat, iall, 't2', subname)
+  iall=-product(shape(ts))*kind(ts)
+  deallocate(ts, stat=istat)
+  call memocc(istat, iall, 'ts', subname)
 
 end subroutine chebyshev
+
+subroutine sparsemm(norb,a,b,c,mad)
+
+use module_base
+use module_types
+
+
+  implicit none
+
+  !Calling Arguments
+  integer, intent(in) :: norb
+  real(kind=8), dimension(norb,norb),intent(in) :: a,b
+  real(kind=8), dimension(norb,norb),intent(out) :: c
+  type(matrixDescriptors),intent(in) :: mad
+
+  !Local variables
+  integer :: i,j,iseg,jorb,iiorb,jjorb
+  real(kind=8) :: temp
+
+
+  call to_zero(norb*norb,c(1,1))
+
+  do i = 1,norb
+    do j = 1,norb
+
+        temp = b(i,j)
+  
+        do iseg = 1,mad%nseg
+          do jorb = mad%keyg(1,iseg), mad%keyg(2,iseg)
+            iiorb = (jorb-1)/norb + 1
+            jjorb = jorb - (iiorb - 1)*norb
+            c(iiorb,j) = c(iiorb,j) + temp*a(j,iiorb) 
+          end do
+        end do
+     end do
+  end do 
+  
+    
+end subroutine sparsemm
