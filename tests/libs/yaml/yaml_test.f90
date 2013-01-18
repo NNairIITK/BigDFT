ac5c00bd8a5f6564733943433e6b3fa2c5d19e51
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2013-01-18 15:00:22 +0100

    Add tests for yaml. Seems to have a bug ??

--- tests/libs/yaml/yaml_test.f90
+++ tests/libs/yaml/yaml_test.f90
@@ -19,7 +19,7 @@ program yaml_test
    real(kind=8), dimension(:), allocatable :: dv
    !First document
    call yaml_new_document()
-   call yaml_open_map("Test")
+   call yaml_open_map("Test 1")
       call yaml_map("Short sentence",.true.)
       call yaml_stream_attributes(iflowlevel=i,ilevel=l,ilast=j,indent=d,flowrite=fl,indent_previous=ip,icursor=ic)
       call yaml_open_map("Foo",flow=.true.)
@@ -35,8 +35,8 @@ program yaml_test
 
    !Second document
    call yaml_new_document()
-   call yaml_open_map("Test")
-      call yaml_map("I have a very long sentence in order to test if yaml_output fails to print that",.true.)
+   call yaml_open_map("Test 2")
+      call yaml_map("I have a very long sentence in order to test if yaml_output fails to print that",.false.)
       call yaml_open_map("Foo",flow=.true.)
       call yaml_map("one",1)
       call yaml_map("two",2)
@@ -53,11 +53,22 @@ program yaml_test
    allocate(iv(0))
    allocate(dv(0))
    call yaml_new_document()
-   !Check calling twice yaml_new_document
+   call yaml_comment('Check calling twice yaml_new_document')
    call yaml_new_document()
    call yaml_map('Vector of characters',cv)
    call yaml_map('Vector of integers',iv)
    call yaml_map('Vector of real(kind=8)',dv)
+   call yaml_close_sequence()
+   deallocate(cv)
+   allocate(cv(8))
+   do i=1,8
+      cv(i)=yaml_toa(i)
+   end do
+   call yaml_comment('Check flow=.true.',hfill='-')
+   call yaml_open_sequence('Open a sequence',flow=.true.)
+      call yaml_sequence(trim(yaml_toa(cv)))
+      call yaml_sequence(trim(yaml_toa(cv)))
+   call yaml_close_sequence()
    call yaml_release_document()
    deallocate(cv)
    deallocate(iv)
