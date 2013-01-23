31c8dcb201bb72a845497ecdd3af89ec6a98357f
Laura Ratcliff laura.ratcliff@cea.fr <>
2013-01-23 17:56:45 +0100

    merge with Stephan 2, tests seem ok, though haven't checked long tests

--- src/linear/inputguessLinear.f90
+++ src/linear/inputguessLinear.f90
@@ -230,8 +230,20 @@ subroutine inputguessConfinement(iproc, nproc, at, input, hx, hy, hz, &
   !!call mpi_finalize(istat)
   !!stop
 
+
+  if (input%lin%mixing_after_inputguess) then
+      if(input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE &
+           .or. input%lin%scf_mode==LINEAR_DIRECT_MINIMIZATION) then
+          call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
+      end if
+  end if
   call updatePotential(input%ixc,input%nspin,denspot,energs%eh,energs%exc,energs%evxc)
 
+  if (input%lin%mixing_after_inputguess) then
+      if(input%lin%scf_mode==LINEAR_MIXPOT_SIMPLE) then
+          call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
+      end if
+  end if
   if (input%exctxpar == 'OP2P') energs%eexctX = uninitialized(energs%eexctX)
 
   allocate(tmb%linmat%ham%matrix_compr(tmb%linmat%ham%nvctr), stat=istat)
@@ -248,46 +260,36 @@ subroutine inputguessConfinement(iproc, nproc, at, input, hx, hy, hz, &
            input%SIC,tmb,fnrm,.true.,.false.,tmblarge,.true.)
   end if
 
-
-!!  call communicate_basis_for_density_collective(iproc, nproc, tmb%lzd, tmb%orbs, tmb%psi, tmb%collcom_sr)
-!!  call sumrho_for_TMBs(iproc, nproc, tmb%Lzd%hgrids(1), tmb%Lzd%hgrids(2), tmb%Lzd%hgrids(3), &
-!!       tmb%orbs, tmblarge%mad, tmb%collcom_sr, tmb%wfnmd%density_kernel_compr, &
-!!       tmb%Lzd%Glr%d%n1i*tmb%Lzd%Glr%d%n2i*denspot%dpbox%n3d, denspot%rhov)
-!!
-!!  ! Mix the density.
-!!  if (input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE) then
-!!     call mix_main(iproc, nproc, 0, input, tmb%Lzd%Glr, input%lin%alpha_mix_lowaccuracy, &
-!!          denspot, mixdiis, rhopotold, pnrm)
-!!  end if
-!!
-!!  
-!!  if(input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE &
-!!       .or. input%lin%scf_mode==LINEAR_DIRECT_MINIMIZATION) then
-!!      call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
-!!  end if
-!!
-!!  call updatePotential(input%ixc,input%nspin,denspot,energs%eh,energs%exc,energs%evxc)
-!!
-!!  ! Mix the potential.
-!!  if (input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE) then
-!!     call mix_main(iproc, nproc, 0, input, tmb%Lzd%Glr, input%lin%alpha_mix_lowaccuracy, &
-!!          denspot, mixdiis, rhopotold, pnrm)
-!!  end if
-!!
-!!  if(input%lin%scf_mode==LINEAR_MIXPOT_SIMPLE) then
-!!      call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
-!!  end if
-
-
-  !call communicate_basis_for_density_collective(iproc, nproc, tmb%lzd, tmb%orbs, tmb%psi, tmb%collcom_sr)
+  call communicate_basis_for_density_collective(iproc, nproc, tmb%lzd, tmb%orbs, tmb%psi, tmb%collcom_sr)
   call sumrho_for_TMBs(iproc, nproc, tmb%Lzd%hgrids(1), tmb%Lzd%hgrids(2), tmb%Lzd%hgrids(3), tmb%orbs, &
        tmb%collcom_sr, tmb%linmat%denskern, tmb%Lzd%Glr%d%n1i*tmb%Lzd%Glr%d%n2i*denspot%dpbox%n3d, denspot%rhov)
 
-  if(input%lin%scf_mode/=LINEAR_MIXPOT_SIMPLE) then
+  !call plot_density(iproc,nproc,'initial',at,rxyz,denspot%dpbox,input%nspin,denspot%rhov)
+
+
+  ! Mix the density.
+  if (input%lin%mixing_after_inputguess) then
+      if (input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE &
+           .or. input%lin%scf_mode==LINEAR_DIRECT_MINIMIZATION) then
+         call mix_main(iproc, nproc, 0, input, tmb%Lzd%Glr, input%lin%alpha_mix_lowaccuracy, &
+              denspot, mixdiis, rhopotold, pnrm)
+      end if
+  end if
+
+  if(input%lin%scf_mode==LINEAR_MIXDENS_SIMPLE .or. input%lin%scf_mode==LINEAR_FOE &
+       .or. input%lin%scf_mode==LINEAR_DIRECT_MINIMIZATION) then
       call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
   end if
 
   call updatePotential(input%ixc,input%nspin,denspot,energs%eh,energs%exc,energs%evxc)
+
+  ! Mix the potential.
+  if (input%lin%mixing_after_inputguess) then
+      if(input%lin%scf_mode==LINEAR_MIXPOT_SIMPLE) then
+         call mix_main(iproc, nproc, 0, input, tmb%Lzd%Glr, input%lin%alpha_mix_lowaccuracy, &
+              denspot, mixdiis, rhopotold, pnrm)
+      end if
+  end if
 
   if(input%lin%scf_mode==LINEAR_MIXPOT_SIMPLE) then
       call dcopy(max(tmb%lzd%glr%d%n1i*tmb%lzd%glr%d%n2i*denspot%dpbox%n3p,1)*input%nspin, denspot%rhov(1), 1, rhopotold(1), 1)
