b35df5669859dfdc3d506a5ef1bf5cefc5102ec7
Stephan Mohr <stephanm@ela.cscs.ch>
2013-08-13 09:39:36 +0200

    some experimental stuff with respect to the omission of the orthogonalization of the TMBs

--- src/linear/linearScaling.f90
+++ src/linear/linearScaling.f90
@@ -377,6 +377,10 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
                !if (iproc==0) write(*,'(a,es18.8)') 'tmb%confdatarr(1)%prefac',tmb%confdatarr(1)%prefac
            end if
 
+           if (pnrm_out<1.d-8) then
+               if (iproc==0) write(*,*) 'outswitch off ortho'
+               orthonormalization_on=.false.
+           end if
            call getLocalizedBasis(iproc,nproc,at,KSwfn%orbs,rxyz,denspot,GPU,trace,trace_old,fnrm_tmb,&
                info_basis_functions,nlpspd,input%lin%scf_mode,proj,ldiis,input%SIC,tmb,energs, &
                reduce_conf,fix_supportfunctions,input%lin%nItPrecond,target_function,input%lin%correctionOrthoconstraint,&
