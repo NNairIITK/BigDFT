e0c110cb19e9af158334055e67205786a6dd6090
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2013-11-20 14:58:42 +0100

    Change report.py to remove .test

--- tests/report.py
+++ tests/report.py
@@ -60,8 +60,9 @@ totime=0
 
 print "Final report for writings in stdout ('passed' means all significant floats are correct):"
 for file in files:
-    dir = os.path.normpath(os.path.dirname(file))
+    dirc = os.path.normpath(os.path.dirname(file))
     fic = "(%s)" % os.path.basename(file)
+    dirfic = ("%-35s %-30s" % (dirc.replace('.test',''),fic.replace('.report',''))).strip()
     #Max value
     try:
         max_discrepancy = float(open(file).readline())
@@ -108,20 +109,18 @@ for file in files:
             time = "%8ss" % time[0]
         else:
             time = ""
-        name = "%s%-27s %-38s" % (start,dir,fic)
-        print "%-74s%s%s%s" % (name.strip(),state,time,end)
+        print "%s%-74s%s%s%s" % (start,dirfic,state,time,end)
     else:
         if not empty_file:
             start = start_fail
             state = "can not parse file.    failed"
-            name = "%s%-27s %-38s" % (start,dir,fic)
-            print "%-74s%s%s" % (name.strip(),state,end)
+            print "%s%-74s%s%s" % (start,dirfic,state,end)
 
 print "Final report for yaml outputs: if succeeded %53s" % "max diff (significant epsilon)"
 for file in yaml_files:
     dirc = os.path.normpath(os.path.dirname(file))
     fic = "(%s)" % os.path.basename(file)
-    dirfic = ("%-27s %-38s" % (dirc,fic)).strip()
+    dirfic = ("%-35s %-30s" % (dirc.replace('.test',''),fic.replace('.report.yaml',''))).strip()
     documents=[a for a in yaml.load_all(open(file, "r").read(), Loader = yaml.CLoader)]
     #find whether all the tests have passed (look at last part)
     try:
