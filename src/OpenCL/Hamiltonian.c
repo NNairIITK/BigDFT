d462d0446b088d47298401c528330e77a0c6fb4e
Damien Caliste <caliste@pilipili.intra.cea.fr>
2014-02-10 11:48:23 +0100

    Use FC_FUNC when necessary, ↑Ð”Û¢ÇÇ↑“™↑Ÿ™ÀÉ” !!!!!!!!

--- src/OpenCL/Hamiltonian.c
+++ src/OpenCL/Hamiltonian.c
@@ -21,15 +21,15 @@ void FC_FUNC_(ocl_fulllocham,OCL_FULLLOCHAM)(bigdft_command_queue *command_queue
                                           cl_mem *psi, cl_mem *out,
                                           cl_mem *work, cl_mem *kinres,
                                           cl_double *epot,cl_double *ekinpot) {
-  uncompress_d_(command_queue, dimensions,
+  FC_FUNC_(uncompress_d, UNCOMPRESS_D)(command_queue, dimensions,
                                nseg_c, nvctr_c, keyg_c, keyv_c,
                                nseg_f, nvctr_f, keyg_f, keyv_f,
                                psi_c, psi_f, out);
-  syn_self_d_(command_queue, dimensions, out, psi);
-  potential_application_d_(command_queue, dimensions, work, psi, out, pot);
-  kinetic_d_(command_queue, dimensions, h, psi, out, work, kinres);
-  ana_self_d_(command_queue, dimensions, kinres, psi);
-  compress_d_(command_queue, dimensions,
+  FC_FUNC_(syn_self_d, SYN_SELF_D)(command_queue, dimensions, out, psi);
+  FC_FUNC_(potential_application_d, POTENTIAL_APPLICATION_D)(command_queue, dimensions, work, psi, out, pot);
+  FC_FUNC_(kinetic_d, KINETIC_D)(command_queue, dimensions, h, psi, out, work, kinres);
+  FC_FUNC_(ana_self_d, ANA_SELF_D)(command_queue, dimensions, kinres, psi);
+  FC_FUNC_(compress_d, COMPRESS_D)(command_queue, dimensions,
 	      nseg_c, nvctr_c, keyg_c, keyv_c,
 	      nseg_f, nvctr_f, keyg_f, keyv_f,
 	      psi_c, psi_f, psi);
@@ -49,13 +49,13 @@ void FC_FUNC_(ocl_fulllocham_generic,OCL_FULLLOCHAM_GENERIC)(bigdft_command_queu
                                           cl_mem *psi, cl_mem *out,
                                           cl_mem *work, cl_mem *kinres,
                                           cl_double *epot,cl_double *ekinpot) {
-  uncompress_d_(command_queue, dimensions,
+  FC_FUNC_(uncompress_d, UNCOMPRESS_D)(command_queue, dimensions,
                                nseg_c, nvctr_c, keyg_c, keyv_c,
                                nseg_f, nvctr_f, keyg_f, keyv_f,
                                psi_c, psi_f, out);
-  syn_self_d_generic_(command_queue, dimensions, periodic, out, psi);
-  potential_application_d_generic_(command_queue, dimensions, periodic, work, kinres, psi, out, pot, epot);
-  kinetic_d_generic_(command_queue, dimensions, periodic, h, psi, out, work, kinres);
+  FC_FUNC_(syn_self_d_generic, SYN_SELF_D_GENERIC)(command_queue, dimensions, periodic, out, psi);
+  FC_FUNC_(potential_application_d_generic, POTENTIAL_APPLICATION_D_GENERIC)(command_queue, dimensions, periodic, work, kinres, psi, out, pot, epot);
+  FC_FUNC_(kinetic_d_generic, KINETIC_D_GENERIC)(command_queue, dimensions, periodic, h, psi, out, work, kinres);
   cl_uint n1 = dimensions[0] * 2;             
   cl_uint n2 = dimensions[1] * 2;
   cl_uint n3 = dimensions[2] * 2;
@@ -63,9 +63,9 @@ void FC_FUNC_(ocl_fulllocham_generic,OCL_FULLLOCHAM_GENERIC)(bigdft_command_queu
   if( !periodic[1] ) n2 += 2*7;
   if( !periodic[2] ) n3 += 2*7;
   cl_uint ndat = n1*n2*n3;
-  dot_d_async_(command_queue,&ndat, work, kinres, psi, out, ekinpot);
-  ana_self_d_generic_(command_queue, dimensions, periodic, kinres, psi);
-  compress_d_(command_queue, dimensions,
+  FC_FUNC_(dot_d_async, DOT_D_ASYNC)(command_queue,&ndat, work, kinres, psi, out, ekinpot);
+  FC_FUNC_(ana_self_d_generic, ANA_SELF_D_GENERIC)(command_queue, dimensions, periodic, kinres, psi);
+  FC_FUNC_(compress_d, COMPRESS_D)(command_queue, dimensions,
 	      nseg_c, nvctr_c, keyg_c, keyv_c,
 	      nseg_f, nvctr_f, keyg_f, keyv_f,
 	      psi_c, psi_f, psi);
@@ -79,9 +79,9 @@ void FC_FUNC_(ocl_isf_to_daub,OCL_ISF_TO_DAUB)(bigdft_command_queue *command_que
                                           cl_mem *psi_c, cl_mem *psi_f,
                                           cl_mem *psi, cl_mem *out,
                                           cl_mem *work, cl_mem *kinres) {
-  magic_filter_t_3d_generic_(command_queue, dimensions, periodic, work, kinres, psi, out);
-  ana_self_d_generic_(command_queue, dimensions, periodic, out, psi);
-  compress_d_(command_queue, dimensions,
+  FC_FUNC_(magic_filter_t_3d_generic, MAGIC_FILTER_T_3D_GENERIC)(command_queue, dimensions, periodic, work, kinres, psi, out);
+  FC_FUNC_(ana_self_d_generic, ANA_SELF_D_GENERIC)(command_queue, dimensions, periodic, out, psi);
+  FC_FUNC_(compress_d, COMPRESS_D)(command_queue, dimensions,
 	      nseg_c, nvctr_c, keyg_c, keyv_c,
 	      nseg_f, nvctr_f, keyg_f, keyv_f,
 	      psi_c, psi_f, psi);
@@ -94,12 +94,12 @@ void FC_FUNC_(ocl_daub_to_isf,OCL_DAUB_TO_ISF)(bigdft_command_queue *command_que
                                           cl_mem *psi_c, cl_mem *psi_f,
                                           cl_mem *psi, cl_mem *out,
                                           cl_mem *work, cl_mem *kinres) {
-  uncompress_d_(command_queue, dimensions,
+  FC_FUNC_(uncompress_d, UNCOMPRESS_D)(command_queue, dimensions,
                                nseg_c, nvctr_c, keyg_c, keyv_c,
                                nseg_f, nvctr_f, keyg_f, keyv_f,
                                psi_c, psi_f, out);
-  syn_self_d_generic_(command_queue, dimensions, periodic, out, psi);
-  magic_filter_3d_generic_(command_queue, dimensions, periodic, work, kinres, psi, out);
+  FC_FUNC_(syn_self_d_generic, SYN_SELF_D_GENERIC)(command_queue, dimensions, periodic, out, psi);
+  FC_FUNC_(magic_filter_3d_generic, MAGIC_FILTER_3D_GENERIC)(command_queue, dimensions, periodic, work, kinres, psi, out);
 }
 
 void FC_FUNC_(ocl_fulllocham_generic_k,OCL_FULLLOCHAM_GENERIC_K)(bigdft_command_queue *command_queue,
@@ -121,19 +121,19 @@ void FC_FUNC_(ocl_fulllocham_generic_k,OCL_FULLLOCHAM_GENERIC_K)(bigdft_command_
 //                                          double *epot_i,double *ekinpot_i) {
   epot[1] = 0.0;
   ekinpot[1] = 0.0;
-  uncompress_d_(command_queue, dimensions,
+  FC_FUNC_(uncompress_d, UNCOMPRESS_D)(command_queue, dimensions,
                                nseg_c, nvctr_c, keyg_c, keyv_c,
                                nseg_f, nvctr_f, keyg_f, keyv_f,
                                psi_c_r, psi_f_r, out_r);
-  syn_self_d_generic_(command_queue, dimensions, periodic, out_r, psi_r);
-  potential_application_d_generic_(command_queue, dimensions, periodic, work_r, kinres_r, psi_r, out_r, pot, &epot[0]);
+  FC_FUNC_(syn_self_d_generic, SYN_SELF_D_GENERIC)(command_queue, dimensions, periodic, out_r, psi_r);
+  FC_FUNC_(potential_application_d_generic, POTENTIAL_APPLICATION_D_GENERIC)(command_queue, dimensions, periodic, work_r, kinres_r, psi_r, out_r, pot, &epot[0]);
   if(*nspinor==2){
-    uncompress_d_(command_queue, dimensions,
+    FC_FUNC_(uncompress_d, UNCOMPRESS_D)(command_queue, dimensions,
                                nseg_c, nvctr_c, keyg_c, keyv_c,
                                nseg_f, nvctr_f, keyg_f, keyv_f,
                                psi_c_i, psi_f_i, out_i);
-    syn_self_d_generic_(command_queue, dimensions, periodic, out_i, psi_i);
-    potential_application_d_generic_(command_queue, dimensions, periodic, work_i, kinres_i, psi_i, out_i, pot, &epot[1]);
+    FC_FUNC_(syn_self_d_generic, SYN_SELF_D_GENERIC)(command_queue, dimensions, periodic, out_i, psi_i);
+    FC_FUNC_(potential_application_d_generic, POTENTIAL_APPLICATION_D_GENERIC)(command_queue, dimensions, periodic, work_i, kinres_i, psi_i, out_i, pot, &epot[1]);
   }
 
 
@@ -147,23 +147,23 @@ void FC_FUNC_(ocl_fulllocham_generic_k,OCL_FULLLOCHAM_GENERIC_K)(bigdft_command_
 
   if(*nspinor==2) {
     double c = .5 * (k[0] * k[0] + k[1] * k[1] + k[2] * k[2]);
-    axpy_self_d_(command_queue, &ndat, &c, psi_r, out_r);
-    axpy_self_d_(command_queue, &ndat, &c, psi_i, out_i);
-    kinetic_k_d_generic_(command_queue, dimensions, periodic, h, k, psi_r, psi_i, out_r, out_i, work_r, work_i, kinres_r, kinres_i);
+    FC_FUNC_(axpy_self_d, AXPY_SELF_D)(command_queue, &ndat, &c, psi_r, out_r);
+    FC_FUNC_(axpy_self_d, AXPY_SELF_D)(command_queue, &ndat, &c, psi_i, out_i);
+    FC_FUNC_(kinetic_k_d_generic, KINETIC_K_D_GENERIC)(command_queue, dimensions, periodic, h, k, psi_r, psi_i, out_r, out_i, work_r, work_i, kinres_r, kinres_i);
   } else {
-    kinetic_d_generic_(command_queue, dimensions, periodic, h, psi_r, out_r, work_r, kinres_r);
+    FC_FUNC_(kinetic_d_generic, KINETIC_D_GENERIC)(command_queue, dimensions, periodic, h, psi_r, out_r, work_r, kinres_r);
   }
 
-  dot_d_async_(command_queue,&ndat, work_r, kinres_r, psi_r, out_r, &ekinpot[0]);
-  ana_self_d_generic_(command_queue, dimensions, periodic, kinres_r, psi_r);
-  compress_d_(command_queue, dimensions,
+  FC_FUNC_(dot_d_async, DOT_D_ASYNC)(command_queue,&ndat, work_r, kinres_r, psi_r, out_r, &ekinpot[0]);
+  FC_FUNC_(ana_self_d_generic, ANA_SELF_D_GENERIC)(command_queue, dimensions, periodic, kinres_r, psi_r);
+  FC_FUNC_(compress_d, COMPRESS_D)(command_queue, dimensions,
 	      nseg_c, nvctr_c, keyg_c, keyv_c,
 	      nseg_f, nvctr_f, keyg_f, keyv_f,
 	      psi_c_r, psi_f_r, psi_r);
   if(*nspinor==2){
-    dot_d_async_(command_queue,&ndat, work_i, kinres_i, psi_i, out_i, &ekinpot[1]);
-    ana_self_d_generic_(command_queue, dimensions, periodic, kinres_i, psi_i);
-    compress_d_(command_queue, dimensions,
+    FC_FUNC_(dot_d_async, DOT_D_ASYNC)(command_queue,&ndat, work_i, kinres_i, psi_i, out_i, &ekinpot[1]);
+    FC_FUNC_(ana_self_d_generic, ANA_SELF_D_GENERIC)(command_queue, dimensions, periodic, kinres_i, psi_i);
+    FC_FUNC_(compress_d, COMPRESS_D)(command_queue, dimensions,
 	      nseg_c, nvctr_c, keyg_c, keyv_c,
 	      nseg_f, nvctr_f, keyg_f, keyv_f,
 	      psi_c_i, psi_f_i, psi_i);
