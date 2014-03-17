90b014a2c2fac265cd205477d0661958233da3c8
Stephan Mohr <>
2014-03-17 11:03:52 +0100

    removed the file src/linear/sparse_matrices.f90

--- src/cluster.f90
+++ src/cluster.f90
@@ -247,6 +247,7 @@ subroutine cluster(nproc,iproc,atoms,rxyz,radii_cf,energy,energs,fxyz,strten,fno
   use psp_projectors
   use sparsematrix_base, only: sparse_matrix_null
   use sparsematrix_init, only: init_sparse_matrix, init_sparsity_from_distance, check_kernel_cutoff
+  use sparsematrix, only: check_matrix_compression
   implicit none
   integer, intent(in) :: nproc,iproc
   real(gp), intent(inout) :: hx_old,hy_old,hz_old
