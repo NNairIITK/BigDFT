a4fd730cfcc051815f85b4e105a3a14250626ee3
Laura Ratcliff <lratcliff@kyuss.mcs.anl.gov>
2014-04-17 15:56:16 -0500

    allowing negative values for Taylor order for new method

--- src/modules/input.f90
+++ src/modules/input.f90
@@ -2006,12 +2006,12 @@ contains
 
     comments='number of iterations in the preconditioner, order of Taylor approximations'
     call input_var(dummy_int,'5',dict//LIN_BASIS//NSTEP_PREC,ranges=(/1,100/))
-    call input_var(dummy_int,'1',dict//LIN_GENERAL//TAYLOR_ORDER,ranges=(/1,100/),comment=comments)
+    call input_var(dummy_int,'1',dict//LIN_GENERAL//TAYLOR_ORDER,ranges=(/-100,100/),comment=comments)
     !call input_var(in%lin%order_taylor,'1',ranges=(/1,100/),comment=comments)
 
     comments = '0-> exact Loewdin, 1-> taylor expansion; &
                &in orthoconstraint: correction for non-orthogonality (0) or no correction (1)'
-    call input_var(dummy_int,'1',dict//LIN_GENERAL//TAYLOR_ORDER,ranges=(/-100,10000/))
+    call input_var(dummy_int,'1',dict//LIN_GENERAL//TAYLOR_ORDER,ranges=(/-100,100/))
     call input_var(dummy_int,'1',ranges=(/0,1/),comment=comments)
     !call input_var(in%lin%correctionOrthoconstraint,'1',ranges=(/0,1/),comment=comments)
 
