2eb4482192ed17140340a6de0724d0e7d54a14ad
Stephan Mohr <>
2014-04-18 07:39:15 +0200

    corrected a small bug

--- src/linear/initAndUtils.f90
+++ src/linear/initAndUtils.f90
@@ -1946,7 +1946,7 @@ subroutine determine_sparsity_pattern_distance(orbs, lzd, astruct, cutoff, nnonz
 
   call f_routine('determine_sparsity_pattern_distance')
 
-      ! NEW
+      nnonzero=0
       do iorb=1,orbs%norbp
          iiorb=orbs%isorb+iorb
          ilr=orbs%inwhichlocreg(iiorb)
