32beba8e8ddbccb83b881850b1e38d0d69daf80b
Stephan Mohr <>
2014-04-30 13:16:14 +0200

    some yaml correction

--- src/linear/getLocBasis.f90
+++ src/linear/getLocBasis.f90
@@ -708,12 +708,14 @@ subroutine getLocalizedBasis(iproc,nproc,at,orbs,rxyz,denspot,GPU,trH,trH_old,&
               ! This can then be deleted if the transition to the new type has been completed.
               !tmb%linmat%ovrlp%matrix_compr=tmb%linmat%ovrlp_%matrix_compr
               if (iproc==0) call yaml_newline()
-              if (iproc==0) call yaml_open_map(flow=.true.)
+              !if (iproc==0) call yaml_open_map(flow=.true.)
+              if (iproc==0) call yaml_open_sequence('kernel update by FOE')
               call foe(iproc, nproc, 0.d0, &
                    energs%ebs, -1, -10, order_taylor, purification_quickreturn, adjust_FOE_temperature, 0, &
                    FOE_FAST, tmb)
               !tmb%linmat%denskern_large%matrix_compr = tmb%linmat%kernel_%matrix_compr
-              if (iproc==0) call yaml_close_map()
+              !if (iproc==0) call yaml_close_map()
+              if (iproc==0) call yaml_close_sequence()
               if (.not.associated_psit_c) then
                   iall=-product(shape(tmb%psit_c))*kind(tmb%psit_c)
                   deallocate(tmb%psit_c, stat=istat)
