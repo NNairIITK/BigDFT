ee7b89a7c10fd53350a7abc41f50adcc3940a723
Stephan Mohr <>
2014-04-30 18:44:13 +0200

    removed the variable nItOrtho

--- src/init.f90
+++ src/init.f90
@@ -1674,7 +1674,6 @@ subroutine input_wf(iproc,nproc,in,GPU,atoms,rxyz,&
   if (inputpsi == INPUT_PSI_LINEAR_AO .or. inputpsi == INPUT_PSI_DISK_LINEAR &
       .or. inputpsi == INPUT_PSI_MEMORY_LINEAR) then
      tmb%orthpar%methTransformOverlap = in%lin%methTransformOverlap
-     tmb%orthpar%nItOrtho = 1
      tmb%orthpar%blocksize_pdsyev = in%lin%blocksize_pdsyev
      tmb%orthpar%blocksize_pdgemm = in%lin%blocksize_pdgemm
      tmb%orthpar%nproc_pdsyev = in%lin%nproc_pdsyev
