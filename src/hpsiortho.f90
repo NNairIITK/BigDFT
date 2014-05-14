fd2884e5d2241986e3cf121c989b497436e002d7
Stephan Mohr <>
2014-05-14 12:43:43 +0200

    transformed src/hpsiortho.f90 to f_malloc

--- src/hpsiortho.f90
+++ src/hpsiortho.f90
@@ -604,8 +604,7 @@ subroutine LocalHamiltonianApplication(iproc,nproc,at,npsidim_orbs,orbs,&
    !       call random_number(psi(i))
    !  end do
    if(GPU%OCLconv .or. GPUconv) then! needed also in the non_ASYNC since now NlPSP is before .and. ASYNCconv)) then
-      allocate(GPU%hpsi_ASYNC(max(1,(Lzd%Glr%wfd%nvctr_c+7*Lzd%Glr%wfd%nvctr_f)*orbs%nspinor*orbs%norbp)),stat=i_stat)
-      call memocc(i_stat,GPU%hpsi_ASYNC,'GPU%hpsi_ASYNC',subname)
+      GPU%hpsi_ASYNC = f_malloc_ptr(max(1, (Lzd%Glr%wfd%nvctr_c+7*Lzd%Glr%wfd%nvctr_f)*orbs%nspinor*orbs%norbp),id='GPU%hpsi_ASYNC')
 !      call to_zero((Lzd%Glr%wfd%nvctr_c+7*Lzd%Glr%wfd%nvctr_f)*orbs%nspinor*orbs%norbp,GPU%hpsi_ASYNC(1))!hpsi(1))
    !else if (GPU%OCLconv) then
    !   GPU%hpsi_ASYNC => hpsi
@@ -1043,9 +1042,7 @@ subroutine SynchronizeHamiltonianApplication(nproc,npsidim_orbs,orbs,Lzd,GPU,xc,
          ispsi=ispsi+&
              (Lzd%Llr(ilr)%wfd%nvctr_c+7*Lzd%Llr(ilr)%wfd%nvctr_f)*orbs%nspinor
       end do
-      i_all=-product(shape(GPU%hpsi_ASYNC))*kind(GPU%hpsi_ASYNC)
-      deallocate(GPU%hpsi_ASYNC,stat=i_stat)
-      call memocc(i_stat,i_all,'GPU%hpsi_ASYNC',subname)
+      call f_free_ptr(GPU%hpsi_ASYNC)
    endif
 
    exctX = xc_exctXfac(xc) /= 0.0_gp
@@ -1200,8 +1197,7 @@ subroutine full_local_potential(iproc,nproc,orbs,Lzd,iflag,dpbox,xc,potential,po
    call timing(iproc,'Pot_after_comm','ON')
    
    if(Lzd%nlr > 1) then
-      allocate(ilrtable(orbs%norbp),stat=i_stat)
-      call memocc(i_stat,ilrtable,'ilrtable',subname)
+      ilrtable = f_malloc(orbs%norbp,id='ilrtable')
       !call to_zero(orbs%norbp*2,ilrtable(1,1))
       ilrtable=0
       ii=0
@@ -1230,8 +1226,7 @@ subroutine full_local_potential(iproc,nproc,orbs,Lzd,iflag,dpbox,xc,potential,po
       !number of inequivalent potential regions
       nilr = ii
    else 
-      allocate(ilrtable(1),stat=i_stat)
-      call memocc(i_stat,ilrtable,'ilrtable',subname)
+      ilrtable = f_malloc(1,id='ilrtable')
       nilr = 1
       ilrtable=1
    end if
@@ -1319,9 +1314,7 @@ subroutine full_local_potential(iproc,nproc,orbs,Lzd,iflag,dpbox,xc,potential,po
       end if
    end if
 
-   i_all=-product(shape(ilrtable))*kind(ilrtable)
-   deallocate(ilrtable,stat=i_stat)
-   call memocc(i_stat,i_all,'ilrtable',subname)
+   call f_free(ilrtable)
 
    ! Deallocate pot.
    if (iflag<2 .and. iflag>0) then
@@ -1436,8 +1429,7 @@ subroutine calculate_energy_and_gradient(iter,iproc,nproc,GPU,ncong,iscf,&
 
   !calculate orbital polarisation directions
   if(wfn%orbs%nspinor==4) then
-     allocate(mom_vec(4,wfn%orbs%norb,min(nproc,2)+ndebug),stat=i_stat)
-     call memocc(i_stat,mom_vec,'mom_vec',subname)
+     mom_vec = f_malloc_ptr((/ 4, wfn%orbs%norb, min(nproc, 2) /),id='mom_vec')
 
      call calc_moments(iproc,nproc,wfn%orbs%norb,wfn%orbs%norb_par,&
           wfn%Lzd%Glr%wfd%nvctr_c+7*wfn%Lzd%Glr%wfd%nvctr_f,wfn%orbs%nspinor,wfn%psi,mom_vec)
@@ -1625,9 +1617,7 @@ subroutine calculate_energy_and_gradient(iter,iproc,nproc,GPU,ncong,iscf,&
         end do
         call yaml_close_sequence()
      end if
-     i_all=-product(shape(mom_vec))*kind(mom_vec)
-     deallocate(mom_vec,stat=i_stat)
-     call memocc(i_stat,i_all,'mom_vec',subname)
+     call f_free_ptr(mom_vec)
   end if
 
 
@@ -1810,16 +1800,11 @@ contains
      if (nproc > 1) then
 
    !   Allocate temporary arrays
-       allocate(ndsplt(0:nproc-1),stat=i_stat)
-       call memocc(i_stat,ndsplt,'ndsplt',subname)
-       allocate(ncntd(0:nproc-1),stat=i_stat)
-       call memocc(i_stat,ncntd,'ncntd',subname)
-       allocate(ncntt(0:nproc-1),stat=i_stat)
-       call memocc(i_stat,ncntt,'ncntt',subname)
-       allocate(raux(2,paw%lmnmax,paw%natom,wfn%orbs%norbp),stat=i_stat)
-       call memocc(i_stat,raux,'raux',subname)
-       allocate(raux2(2,paw%lmnmax,paw%natom,wfn%orbs%norb),stat=i_stat)
-       call memocc(i_stat,raux,'raux2',subname)
+       ndsplt = f_malloc(0.to.nproc-1,id='ndsplt')
+       ncntd = f_malloc(0.to.nproc-1,id='ncntd')
+       ncntt = f_malloc(0.to.nproc-1,id='ncntt')
+       raux = f_malloc((/ 2, paw%lmnmax, paw%natom, wfn%orbs%norbp /),id='raux')
+       raux2 = f_malloc((/ 2, paw%lmnmax, paw%natom, wfn%orbs%norb /),id='raux2')
 
    !   Set tables for mpi operations:
    !   Send buffer:
@@ -1883,21 +1868,11 @@ contains
          end do
        end do
    !   Deallocate arrays:
-       i_all=-product(shape(ndsplt))*kind(ndsplt)
-       deallocate(ndsplt,stat=i_stat)
-       call memocc(i_stat,i_all,'ndsplt',subname)
-       i_all=-product(shape(ncntd))*kind(ncntd)
-       deallocate(ncntd,stat=i_stat)
-       call memocc(i_stat,i_all,'ncntd',subname)
-       i_all=-product(shape(ncntt))*kind(ncntt)
-       deallocate(ncntt,stat=i_stat)
-       call memocc(i_stat,i_all,'ncntt',subname)
-       i_all=-product(shape(raux))*kind(raux)
-       deallocate(raux,stat=i_stat)
-       call memocc(i_stat,i_all,'raux',subname)
-       i_all=-product(shape(raux2))*kind(raux2)
-       deallocate(raux2,stat=i_stat)
-       call memocc(i_stat,i_all,'raux2',subname)
+       call f_free(ndsplt)
+       call f_free(ncntd)
+       call f_free(ncntt)
+       call f_free(raux)
+       call f_free(raux2)
      end if
 
 
@@ -2058,8 +2033,7 @@ subroutine eigensystem_info(iproc,nproc,tolerance,nvctr,orbs,psi)
   !we add the calculation of the moments for printing their value
   !close to the corresponding eigenvector
   if(orbs%nspinor==4) then
-     allocate(mom_vec(4,orbs%norb,min(nproc,2)+ndebug),stat=i_stat)
-     call memocc(i_stat,mom_vec,'mom_vec',subname)
+     mom_vec = f_malloc_ptr((/ 4, orbs%norb, min(nproc, 2) /),id='mom_vec')
 
      call calc_moments(iproc,nproc,orbs%norb,orbs%norb_par,&
           nvctr,&
@@ -2080,9 +2054,7 @@ subroutine eigensystem_info(iproc,nproc,tolerance,nvctr,orbs,psi)
   end if
 
   if (orbs%nspinor ==4) then
-     i_all=-product(shape(mom_vec))*kind(mom_vec)
-     deallocate(mom_vec,stat=i_stat)
-     call memocc(i_stat,i_all,'mom_vec',subname)
+     call f_free_ptr(mom_vec)
   end if
 end subroutine eigensystem_info
 
@@ -2447,8 +2419,7 @@ subroutine calc_moments(iproc,nproc,norb,norb_par,nvctr,nspinor,psi,mom_vec)
       end do
 
       if(nproc>1) then
-         allocate(norb_displ(0:nproc-1+ndebug),stat=i_stat)
-         call memocc(i_stat,norb_displ,'norb_displ',subname)
+         norb_displ = f_malloc(0.to.nproc-1,id='norb_displ')
 
          norb_displ(0)=0
          do jproc=1,nproc-1
@@ -2459,9 +2430,7 @@ subroutine calc_moments(iproc,nproc,norb,norb_par,nvctr,nspinor,psi,mom_vec)
             &   mom_vec(1,1,1),4*norb_par,4*norb_displ,mpidtypw,&
          0,bigdft_mpi%mpi_comm,ierr)
 
-         i_all=-product(shape(norb_displ))*kind(norb_displ)
-         deallocate(norb_displ,stat=i_stat)
-         call memocc(i_stat,i_all,'norb_displ',subname)
+         call f_free(norb_displ)
       end if
 
    end if
@@ -2494,10 +2463,8 @@ subroutine check_communications(iproc,nproc,orbs,lzd,comms)
 
 
    !allocate the "wavefunction" amd fill it, and also the workspace
-   allocate(psi(max(orbs%npsidim_orbs,orbs%npsidim_comp)+ndebug),stat=i_stat)
-   call memocc(i_stat,psi,'psi',subname)
-   allocate(pwork(max(orbs%npsidim_orbs,orbs%npsidim_comp)+ndebug),stat=i_stat)
-   call memocc(i_stat,pwork,'pwork',subname)
+   psi = f_malloc(max(orbs%npsidim_orbs, orbs%npsidim_comp),id='psi')
+   pwork = f_malloc_ptr(max(orbs%npsidim_orbs, orbs%npsidim_comp),id='pwork')
 
 
    do iorb=1,orbs%norbp
@@ -2673,12 +2640,8 @@ subroutine check_communications(iproc,nproc,orbs,lzd,comms)
    if (abort) call MPI_ABORT(bigdft_mpi%mpi_comm,ierr)
    call MPI_BARRIER(bigdft_mpi%mpi_comm, ierr)
 
-   i_all=-product(shape(psi))*kind(psi)
-   deallocate(psi,stat=i_stat)
-   call memocc(i_stat,i_all,'psi',subname)
-   i_all=-product(shape(pwork))*kind(pwork)
-   deallocate(pwork,stat=i_stat)
-   call memocc(i_stat,i_all,'pwork',subname)
+   call f_free(psi)
+   call f_free_ptr(pwork)
 
 END SUBROUTINE check_communications
 
