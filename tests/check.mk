82ab89e2034d920b7c85a62e59706de381deacdc
Damien Caliste <damien.caliste@cea.fr>
2014-05-14 14:42:05 +0200

    Correct issues with toy_model compilation.

--- tests/check.mk
+++ tests/check.mk
@@ -32,7 +32,7 @@ if BUILD_DYNAMIC_LIBS
 LD_LIBRARY_PATH := ${LD_LIBRARY_PATH}:$(abs_top_builddir)/src
 endif
 
-AM_FCFLAGS = -I$(top_builddir)/includes -I$(top_srcdir)/src/PSolver -I. @LIBABINIT_INCLUDE@ @LIBXC_INCLUDE@  @MPI_INCLUDE@
+AM_FCFLAGS = -I$(top_builddir)/includes -I$(top_srcdir)/PSolver/src -I. @LIBABINIT_INCLUDE@ @LIBXC_INCLUDE@  @MPI_INCLUDE@
 
 PSPS = psppar.H \
        psppar.C \
