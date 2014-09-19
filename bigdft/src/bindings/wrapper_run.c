ac533931ec9b71ddb4ed5ec884305e72bd3d8906
Damien Caliste <damien.caliste@cea.fr>
2014-09-19 15:43:31 +0200

    iCorrect memory leak with dictionnary.

--- bigdft/src/bindings/wrapper_run.c
+++ bigdft/src/bindings/wrapper_run.c
@@ -344,18 +344,11 @@ BigDFT_Run* bigdft_run_new_from_files(const gchar *radical, const gchar *posinp)
 BigDFT_Run* bigdft_run_new_from_dict(BigDFT_Dict *dict)
 {
   BigDFT_Run *run;
-  f90_dictionary_pointer copy;
 
   run = bigdft_run_new();
 
-  /* Make a copy of the given dictionary. */
-  FC_FUNC_(dict_init_binding, DICT_INIT_BINDING)(&copy);
-  FC_FUNC_(dict_copy_binding, DICT_COPY_BINDING)(&copy, &dict->root);
-
   /* Associate the dictionary and parse it. */
-  FC_FUNC_(run_objects_update, RUN_OBJECTS_UPDATE)(F_TYPE(run->data), &copy);
-  //FC_FUNC_(run_objects_set_dict, RUN_OBJECTS_SET_DICT)(F_TYPE(run->data), &copy);
-  //FC_FUNC_(set_run_objects, SET_RUN_OBJECTS)(F_TYPE(run->data));
+  FC_FUNC_(run_objects_update, RUN_OBJECTS_UPDATE)(F_TYPE(run->data), &dict->root);
 
   _attributes_from_fortran(run);
 
