dd50f16beab68d3c09e1c86b9031622f1e293a0e
Stephan Mohr <>
2014-09-19 08:13:46 +0200

    minor modification

--- src/linear/init_collective_comms.f90
+++ src/linear/init_collective_comms.f90
@@ -846,39 +846,39 @@ subroutine calculate_overlap_transposed(iproc, nproc, orbs, collcom, &
 
   call timing(iproc,'ovrlptransComm','ON') !lr408t
 
-  if(nproc > 1) then
-      call mpiallred(ovrlp%matrix_compr(1), smat%nvctr*smat%nspin, mpi_sum, bigdft_mpi%mpi_comm)
-  end if
+  !!if(nproc > 1) then
+  !!    call mpiallred(ovrlp%matrix_compr(1), smat%nvctr*smat%nspin, mpi_sum, bigdft_mpi%mpi_comm)
+  !!end if
 
-  !!ncount = 0
-  !!do itg=1,smat%ntaskgroupp
-  !!    iitg = smat%inwhichtaskgroup(itg)
-  !!    ncount = ncount + smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
-  !!end do
-  !!recvbuf = f_malloc(ncount,id='recvbuf')
+  ncount = 0
+  do itg=1,smat%ntaskgroupp
+      iitg = smat%inwhichtaskgroup(itg)
+      ncount = ncount + smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
+  end do
+  recvbuf = f_malloc(ncount,id='recvbuf')
 
-  !!ncount = 0
-  !!request = f_malloc(smat%ntaskgroupp,id='request')
-  !!do itg=1,smat%ntaskgroupp
-  !!    iitg = smat%inwhichtaskgroup(itg)
-  !!    ist_send = smat%taskgroup_startend(1,1,iitg)
-  !!    ist_recv = ncount + 1
-  !!    ncount = smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
-  !!    call mpi_iallreduce(ovrlp%matrix_compr(ist_send), recvbuf(ist_recv), ncount, &
-  !!         mpi_double_precision, mpi_sum, smat%mpi_groups(iitg)%mpi_comm, request(itg), ierr)
-  !!end do
-  !!call mpi_waitall(smat%ntaskgroupp, request, mpi_statuses_ignore, ierr)
-  !!ncount = 0
-  !!do itg=1,smat%ntaskgroupp
-  !!    iitg = smat%inwhichtaskgroup(itg)
-  !!    ist_send = smat%taskgroup_startend(1,1,iitg)
-  !!    ist_recv = ncount + 1
-  !!    ncount = smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
-  !!    call vcopy(ncount, recvbuf(ist_recv), 1, ovrlp%matrix_compr(ist_send), 1)
-  !!end do
+  ncount = 0
+  request = f_malloc(smat%ntaskgroupp,id='request')
+  do itg=1,smat%ntaskgroupp
+      iitg = smat%inwhichtaskgroup(itg)
+      ist_send = smat%taskgroup_startend(1,1,iitg)
+      ist_recv = ncount + 1
+      ncount = smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
+      call mpi_iallreduce(ovrlp%matrix_compr(ist_send), recvbuf(ist_recv), ncount, &
+           mpi_double_precision, mpi_sum, smat%mpi_groups(iitg)%mpi_comm, request(itg), ierr)
+  end do
+  call mpi_waitall(smat%ntaskgroupp, request, mpi_statuses_ignore, ierr)
+  ncount = 0
+  do itg=1,smat%ntaskgroupp
+      iitg = smat%inwhichtaskgroup(itg)
+      ist_send = smat%taskgroup_startend(1,1,iitg)
+      ist_recv = ncount + 1
+      ncount = smat%taskgroup_startend(2,1,iitg)-smat%taskgroup_startend(1,1,iitg)+1
+      call vcopy(ncount, recvbuf(ist_recv), 1, ovrlp%matrix_compr(ist_send), 1)
+  end do
 
-  !!call f_free(request)
-  !!call f_free(recvbuf)
+  call f_free(request)
+  call f_free(recvbuf)
 
 
   smat%can_use_dense=.false.
