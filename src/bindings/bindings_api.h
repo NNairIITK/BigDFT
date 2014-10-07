99c4241b510dbb1b998cda3e2729289242efc2ad
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2014-10-07 11:27:10 +0200

    Correct run_objects_dump_to_file for bound-checks with gfortran

--- src/bindings/bindings_api.h
+++ src/bindings/bindings_api.h
@@ -2660,7 +2660,7 @@ type(run_objects), pointer :: runObj
 void FC_FUNC_(run_objects_destroy, RUN_OBJECTS_DESTROY)(f90_run_objects_pointer *runObj);
 /* run_objects_dump_to_file src/bindings/bindingsf.f90:1402 */
 /* Fortran header:
-subroutine run_objects_dump_to_file(iostat, dict, fname, userOnly)
+subroutine run_objects_dump_to_file(iostat, dict, fname, userOnly,ln)
 use dictionaries, only: dictionary
 use module_input_keys, only: input_keys_dump
 use module_defs, only: UNINITIALIZED, gp
@@ -2671,16 +2671,13 @@ integer, intent(out) :: iostat
 type(dictionary), pointer :: dict
 character(len = ln), intent(in) :: fname
 logical, intent(in) :: userOnly
-
-integer, parameter :: iunit = 145214 
-integer :: iunit_def
-real(gp), dimension(3), parameter :: dummy = (/ 0._gp, 0._gp, 0._gp /)
 */
 void FC_FUNC_(run_objects_dump_to_file, RUN_OBJECTS_DUMP_TO_FILE)(int *iostat, 
                                                                   f90_dictionary_pointer *dict, 
                                                                   const char *fname, 
                                                                   const int *userOnly, 
-                                                                  const int* str_ln_1);
+                                                                  const int *ln,
+                                                                  int str_ln_1);
 /* run_objects_get src/bindings/bindingsf.f90:1389 */
 /* Fortran header:
 subroutine run_objects_get(runObj, dict, inputs, atoms)
