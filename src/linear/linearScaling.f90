1e9f3f0657fd991c08d3b0bf7766733fbca561f6
Stephan Mohr <>
2014-10-29 15:43:39 +0100

    some small optimizations, references need to be updated

--- src/linear/linearScaling.f90
+++ src/linear/linearScaling.f90
@@ -306,10 +306,14 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
              ! NB nothing is written to screen for this get_coeff
              if (.not. input%lin%constrained_dft) then
                 ! Check whether the overlap matrix must be calculated and inverted (otherwise it has already been done)
-                calculate_overlap = ((update_phi .and. .not.input%correction_co_contra) .or. cur_it_highaccuracy==1)
-                invert_overlap_matrix = ((input%method_updatekernel/=UPDATE_BY_FOE .and. &
-                                         input%method_updatekernel/=UPDATE_BY_RENORMALIZATION) .or. &
-                                         cur_it_highaccuracy==1)
+                !!calculate_overlap = ((update_phi .and. .not.input%correction_co_contra) .or. cur_it_highaccuracy==1)
+                !!invert_overlap_matrix = ((input%method_updatekernel/=UPDATE_BY_FOE .and. &
+                !!                         input%method_updatekernel/=UPDATE_BY_RENORMALIZATION) .or. &
+                !!                         cur_it_highaccuracy==1)
+                calculate_overlap = ((update_phi .and. .not.input%correction_co_contra))! .or. cur_it_highaccuracy==1)
+                invert_overlap_matrix = (.not.(target_function==TARGET_FUNCTION_IS_HYBRID .and. &
+                                          (input%method_updatekernel==UPDATE_BY_FOE .or. &
+                                         input%method_updatekernel==UPDATE_BY_RENORMALIZATION)))
                 call get_coeff(iproc,nproc,input%lin%scf_mode,KSwfn%orbs,at,rxyz,denspot,GPU,&
                      infoCoeff,energs,nlpsp,input%SIC,tmb,pnrm,calculate_overlap,invert_overlap_matrix,update_phi,&
                      .true.,input%lin%extra_states,itout,0,0,norder_taylor,input%lin%max_inversion_error,&
@@ -591,10 +595,12 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
                 call yaml_comment('kernel iter:'//yaml_toa(it_scc,fmt='(i6)'),hfill='-')
              end if
              ! Check whether the overlap matrix must be calculated and inverted (otherwise it has already been done)
-             calculate_overlap = ((update_phi .and. .not.input%correction_co_contra) .or. cur_it_highaccuracy==1)
-             invert_overlap_matrix = ((input%method_updatekernel/=UPDATE_BY_FOE .and. &
-                                      input%method_updatekernel/=UPDATE_BY_RENORMALIZATION) .or. &
-                                      cur_it_highaccuracy==1)
+             calculate_overlap = ((update_phi .and. .not.input%correction_co_contra))! .or. cur_it_highaccuracy==1)
+             invert_overlap_matrix = (.not.(target_function==TARGET_FUNCTION_IS_HYBRID .and. &
+                                       (input%method_updatekernel==UPDATE_BY_FOE .or. &
+                                      input%method_updatekernel==UPDATE_BY_RENORMALIZATION)) .and. &
+                                      it_scc==1)
+                                      !cur_it_highaccuracy==1)
              if(update_phi .and. can_use_ham) then! .and. info_basis_functions>=0) then
                 if (input%lin%constrained_dft) then
                    call get_coeff(iproc,nproc,input%lin%scf_mode,KSwfn%orbs,at,rxyz,denspot,GPU,&
