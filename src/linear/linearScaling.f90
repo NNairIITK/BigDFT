536cc8a8338b1596cd90af6fd5defce6736ea82f
Stephan Mohr <>
2014-11-17 14:31:16 +0100

    correction to prevent integer overflow

--- src/linear/linearScaling.f90
+++ src/linear/linearScaling.f90
@@ -78,6 +78,7 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
   type(matrices) :: weight_matrix_
   real(kind=8) :: sign_of_energy_change
   integer :: nit_energyoscillation
+  integer(kind=8) :: nsize
 
   real(8),dimension(:),allocatable :: rho_tmp, tmparr
   real(8) :: tt, ddot
@@ -817,7 +818,8 @@ subroutine linearScaling(iproc,nproc,KSwfn,tmb,at,input,rxyz,denspot,rhopotold,n
                    end if
                    !pnrm_out = pnrm_out/dble(input%nspin)
 
-                   pnrm_out=sqrt(pnrm_out)/(KSwfn%Lzd%Glr%d%n1i*KSwfn%Lzd%Glr%d%n2i*KSwfn%Lzd%Glr%d%n3i)
+                   nsize = int(KSwfn%Lzd%Glr%d%n1i,kind=8)*int(KSwfn%Lzd%Glr%d%n2i,kidn=8)*int(KSwfn%Lzd%Glr%d%n3i,kind=8)
+                   pnrm_out=sqrt(pnrm_out)/real(nsize,kind=8)
                    !only want to copy across when CDFT loop has also converged
                    if (.not. input%lin%constrained_dft .or. (ebs-cdft%charge < cdft_charge_thresh)) then
                       call vcopy(max(KSwfn%Lzd%Glr%d%n1i*KSwfn%Lzd%Glr%d%n2i*denspot%dpbox%n3d,1)*input%nspin, &
