63e2c823952994fc8eaa5639bbef4157b12c32a0
Damien Caliste <damien.caliste@cea.fr>
2014-12-03 16:44:26 +0100

    Correct an issue introduced in splinedsaddle.

--- bigdft/src/splinedsaddle.f90
+++ bigdft/src/splinedsaddle.f90
@@ -310,7 +310,7 @@ subroutine givemesaddle(epot_sp,ratsp,fatsp,ifile,nproc,iproc,run_opt,ncount_big
     call bigdft_run_new(run)
 
     if(trim(pnow%hybrid)=='yes') then
-       call bigdft_set_run_properties(run,input_id='ll_input')
+       call bigdft_set_run_properties(run,input_id='ll_input', run_from_files = .true.)
 !!$       call user_dict_from_files(dict,'ll_input','posinp',bigdft_mpi)
 !!$       !call read_input_dict_from_files("ll_input", bigdft_mpi,dict)
 !!$    else
@@ -326,7 +326,6 @@ subroutine givemesaddle(epot_sp,ratsp,fatsp,ifile,nproc,iproc,run_opt,ncount_big
 !!$    call inputs_from_dict(ll_inputs, ll_atoms, dict)
 !!$    call dict_free(dict)
 !!$    call deallocate_atoms_data(ll_atoms)
-    
     !associate the runobjects to the previous restart
     call run_objects_init(ll_runObj,run,source=run_opt)
     !input files have been parsed  
