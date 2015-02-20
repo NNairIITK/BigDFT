a0f7290bb3f71b64e12d00cdbf48c39c621661a6
Damien Caliste <damien.caliste@cea.fr>
2015-02-20 16:03:00 +0100

    Add missing routine for pawdenpot.

--- libABINIT/src/add_libpaw/abi_interfaces_libpaw.F90
+++ libABINIT/src/add_libpaw/abi_interfaces_libpaw.F90
@@ -228,5 +228,105 @@ module abi_interfaces_libpaw
       integer,optional,target,intent(in) :: mpi_atmtab(:)
     end subroutine pawmkrho
  end interface
+
+ interface
+    subroutine pawdensities(compch_sph,cplex,iatom,lmselectin,lmselectout,lm_size,nhat1,nspden,nzlmopt,&
+         &          opt_compch,opt_dens,opt_l,opt_print,pawang,pawprtvol,pawrad,pawrhoij,pawtab,rho1,trho1,&
+         &          one_over_rad2) ! optional
+
+      USE_DEFS
+
+      use m_pawang, only : pawang_type
+      use m_pawrad, only : pawrad_type
+      use m_pawtab, only : pawtab_type
+      use m_pawrhoij, only : pawrhoij_type
+
+      implicit none
+
+      !Arguments ---------------------------------------------
+      !scalars
+      integer,intent(in) :: cplex,iatom,lm_size,nspden,nzlmopt,opt_compch,opt_dens,opt_l,opt_print,pawprtvol
+      ! jmb  real(dp),intent(out) :: compch_sph
+      real(dp),intent(inout) :: compch_sph
+      type(pawang_type),intent(in) :: pawang
+      type(pawrad_type),intent(in) :: pawrad
+      type(pawrhoij_type),intent(in) :: pawrhoij
+      type(pawtab_type),intent(in) :: pawtab
+      !arrays
+      logical,intent(in) :: lmselectin(lm_size)
+      logical,intent(inout) :: lmselectout(lm_size) !vz_i
+      real(dp),intent(in),target,optional :: one_over_rad2(pawrad%mesh_size)
+      real(dp),intent(out) :: nhat1(cplex*pawrad%mesh_size,lm_size,nspden*(1-((opt_dens+1)/2)))
+      real(dp),intent(out) ::  rho1(cplex*pawrad%mesh_size,lm_size,nspden)
+      real(dp),intent(out) :: trho1(cplex*pawrad%mesh_size,lm_size,nspden*(1-(opt_dens/2)))
+    end subroutine pawdensities
+ end interface
+
+ interface
+    subroutine pawdenpot(compch_sph,epaw,epawdc,ipert,ixc,&
+         & my_natom,natom,nspden,ntypat,nzlmopt,option,paw_an,paw_an0,&
+         & paw_ij,pawang,pawprtvol,pawrad,pawrhoij,pawspnorb,pawtab,pawxcdev,spnorbscl,xclevel,xc_denpos,ucvol,znucl,&
+         & mpi_atmtab,mpi_comm_atom,vpotzero) ! optional arguments
+
+      USE_DEFS
+      use m_pawang,  only: pawang_type
+      use m_pawrad,  only: pawrad_type
+      use m_pawtab,  only: pawtab_type
+      use m_paw_an, only : paw_an_type
+      use m_paw_ij, only : paw_ij_type
+      use m_pawrhoij,only: pawrhoij_type
+      ! use m_electronpositron, only: electronpositron_type,electronpositron_calctype
+
+      implicit none
+
+      !Arguments ---------------------------------------------
+      !scalars
+      integer,intent(in) :: ipert,ixc,my_natom,natom,nspden,ntypat,nzlmopt,option,pawprtvol
+      integer,intent(in) :: pawspnorb,pawxcdev,xclevel
+      integer,optional,intent(in) :: mpi_comm_atom 
+      real(dp), intent(in) :: spnorbscl,xc_denpos,ucvol
+      real(dp),intent(out) :: compch_sph,epaw,epawdc
+      ! xg  real(dp),intent(inout) :: compch_sph,epaw,epawdc
+      real(dp),intent(out),optional :: vpotzero(2)
+      ! type(electronpositron_type),pointer,optional :: electronpositron
+      type(pawang_type),intent(in) :: pawang
+      !arrays
+      integer,optional,target,intent(in) :: mpi_atmtab(:)
+      real(dp) :: znucl(ntypat)
+      type(paw_an_type),intent(inout) :: paw_an(my_natom)
+      type(paw_an_type), intent(in) :: paw_an0(my_natom)
+      type(paw_ij_type),intent(inout) :: paw_ij(my_natom)
+      type(pawrad_type),intent(in) :: pawrad(ntypat)
+      type(pawrhoij_type),intent(inout) :: pawrhoij(my_natom)
+      type(pawtab_type),intent(in) :: pawtab(ntypat)
+    end subroutine pawdenpot
+ end interface
+
+ interface
+    subroutine wvl_nhatgrid(atindx1,geocode,h,i3s,natom,natom_tot,&
+         & nattyp,ntypat,n1,n1i,n2,n2i,n3,n3pi,optcut,optgr0,optgr1,optgr2,optrad,&
+         & pawfgrtab,pawtab,shift,rxyz)
+
+      USE_DEFS
+
+      use m_pawtab,       only : pawtab_type
+      use m_pawfgrtab,    only : pawfgrtab_type
+
+      implicit none
+
+      !Arguments ---------------------------------------------
+      !scalars
+      integer,intent(in) :: i3s,natom,natom_tot,ntypat,optcut,optgr0,optgr1,optgr2,optrad
+      integer,intent(in) :: n1,n2,n3,n1i,n2i,n3pi,shift
+      real(dp),intent(in) :: h(3)
+      character(1),intent(in) :: geocode
+      !integer,intent(in),optional :: mpi_comm_wvl
+      !arrays
+      integer,intent(in) :: atindx1(natom),nattyp(ntypat)
+      real(dp),intent(in) :: rxyz(3,natom)
+      type(pawfgrtab_type),intent(inout) :: pawfgrtab(natom)
+      type(pawtab_type),intent(in) :: pawtab(ntypat)
+    end subroutine wvl_nhatgrid
+ end interface
 end module abi_interfaces_libpaw
 !!***
