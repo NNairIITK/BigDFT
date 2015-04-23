4683abf0d147e785f79739c6219879df600e8f8e
Damien Caliste <damien.caliste@cea.fr>
2015-04-23 16:58:43 +0200

    Add missing header.

--- bigdft/config/scripts/bindings_api.sh
+++ bigdft/config/scripts/bindings_api.sh
@@ -6,6 +6,15 @@ omit=`dirname $0`/bindings_api.omit
 API=`grep -rh "^ *FC_FUNC[^#]*$" $1 | cut -d'(' -f2 | cut -d',' -f1 | sort -u | grep -vf $omit`
 GETS=`grep -rh "^  *GET_ATTR" $1 | cut -d'(' -f2 | sed -e "s/ //g" | cut -d',' -f1,3 --output-delimiter="_get_" | sort -u | grep -vf $omit`
 
+echo "/** @file"
+echo " * Bindings for the BigDFT package"
+echo " * @author"
+echo " * Copyright (C) 2013-2015 BigDFT group (DC)"
+echo " * This file is distributed under the terms of the"
+echo " * GNU General Public License, see ~/COPYING file"
+echo " * or http://www.gnu.org/copyleft/gpl.txt ."
+echo " * For the list of contributors, see ~/AUTHORS"
+echo "**/"
 echo "#ifndef BINDINGS_API_H"
 echo "#define BINDINGS_API_H"
 echo
