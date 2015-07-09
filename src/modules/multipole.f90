c66a4743b2dd11ffa5a44c8c4c2a3ab5c35d4f5d
Stephan Mohr <>
2015-07-09 17:28:46 +0200

    modified the output format for the atomic multipoles (now identical to the input)

--- src/modules/multipole.f90
+++ src/modules/multipole.f90
@@ -276,7 +276,7 @@ module multipole
           real(kind=8),intent(in) :: sigma, r
           real(kind=8) :: g
           
-          g = exp(-r**2/(2.d0*sigma**2))
+          g = safe_exp(-r**2/(2.d0*sigma**2))
           g = g/sqrt(2.d0*pi*sigma**2)**3
           !g = g/(sigma**3*sqrt(2.d0*pi)**3)
         end function gaussian
@@ -725,8 +725,10 @@ module multipole
       call f_free(locregcenter)
 
       if (iproc==0) then
-          call write_multipoles(at%astruct%nat, at%astruct%ntypes, at%astruct%iatype, at%astruct%atomnames, &
-               multipoles, rmax, lzd%hgrids, without_normalization=.false.)
+          !!call write_multipoles(at%astruct%nat, at%astruct%ntypes, at%astruct%iatype, at%astruct%atomnames, &
+          !!     multipoles, rmax, lzd%hgrids, without_normalization=.false.)
+          call write_multipoles_new(at%astruct%nat, at%astruct%ntypes, at%astruct%iatype, at%astruct%atomnames, &
+               at%astruct%rxyz, multipoles, rmax, lzd%hgrids, without_normalization=.false.)
       end if
       call f_free(rmax)
       call f_free(psir_get)
@@ -840,7 +842,9 @@ module multipole
           !call mpi_barrier(mpi_comm_world,i3)
 
           if (iproc==0) then
-              call write_multipoles(1, 1, (/1/), (/'testatom'/), multipoles, rmax, lzd%hgrids, without_normalization=.true.)
+              !!call write_multipoles(1, 1, (/1/), (/'testatom'/), multipoles, rmax, lzd%hgrids, without_normalization=.true.)
+              call write_multipoles_new(1, 1, (/1/), (/'testatom'/), (/0.d0,0.d0,0.d0/), &
+                   multipoles, rmax, lzd%hgrids, without_normalization=.true.)
           end if
 
         end subroutine unitary_test
@@ -985,6 +989,92 @@ module multipole
           call f_free(multipoles_tmp)
 
     end subroutine write_multipoles
+
+
+
+    subroutine write_multipoles_new(nat, ntypes, iatype, atomnames, rxyz, multipoles, rmax, hgrids, without_normalization)
+      use yaml_output
+      implicit none
+      
+      ! Calling arguments
+      integer,intent(in) :: nat, ntypes
+      integer,dimension(nat),intent(in) :: iatype
+      character(len=*),dimension(ntypes),intent(in) :: atomnames
+      real(kind=8),dimension(3,nat),intent(in) :: rxyz
+      real(kind=8),dimension(-lmax:lmax,0:lmax,nat),intent(in) :: multipoles
+      real(kind=8),dimension(nat),intent(in) :: rmax
+      real(kind=8),dimension(3),intent(in) :: hgrids
+      logical,intent(in) :: without_normalization
+      
+      ! Local variables
+      character(len=20) :: atomname
+      integer :: i, iat, l, m, nit
+      real(kind=8) :: max_error, factor!, get_normalization, get_test_factor
+      real(kind=8),dimension(:,:,:),allocatable :: multipoles_tmp
+
+          multipoles_tmp = f_malloc((/-lmax.to.lmax,0.to.lmax,1.to.nat/),id='multipoles_tmp')
+
+          if (without_normalization) then
+              nit = 2
+          else
+              nit = 1
+          end if
+
+          factor = 0.5d0*hgrids(1)*0.5d0*hgrids(2)*0.5d0*hgrids(3)
+
+          max_error = 0.d0
+          call yaml_mapping_open('Multipole coefficients')
+          do i=1,nit
+              if (i==1) then
+                  call yaml_map('normalized',.true.)
+                  call yaml_map('radius of normalization sphere',(/minval(rmax),maxval(rmax)/))
+                  call f_memcpy(src=multipoles, dest=multipoles_tmp)
+              else if (i==2) then
+                  call yaml_map('normalized',.false.)
+                  do iat=1,nat
+                      do l=0,lmax
+                          do m=-l,l
+                              !multipoles_tmp(m,l,iat) = multipoles(m,l,iat)/((get_normalization(rmax, l, m)*0.821583836)**2)
+                              !multipoles_tmp(m,l,iat) = multipoles(m,l,iat)*((get_normalization(rmax, l, m)*0.106726871))
+                              multipoles_tmp(m,l,iat) = multipoles(m,l,iat)*get_normalization(rmax(iat),l,m)**2*factor
+                              max_error = max(max_error,abs(multipoles_tmp(m,l,iat)-get_test_factor(l,m)))
+                              !write(*,'(a,3i5,2es14.5)') 'iat, l, m, multipoles(m,l,iat), ref', iat, l, m, multipoles(m,l,iat), get_test_factor(l,m)
+                          end do
+                      end do
+                  end do
+              end if
+              call yaml_sequence_open('Values')
+              do iat=1,nat
+                  call yaml_sequence(advance='no')
+                  atomname=atomnames(iatype(iat))
+                  !call yaml_sequence_open(trim(atomname))
+                  call yaml_map('sym',adjustl(trim(atomname)))
+                  call yaml_map('r',rxyz(1:3,iat))
+                  do l=0,lmax
+                      !call yaml_sequence(advance='no')
+                      !call yaml_map('l='//yaml_toa(l),multipoles(-l:l,l,iat),fmt='(1es16.8)')
+                      !call yaml_map('l='//yaml_toa(l),multipoles(-l:l,l,iat)*sqrt(4.d0**(2*l+3)),fmt='(1es16.8)')
+                      !do m=-l,l
+                          !multipoles(m,l,iat) = multipoles(m,l,iat)*get_normalization(rmax, l, m)
+                          !max_error = max(max_error,abs(multipoles(m,l,iat)-get_test_factor(l,m)))
+                      !end do
+                      call yaml_map('q'//adjustl(trim(yaml_toa(l))),multipoles_tmp(-l:l,l,iat),fmt='(1es16.8)')
+                      call yaml_newline()
+                  end do
+                  !call yaml_comment(trim(yaml_toa(iat,fmt='(i4.4)')))
+                  !call yaml_sequence_close()
+              end do
+              call yaml_sequence_close()
+              if (i==2) then
+                  call yaml_map('max error from original values',max_error)
+              end if
+          end do
+          call yaml_mapping_close()
+
+
+          call f_free(multipoles_tmp)
+
+    end subroutine write_multipoles_new
 
 
     function get_normalization(rmax, l, m) result(fn)
