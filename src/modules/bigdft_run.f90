c849d7981f15f03c0048b6fb5c997ad629632bf7
Thierry Deutsch <Thierry.Deutsch@cea.fr>
2015-11-26 16:24:53 +0100

    Add comments

--- src/modules/bigdft_run.f90
+++ src/modules/bigdft_run.f90
@@ -1,7 +1,7 @@
 !> @file
 !!  Define main module for using BigDFT as a blackbox
 !! @author
-!!    Copyright (C) 2007-2011 BigDFT group (LG, DC)
+!!    Copyright (C) 2007-2015 BigDFT group (LG, DC)
 !!    This file is distributed under the terms of the
 !!    GNU General Public License, see ~/COPYING file
 !!    or http://www.gnu.org/copyleft/gpl.txt .
