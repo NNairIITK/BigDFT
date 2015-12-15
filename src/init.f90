cd5ce4439bedf9c2878743c15d9bb716fbea4005
Stephan Mohr <>
2015-12-15 22:26:30 +0100

    small correction

--- src/init.f90
+++ src/init.f90
@@ -2989,20 +2989,21 @@ subroutine input_wf(iproc,nproc,in,GPU,atoms,rxyz,&
         if (in%lin%kernel_restart_mode==LIN_RESTART_KERNEL .or. in%lin%kernel_restart_mode==LIN_RESTART_DIAG_KERNEL) then
            ! purify kernel? - need to do more testing but doesn't seem to be particularly beneficial
            if (.false.) then
-              if (iproc==0) then
-                  call yaml_sequence(advance='no')
-                  call yaml_mapping_open(flow=.true.)
-                  call yaml_map('Initial kernel purification',.true.)
-              end if
-              !overlap_calculated=.true.
-              do ispin=1,tmb%linmat%l%nspin
+              ! Commented as false anyway... Needed since purify_kernel has been moved to the unused directory
+              !!!!if (iproc==0) then
+              !!!!    call yaml_sequence(advance='no')
+              !!!!    call yaml_mapping_open(flow=.true.)
+              !!!!    call yaml_map('Initial kernel purification',.true.)
+              !!!!end if
+              !!!!!overlap_calculated=.true.
+              !!!!do ispin=1,tmb%linmat%l%nspin
 
-                  !subroutine purify_kernel(iproc, nproc, tmb, overlap_calculated, it_shift, it_opt, order_taylor, &
-                  !           max_inversion_error, purification_quickreturn, ispin)
-                  call purify_kernel(iproc, nproc, tmb, overlap_calculated, 1, 30, in%lin%order_taylor, &
-                       in%lin%max_inversion_error, .false., ispin)
-              end do
-              if (iproc==0) call yaml_mapping_close()
+              !!!!    !subroutine purify_kernel(iproc, nproc, tmb, overlap_calculated, it_shift, it_opt, order_taylor, &
+              !!!!    !           max_inversion_error, purification_quickreturn, ispin)
+              !!!!    call purify_kernel(iproc, nproc, tmb, overlap_calculated, 1, 30, in%lin%order_taylor, &
+              !!!!         in%lin%max_inversion_error, .false., ispin)
+              !!!!end do
+              !!!!if (iproc==0) call yaml_mapping_close()
            end if
         else
            ! come back to this - reconstruct kernel too expensive with exact version, but Taylor needs to be done ~ 3 times here...
