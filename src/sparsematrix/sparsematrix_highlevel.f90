d54b210f5c05621f8509ec665ecacbcdac4757db
Stephan Mohr <stephan.mohr@bsc.es>
2015-12-22 17:19:01 +0100

    working on a clean API for the sparse matrix library, including a test for ICE

--- src/sparsematrix/sparsematrix_highlevel.f90
+++ src/sparsematrix/sparsematrix_highlevel.f90
@@ -7,6 +7,10 @@ module sparsematrix_highlevel
   public :: sparse_matrix_init_from_data
   public :: matrices_init
   public :: matrices_set
+  public :: ccs_data_from_sparse_matrix
+  public :: ccs_matrix_write
+  public :: matrix_matrix_multiplication
+  public :: matrix_chebyshev_expansion
 
   contains
 
@@ -180,5 +184,134 @@ module sparsematrix_highlevel
       call f_release_routine()
     
     end subroutine matrices_set
+
+
+    subroutine ccs_data_from_sparse_matrix(smat, row_ind, col_ptr)
+      use module_base
+      use sparsematrix_base, only: sparse_matrix
+      use sparsematrix_init, only: sparsebigdft_to_ccs
+      implicit none
+
+      ! Calling arguments
+      type(sparse_matrix),intent(in) :: smat
+      integer,dimension(:),pointer,intent(inout) :: row_ind, col_ptr
+
+      if (size(row_ind)/=smat%nvctr) then
+          call f_err_throw('The size of the array row_ind is wrong: '&
+               &//trim(yaml_toa(size(row_ind)))//' instead of '//trim(yaml_toa(smat%nvctr)))
+      end if
+      if (size(col_ptr)/=smat%nfvctr) then
+          call f_err_throw('The size of the array col_ptr is wrong: '&
+               &//trim(yaml_toa(size(col_ptr)))//' instead of '//trim(yaml_toa(smat%nfvctr)))
+      end if
+      call sparsebigdft_to_ccs(smat%nfvctr, smat%nvctr, smat%nseg, smat%keyg, row_ind, col_ptr)
+    end subroutine ccs_data_from_sparse_matrix
+
+
+    subroutine ccs_matrix_write(filename, smat, row_ind, col_ptr, mat)
+      use module_base
+      use sparsematrix_base, only: sparse_matrix, matrices
+      use io, only: write_ccs_matrix
+      implicit none
+
+      ! Calling arguments
+      character(len=*),intent(in) :: filename
+      type(sparse_matrix),intent(in) :: smat
+      integer,dimension(:),pointer,intent(inout) :: row_ind, col_ptr
+      type(matrices),intent(in) :: mat
+
+      if (size(row_ind)/=smat%nvctr) then
+          call f_err_throw('The size of the array row_ind is wrong: '&
+               &//trim(yaml_toa(size(row_ind)))//' instead of '//trim(yaml_toa(smat%nvctr)))
+      end if
+      if (size(col_ptr)/=smat%nfvctr) then
+          call f_err_throw('The size of the array col_ptr is wrong: '&
+               &//trim(yaml_toa(size(col_ptr)))//' instead of '//trim(yaml_toa(smat%nfvctr)))
+      end if
+
+      call write_ccs_matrix(filename, smat%nfvctr, smat%nvctr, row_ind, col_ptr, mat%matrix_compr)
+
+    end subroutine ccs_matrix_write
+
+
+    subroutine matrix_matrix_multiplication(iproc, nproc, smat, a, b, c)
+      use module_base
+      use sparsematrix_base, only: sparse_matrix, matrices
+      use sparsematrix, only: matrix_matrix_mult_wrapper
+      implicit none
+
+      ! Calling arguments
+      integer,intent(in) :: iproc, nproc
+      type(sparse_matrix),intent(in) :: smat
+      type(matrices),intent(inout) :: a
+      type(matrices),intent(inout) :: b, c !b actually also in... 
+
+      call f_routine(id='matrix_matrix_multiplication')
+
+      if (size(a%matrix_compr)/=smat%nvctrp_tg) then
+          call f_err_throw('The size of the array a%matrix_compr is wrong: '&
+               &//trim(yaml_toa(size(a%matrix_compr)))//' instead of '//trim(yaml_toa(smat%nvctrp_tg)))
+      end if
+      if (size(b%matrix_compr)/=smat%nvctrp_tg) then
+          call f_err_throw('The size of the array b%matrix_compr is wrong: '&
+               &//trim(yaml_toa(size(b%matrix_compr)))//' instead of '//trim(yaml_toa(smat%nvctrp_tg)))
+      end if
+      if (size(c%matrix_compr)/=smat%nvctrp_tg) then
+          call f_err_throw('The size of the array c%matrix_compr is wrong: '&
+               &//trim(yaml_toa(size(c%matrix_compr)))//' instead of '//trim(yaml_toa(smat%nvctrp_tg)))
+      end if
+      call matrix_matrix_mult_wrapper(iproc, nproc, smat, a%matrix_compr, b%matrix_compr, c%matrix_compr)
+
+      call f_release_routine()
+
+    end subroutine matrix_matrix_multiplication
+
+
+    subroutine matrix_chebyshev_expansion(iproc, nproc, ndeg, ncalc, ex, &
+               smat_in, smat_out, mat_in, mat_out)
+      use module_base
+      use sparsematrix_base, only: sparse_matrix, matrices
+      use ice, only: inverse_chebyshev_expansion
+      implicit none
+
+      ! Calling arguments
+      integer,intent(in) :: iproc, nproc, ndeg, ncalc
+      type(sparse_matrix),intent(in) ::smat_in, smat_out
+      real(kind=8),dimension(ncalc),intent(in) :: ex
+      type(matrices),intent(in) :: mat_in
+      type(matrices),dimension(ncalc),intent(inout) :: mat_out
+
+      ! Local variables
+      integer :: i
+
+      call f_routine(id='matrix_chebyshev_expansion')
+
+      ! Check the dimensions of the internal arrays
+      if (size(mat_in%matrix_compr)/=smat_in%nvctrp_tg) then
+          call f_err_throw('The size of the array mat_in%matrix_compr is wrong: '&
+               &//trim(yaml_toa(size(mat_in%matrix_compr)))//' instead of '//trim(yaml_toa(smat_in%nvctrp_tg)))
+      end if
+      do i=1,ncalc
+          if (size(mat_out(i)%matrix_compr)/=smat_out%nvctrp_tg) then
+              call f_err_throw('The size of the array mat_out%matrix_compr is wrong: '&
+                   &//trim(yaml_toa(size(mat_out(i)%matrix_compr)))//' instead of '//trim(yaml_toa(smat_out%nvctrp_tg)))
+          end if
+      end do
+
+      ! Check that number of non-zero elements of smat_in is not larger than that of smat_out. This is just a minimal
+      ! check to ensure that the sparsity pattern of smat_in is contained within the one of smat_out. However this check
+      ! is not sufficient to make sure that this condition is fulfilled.
+      if (smat_in%nvctr>smat_out%nvctr) then
+          call f_err_throw('The number of non-zero elements of smat_in ('//&
+               trim(yaml_toa(smat_in%nvctr))//') is larger than the one of smat_out ('//&
+               trim(yaml_toa(smat_out%nvctr))//')')
+      end if
+
+      call inverse_chebyshev_expansion(iproc, nproc, ndeg, &
+           smat_in, smat_out, ncalc, ex, mat_in, mat_out)
+
+      call f_release_routine()
+
+    end subroutine matrix_chebyshev_expansion
 
 end module sparsematrix_highlevel
--- tests/libs/sparsematrix/ice_ccs.f90
+++ tests/libs/sparsematrix/ice_ccs.f90
@@ -5,7 +5,10 @@ program driver
   use bigdft_run, only: bigdft_init
   use sparsematrix_highlevel, only: sparse_matrix_and_matrices_init_from_file_ccs, &
                                     sparse_matrix_init_from_file_ccs, matrices_init, &
-                                    matrices_set, sparse_matrix_init_from_data
+                                    matrices_set, sparse_matrix_init_from_data, &
+                                    ccs_data_from_sparse_matrix, ccs_matrix_write, &
+                                    matrix_matrix_multiplication, matrix_chebyshev_expansion
+  use sparsematrix, only: matrix_matrix_mult_wrapper
   use ice, only: inverse_chebyshev_expansion
   use utilities, only: get_ccs_data_from_file
   implicit none
@@ -13,65 +16,94 @@ program driver
   ! Variables
   integer :: i
   integer :: norder_polynomial
-  type(sparse_matrix) :: smat_s, smat_l, smat3
-  type(matrices) :: overlap
-  type(matrices),dimension(1) :: inv_overlap, mat2
+  type(sparse_matrix) :: smat1, smat2, smat3
+  type(matrices) :: mat1
+  type(matrices),dimension(2) :: mat2
   type(matrices),dimension(2) :: mat3
   integer :: nfvctr, nvctr
   integer,dimension(:),pointer :: row_ind, col_ptr
 
-
+  ! Initialize flib
   call f_lib_initialize()
 
+  ! General initialization, including MPI. Here we have:
+  ! bigdft_mpi%iproc is the task ID
+  ! bigdft_mpi%nproc is the total number of tasks
   call bigdft_init()
 
-  ! Creates the type which has the sparse matrix descriptors (smat_s) as well as
-  ! the type which contains the matrix data (overlap). The matrix element are stored in overlap%matrix_compr.
-  call sparse_matrix_and_matrices_init_from_file_ccs('overlap_ccs.dat', bigdft_mpi%iproc, bigdft_mpi%nproc, smat_s, overlap)
+  ! Read from matrix1.dat and create the type containing the sparse matrix descriptors (smat1) as well as
+  ! the type which contains the matrix data (overlap). The matrix element are stored in mat1%matrix_compr.
+  call sparse_matrix_and_matrices_init_from_file_ccs('matrix1.dat', bigdft_mpi%iproc, bigdft_mpi%nproc, smat1, mat1)
 
-  ! Creates the type which has the sparse matrix descriptors (smat_l).
-  call sparse_matrix_init_from_file_ccs('kernel_ccs.dat', bigdft_mpi%iproc, bigdft_mpi%nproc, smat_l)
+  ! Read from matrix2.dat and creates the type containing the sparse matrix descriptors (smat2).
+  call sparse_matrix_init_from_file_ccs('matrix2.dat', bigdft_mpi%iproc, bigdft_mpi%nproc, smat2)
 
-  ! This routine prepares the type containing the matrix data.
-  call matrices_init(smat_l, inv_overlap(1))
+  ! Prepares the type containing the matrix data.
+  call matrices_init(smat2, mat2(1))
 
-  ! Calculate the square root of the matrix
+  ! Calculate the square root of the matrix described by the pair smat1/mat1 and store the result in
+  ! smat2/mat2. Attention: The sparsity pattern of smat2 must be contained within smat1.
+  ! It is your responsabilty to assure this, the routine does only some minimal checks.
+  ! The final result is contained in mat2(1)%matrix_compr.
   norder_polynomial = 30
-  call inverse_chebyshev_expansion(bigdft_mpi%iproc, bigdft_mpi%nproc, norder_polynomial, &
-       smat_s, smat_l, 1, (/0.5d0/), overlap, inv_overlap)
+  call matrix_chebyshev_expansion(bigdft_mpi%iproc, bigdft_mpi%nproc, norder_polynomial, 1, (/0.5d0/), &
+       smat1, smat2, mat1, mat2(1))
 
-  ! Create another matrix type, this time directly with the CCS format descriptors. Get these descriptors
-  ! from an auxiliary routine.
-  call get_ccs_data_from_file('kernel_ccs.dat', nfvctr, nvctr, row_ind, col_ptr)
+  ! Create another matrix type, this time directly with the CCS format descriptors.
+  ! Get these descriptors from an auxiliary routine using again matrix2.dat
+  call get_ccs_data_from_file('matrix2.dat', nfvctr, nvctr, row_ind, col_ptr)
   call sparse_matrix_init_from_data(bigdft_mpi%iproc, bigdft_mpi%nproc, nfvctr, nvctr, row_ind, col_ptr, smat3)
-  call f_free_ptr(row_ind)
-  call f_free_ptr(col_ptr)
 
   ! Prepare a new matrix data type.
-  call matrices_init(smat_l, mat2(1))
-  ! Set the contents of this new matrix type
-  call matrices_set(smat_l, inv_overlap(1)%matrix_compr, mat2(1))
+  call matrices_init(smat2, mat2(2))
+  ! Set the contents of this new matrix type, using the above result.
+  call matrices_set(smat2, mat2(1)%matrix_compr, mat2(2))
 
-  ! Prepare two new matrices
+  ! Prepare two new matrix data types
   do i=1,2
       call matrices_init(smat3, mat3(i))
   end do
 
-  ! Calculate the square root and the inverse square of the matrix
+  ! Calculate at the same time the square root and the inverse square of the matrix described  by the pair smat2/mat2
+  ! and store the result in smat3/mat3. The final results are thus in mat3(1)%matrix_compr and mat3(2)%matrix_compr.
+  ! The same wraning as above applies.
   norder_polynomial = 40
-  call inverse_chebyshev_expansion(bigdft_mpi%iproc, bigdft_mpi%nproc, norder_polynomial, &
-       smat_l, smat3, 2, (/0.5d0,-0.5d0/), mat2(1), mat3)
+  call matrix_chebyshev_expansion(bigdft_mpi%iproc, bigdft_mpi%nproc, norder_polynomial, 2, (/0.5d0,-0.5d0/), &
+       smat2, smat3, mat2(2), mat3)
 
-  call deallocate_sparse_matrix(smat_s)
-  call deallocate_sparse_matrix(smat_l)
+  ! Calculate the CCS descriptors from the sparse_matrix type.
+  call ccs_data_from_sparse_matrix(smat3, row_ind, col_ptr)
+
+  ! Write the two matrices to disk, using the CCS format
+  call ccs_matrix_write('squareroot.dat', smat3, row_ind, col_ptr, mat3(1))
+  call ccs_matrix_write('invsquareroot.dat', smat3, row_ind, col_ptr, mat3(2))
+
+  ! Multiply the two matrices calculated above (i.e. the square root and the inverse square root) and
+  ! store the result in mat2. The final result is thus contained in mat2%matrix_compr.
+  call matrix_matrix_multiplication(bigdft_mpi%iproc, bigdft_mpi%nproc, smat3, &
+       mat3(1), mat3(2), mat2(1))
+
+  ! Write the result of the above multiplication to a file. Since we multiply the square root times the 
+  ! inverse square root, the result will be the unity matrix.
+  call ccs_matrix_write('unity.dat', smat3, row_ind, col_ptr, mat2(1))
+
+  ! Deallocate all the sparse matrix descriptrs types
+  call deallocate_sparse_matrix(smat1)
+  call deallocate_sparse_matrix(smat2)
   call deallocate_sparse_matrix(smat3)
-  call deallocate_matrices(overlap)
-  call deallocate_matrices(inv_overlap(1))
-  call deallocate_matrices(mat2(1))
+
+  ! Deallocate all the matrix data types
+  call deallocate_matrices(mat1)
   do i=1,2
+      call deallocate_matrices(mat2(i))
       call deallocate_matrices(mat3(i))
   end do
 
+  ! Deallocate the CCS format descriptors
+  call f_free_ptr(row_ind)
+  call f_free_ptr(col_ptr)
+
+  ! Finalize flib
   call f_lib_finalize()
 
 
