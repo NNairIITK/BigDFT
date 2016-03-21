5e630132ed1b6b210a3b22066c12dd425deb48a0
Stephan Mohr <>
2016-03-21 14:56:56 +0100

    removed dependencies in the sparsematrix libs tests

--- tests/libs/sparsematrix/driver_foe_ccs.f90
+++ tests/libs/sparsematrix/driver_foe_ccs.f90
@@ -1,11 +1,8 @@
 program driver_css
-  use module_base
-  use sparsematrix_base, only: sparse_matrix, matrices, &
-                               deallocate_sparse_matrix, deallocate_matrices, &
-                               assignment(=), sparsematrix_malloc_ptr, SPARSE_FULL
+  ! The following module are part of the sparsematrix library
+  use sparsematrix_base
   use foe_base, only: foe_data, foe_data_deallocate
   use foe_common, only: init_foe
-  use bigdft_run, only: bigdft_init
   use sparsematrix_highlevel, only: sparse_matrix_and_matrices_init_from_file_ccs, &
                                     sparse_matrix_init_from_file_ccs, matrices_init, &
                                     matrices_get_values, matrices_set_values, &
@@ -13,13 +10,15 @@ program driver_css
                                     ccs_data_from_sparse_matrix, ccs_matrix_write, &
                                     matrix_matrix_multiplication, matrix_fermi_operator_expansion, &
                                     trace_AB
-  use utilities, only: get_ccs_data_from_file
   use sparsematrix, only: write_matrix_compressed, transform_sparse_matrix
-  use yaml_output
+  ! The following module is an auxiliary module for this test
+  use utilities, only: get_ccs_data_from_file
+  ! The following module should no be used!
+  use module_types, only: bigdft_init_errors, &
+                          bigdft_init_timing_categories
   implicit none
 
   ! Variables
-  integer :: i
   type(sparse_matrix) :: smat_s, smat_h, smat_k
   type(matrices) :: mat_s, mat_h, mat_k
   type(matrices),dimension(1) :: mat_ovrlpminusonehalf
@@ -33,28 +32,30 @@ program driver_css
   ! Initialize flib
   call f_lib_initialize()
 
-  ! General initialization, including MPI. Here we have:
+  ! MPI initialization; we have:
   ! iproc is the task ID
   ! nproc is the total number of tasks
-  ! PROBLEM: This is in src/modules...
-  call bigdft_init()
-  !call mpiinit()
-  !iproc=mpirank()
-  !nproc=mpisize()
+  call mpiinit()
+  iproc=mpirank()
+  nproc=mpisize()
 
+  ! Initialize the BigDFT error handling and timing.
+  ! PROBLEM: THIS IS IN MODULE_TYPES
+  call bigdft_init_errors()
+  call bigdft_init_timing_categories()
 
   ! Read from matrix1.dat and create the type containing the sparse matrix descriptors (smat_s) as well as
   ! the type which contains the matrix data (overlap). The matrix element are stored in mat_s%matrix_compr.
   ! Do the same also for matrix2.dat
   call sparse_matrix_and_matrices_init_from_file_ccs('overlap_ccs.dat', &
-       bigdft_mpi%iproc, bigdft_mpi%nproc, bigdft_mpi%mpi_comm, smat_s, mat_s)
+       iproc, nproc, mpi_comm_world, smat_s, mat_s)
   call sparse_matrix_and_matrices_init_from_file_ccs('hamiltonian_ccs.dat', &
-       bigdft_mpi%iproc, bigdft_mpi%nproc, bigdft_mpi%mpi_comm, smat_h, mat_h)
+       iproc, nproc, mpi_comm_world, smat_h, mat_h)
 
   ! Create another matrix type, this time directly with the CCS format descriptors.
   ! Get these descriptors from an auxiliary routine using matrix3.dat
   call get_ccs_data_from_file('density_kernel_ccs.dat', nfvctr, nvctr, row_ind, col_ptr)
-  call sparse_matrix_init_from_data_ccs(bigdft_mpi%iproc, bigdft_mpi%nproc, bigdft_mpi%mpi_comm, &
+  call sparse_matrix_init_from_data_ccs(iproc, nproc, mpi_comm_world, &
        nfvctr, nvctr, row_ind, col_ptr, smat_k)
 
   ! Prepares the type containing the matrix data.
@@ -65,7 +66,7 @@ program driver_css
   ! Only provide the mandatory values and take for the optional values the default ones.
   charge = f_malloc(smat_s%nspin,id='charge')
   charge(:) = 722.d0
-  call init_foe(bigdft_mpi%iproc, bigdft_mpi%nproc, smat_s%nspin, charge, foe_obj)
+  call init_foe(iproc, nproc, smat_s%nspin, charge, foe_obj)
 
   ! Calculate the density kernel for the system described by the pair smat_s/mat_s and smat_h/mat_h and 
   ! store the result in smat_k/mat_k.
@@ -73,19 +74,20 @@ program driver_css
   ! and the one of smat_h within that of smat_k. It is your responsabilty to assure this, 
   ! the routine does only some minimal checks.
   ! The final result will be contained in mat_k%matrix_compr.
-  call matrix_fermi_operator_expansion(bigdft_mpi%iproc, bigdft_mpi%nproc, foe_obj, smat_s, smat_h, smat_k, &
+  call matrix_fermi_operator_expansion(iproc, nproc, mpi_comm_world, &
+       foe_obj, smat_s, smat_h, smat_k, &
        mat_s, mat_h, mat_ovrlpminusonehalf, mat_k, energy, &
        calculate_minusonehalf=.true., foe_verbosity=0)
 
   ! Write the result in YAML format to the standard output (required for non-regression tests).
-  if (bigdft_mpi%iproc==0) call write_matrix_compressed('Result of FOE', smat_k, mat_k)
+  if (iproc==0) call write_matrix_compressed('Result of FOE', smat_k, mat_k)
 
   ! Calculate trace(KS)
-  !tr_KS = trace_sparse(bigdft_mpi%iproc, bigdft_mpi%nproc, smat_s, smat_k, mat_s%matrix_compr, mat_k%matrix_compr, 1)
-  tr_KS = trace_AB(bigdft_mpi%iproc, bigdft_mpi%nproc, bigdft_mpi%mpi_comm, smat_s, smat_k, mat_s, mat_k, 1)
+  !tr_KS = trace_sparse(iproc, nproc, smat_s, smat_k, mat_s%matrix_compr, mat_k%matrix_compr, 1)
+  tr_KS = trace_AB(iproc, nproc, mpi_comm_world, smat_s, smat_k, mat_s, mat_k, 1)
 
   ! Write the result
-  if (bigdft_mpi%iproc==0) call yaml_map('trace(KS)',tr_KS)
+  if (iproc==0) call yaml_map('trace(KS)',tr_KS)
 
   ! Extract the compressed kernel matrix from the data type.
   ! The first routine allocates an array with the correct size, the second one extracts the result.
@@ -98,7 +100,7 @@ program driver_css
 
   ! Transform the overlap matrix to the sparsity pattern of the kernel
   overlap_large = sparsematrix_malloc_ptr(smat_k, iaction=SPARSE_FULL, id='overlap_large')
-  call transform_sparse_matrix(bigdft_mpi%iproc, smat_s, smat_k, 'small_to_large', &
+  call transform_sparse_matrix(iproc, smat_s, smat_k, 'small_to_large', &
        smat_in=overlap, lmat_out=overlap_large)
 
   ! Again calculate trace(KS), this time directly with the array holding the data.
@@ -106,10 +108,10 @@ program driver_css
   tr_KS_check = dot(smat_k%nvctr, kernel(1), 1, overlap_large(1), 1)
 
   ! Write the result
-  if (bigdft_mpi%iproc==0) call yaml_map('trace(KS) check',tr_KS_check)
+  if (iproc==0) call yaml_map('trace(KS) check',tr_KS_check)
 
   ! Write the difference to the previous result
-  if (bigdft_mpi%iproc==0) call yaml_map('difference',tr_KS-tr_KS_check)
+  if (iproc==0) call yaml_map('difference',tr_KS-tr_KS_check)
 
   ! Deallocate the object holding the FOE parameters
   call foe_data_deallocate(foe_obj)
@@ -137,7 +139,8 @@ program driver_css
   call bigdft_finalize(ierr)
 
   ! Finalize flib
-  call f_lib_finalize()
-
+  ! SM: I have the impression that every task should call this routine, but if I do so
+  ! some things are printed nproc times instead of once.
+  if (iproc==0) call f_lib_finalize()
 
 end program driver_css
