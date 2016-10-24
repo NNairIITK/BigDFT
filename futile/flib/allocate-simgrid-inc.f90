98f1472ef230ebc1e58740b5875334c35a6370bc
Luigi Genovese <luigi.genovese@cea.fr>
2016-10-25 00:08:15 +0200

    Put the multithreading in the FFT3D minibench

--- futile/flib/allocate-simgrid-inc.f90
+++ futile/flib/allocate-simgrid-inc.f90
@@ -17,7 +17,7 @@
   if (c_allocation) then
      p = smpi_shared_malloc(product(m%shape(1:(m%rank-1)))*(m%shape(m%rank)+padding)*f_sizeof(d),&
           trim(m%array_id)//char(0), int(m%rank,f_long))
-     call c_f_pointer(p, array, m%shape(1:m%rank))
+     call c_f_pointer(p, array, int(m%shape(1:m%rank)))
      !call f_map_ptr(m%lbounds,m%ubounds,array_tmp,array)
      !here we might add a fortran call to the function for the reshaping of the pointer
      ierror=0
