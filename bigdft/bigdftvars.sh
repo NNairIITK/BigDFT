ff1e2421f6789436b72972c04a0b7c1c0fd02393
Luigi Genovese <luigi.genovese@cea.fr>
2016-11-02 22:25:47 +0100

    Modifications for source detection

--- bigdft/bigdftvars.sh
+++ bigdft/bigdftvars.sh
@@ -1,7 +1,13 @@
-echo "While sourcing this script thethe following environment variable will be set:"
+echo "While sourcing this script the following environment variable will be set:"
 for file in futile_environment.sh bigdft_environment.sh
 do
 sh $file
 eval `sh $file`
 done
-echo '...done.'
+$(return >/dev/null 2>&1)
+if [ "$?" -eq "0" ]
+then
+    echo "...done."
+else
+    echo "ERROR: This script is NOT sourced! Execute 'source" $0 "' instead"
+fi
